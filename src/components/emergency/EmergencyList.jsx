bb9282495954f8e5ebda9897f64dd4e103c9a58b
Bobby Martin <robert.martin.619@gmail.com>
2018-08-13 16:50:33 -0700

    Remove Icons component from emergency

--- src/components/emergency/EmergencyList.jsx
+++ src/components/emergency/EmergencyList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
-import Icon from '../icon/Icon';
 
 import EmergencyNumber from './EmergencyNumber';
 import './Emergency.scss';
