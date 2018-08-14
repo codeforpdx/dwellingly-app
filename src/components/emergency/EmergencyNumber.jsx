62b5eb552f07a16e1fa40be13bf58da67d219611
Bobby Martin <robert.martin.619@gmail.com>
2018-08-14 15:01:18 -0700

    Add multiple state slices to state, still need to turn this into a reducer

--- src/components/emergency/EmergencyNumber.jsx
+++ src/components/emergency/EmergencyNumber.jsx
@@ -1,46 +1,61 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import EmergencyNumberEdit from '../forms/EmergencyNumberEdit';
+import EmergencyNumberStatic from './EmergencyNumberStatic';
 
 class EmergencyNumber extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      editing: false
+      editing: false,
+      doneEditing: false,
+      title: '',
+      number01: '',
+      number02: ''
     }
     this.handleEditingNumber = this.handleEditingNumber.bind(this);
+    this.handleChangedNumberField = this.handleChangedNumberField.bind(this);
+    this.handleSavingNewEmergencyNumber = this.handleSavingNewEmergencyNumber.bind(this);
   }
 
   handleEditingNumber(prevState) {
     this.setState({ editing: !prevState.editing })
   }
 
+  handleSavingNewEmergencyNumber(prevState) {
+    this.setState({ doneEditing: !prevState.doneEditing })
+    this.setState({ editing: !prevState.editing })
+  }
+
+  handleChangedNumberField(e) {
+    const targetName = e.target.name;
+    this.setState({
+      [targetName]: e.target.value
+    })
+  }
+
   render() {
     const { emergency } = this.props;
     return (
       <div onClick={this.handleEditingNumber} role="presentation">
-      {!this.state.editing ?
-        <div className="emergencyNumberRow" key={`row-${emergency.id}`}>
-          <div className="emergencyTitle">
-            {emergency.title}
-          </div>
-          <div className="emergencyNumberContainer">
-            <div className="emergencyNumber">
-              <a href={`tel:${emergency.number01}`}>
-                {emergency.number01}
-              </a>
-            </div>
-            {emergency.number02 !== null && emergency.number02 !== ''
-              && (
-              <div className="emergencyNumber">
-                |&nbsp;&nbsp;&nbsp;<a href={`tel:${emergency.number02}`}>
-                  {emergency.number02}
-                </a>
-              </div>
-              )
-            }
-          </div>
-        </div> :
-        <div>Editing!!!!!!!</div>}
+      {
+        !this.state.editing ?
+          <EmergencyNumberStatic
+            emergency={emergency}
+            doneEditing={this.state.doneEditing}
+            newTitle={this.state.title}
+            newNumber01={this.state.number01}
+            newNumber02={this.state.number02} /> :
+          <EmergencyNumberEdit
+            id={emergency.id}
+            title={emergency.title}
+            number01={emergency.number01}
+            number02={emergency.number02 !== null && emergency.number02 !== '' ? emergency.number02 : null }
+            onCancellingEditEmergencyNumber={this.handleEditingNumber}
+            onEditingEmergencyNumber={this.handleChangedNumberField}
+            onSavingNewEmergencyNumber={this.handleSavingNewEmergencyNumber}
+          />
+      }
       </div>
     );
   }
