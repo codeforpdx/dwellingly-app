bb87e60e507df044df26dfa4d4bf654868e997a3
Bobby Martin <robert.martin.619@gmail.com>
2018-08-14 15:24:50 -0700

    Add helper function to run both functions on save

--- src/components/forms/EmergencyNumberEdit.jsx
+++ src/components/forms/EmergencyNumberEdit.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const EmergencyNumberEdit = (props) => (
-  <div id={props.id}>
-    <input name="title" placeholder={props.title} onChange={props.onEditingEmergencyNumber} type="text" />
-    <input name="number01" placeholder={props.number01} onChange={props.onEditingEmergencyNumber} type="text" />
-    {props.number02 !== null && props.number02 !== ''
-      && (
-        <input name="number02" placeholder={props.number02} onChange={props.onEditingEmergencyNumber} type="text" />
-      )
-    }
-    <button type="submit" className="btn" onClick={props.onSavingNewEmergencyNumber}>Save</button>
-    <button type="button" className="btn btn--danger" onClick={props.onCancellingEditEmergencyNumber}>Cancel</button>
-  </div>
-);
+const EmergencyNumberEdit = (props) => {
+  const onSubmit = () => {
+    props.onSavingNewEmergencyNumber();
+    props.onCancellingEditEmergencyNumber();
+  }
+  return (
+    <div id={props.id}>
+      <input name="title" placeholder={props.title} onChange={props.onEditingEmergencyNumber} type="text" />
+      <input name="number01" placeholder={props.number01} onChange={props.onEditingEmergencyNumber} type="text" />
+      {props.number02 !== null && props.number02 !== ''
+        && (
+          <input name="number02" placeholder={props.number02} onChange={props.onEditingEmergencyNumber} type="text" />
+        )
+      }
+      <button type="submit" className="btn" onClick={() => onSubmit()}>Save</button>
+      <button type="button" className="btn btn--danger" onClick={props.onCancellingEditEmergencyNumber}>Cancel</button>
+    </div>
+  )
+};
 
 EmergencyNumberEdit.propTypes = {
   id: PropTypes.string.isRequired,
