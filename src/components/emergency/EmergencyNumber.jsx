5f78129137b24dff7a29a6039231d02b6440791d
Bobby Martin <robert.martin.619@gmail.com>
2018-08-14 10:59:36 -0700

    Make emergency number component stateful to add editing component

--- src/components/emergency/EmergencyNumber.jsx
+++ src/components/emergency/EmergencyNumber.jsx
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const EmergencyNumber = ({ emergency }) => (
-  <div className="emergencyNumberRow" key={`row-${emergency.id}`}>
-    <div className="emergencyTitle">
-      {emergency.title}
-    </div>
-    <div className="emergencyNumberContainer">
-      <div className="emergencyNumber">
-        <a href={`tel:${emergency.number01}`}>
-          {emergency.number01}
-        </a>
+class EmergencyNumber extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editing: false
+    }
+    this.handleEditingNumber = this.handleEditingNumber.bind(this);
+  }
+
+  handleEditingNumber(prevState) {
+    this.setState({ editing: !prevState.editing })
+  }
+
+  render() {
+    const { emergency } = this.props;
+    return (
+      <div onClick={this.handleEditingNumber} role="presentation">
+      {!this.state.editing ?
+        <div className="emergencyNumberRow" key={`row-${emergency.id}`}>
+          <div className="emergencyTitle">
+            {emergency.title}
+          </div>
+          <div className="emergencyNumberContainer">
+            <div className="emergencyNumber">
+              <a href={`tel:${emergency.number01}`}>
+                {emergency.number01}
+              </a>
+            </div>
+            {emergency.number02 !== null && emergency.number02 !== ''
+              && (
+              <div className="emergencyNumber">
+                |&nbsp;&nbsp;&nbsp;<a href={`tel:${emergency.number02}`}>
+                  {emergency.number02}
+                </a>
+              </div>
+              )
+            }
+          </div>
+        </div> :
+        <div>Editing!!!!!!!</div>}
       </div>
-      {emergency.number02 !== null && emergency.number02 !== ''
-        && (
-        <div className="emergencyNumber">
-          |&nbsp;&nbsp;&nbsp;<a href={`tel:${emergency.number02}`}>
-            {emergency.number02}
-          </a>
-        </div>
-        )
-      }
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 EmergencyNumber.propTypes = {
   emergency: PropTypes.shape({
