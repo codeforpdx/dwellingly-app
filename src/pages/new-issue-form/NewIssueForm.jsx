ece48988e7f29dd067875326baca751ef426c066
Bobby Martin <robert.martin.619@gmail.com>
2018-08-15 11:31:10 -0700

    Add actions to header

--- src/pages/new-issue-form/NewIssueForm.jsx
+++ src/pages/new-issue-form/NewIssueForm.jsx
@@ -14,13 +14,33 @@ class NewIssueForm extends Component {
         <Header variant="form">
           {() => (
             <div>
-              <div className="actions">&nbsp;</div>
+              <div className="actions">
+                <button
+                  type="button"
+                  aria-label="Cancel"
+                  className="action action--strong action--left">
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  aria-label="Done"
+                  className="action action--strong action--right">
+                  Done
+                </button>
+              </div>
               <Header.Label label="New Issue" type="basic" />
             </div>
           )}
         </Header>
 
-        <section className="main width-wrapper" />
+        <section className="main width-wrapper">
+          <fieldset>
+            <div className="message message--light">
+              <p>Please tell us why you are opening a new issue.</p>
+            </div>
+            <textarea />
+          </fieldset>
+        </section>
       </div>
     );
   }
