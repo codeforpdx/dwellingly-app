96414d8fe52a61dc86361d996741820d370a6686
Joseph Wilson <jgwilsondesign@gmail.com>
2018-08-15 17:35:55 -0700

    fixing the Card file path

--- src/pages/new-issue-form/NewIssueForm.jsx
+++ src/pages/new-issue-form/NewIssueForm.jsx
@@ -5,14 +5,15 @@ import RadioArray from '../../components/input/RadioArray';
 
 // import { dummyUser } from '../../data';
 
-
 class NewIssueForm extends Component {
   constructor(props) {
     super(props);
 
     this.handleChange = this.handleChange.bind(this);
     this.handleChangingIssueType = this.handleChangingIssueType.bind(this);
-    this.handleMovingToNextIssueStep = this.handleMovingToNextIssueStep.bind(this);
+    this.handleMovingToNextIssueStep = this.handleMovingToNextIssueStep.bind(
+      this
+    );
     this.handleSavingNote = this.handleSavingNote.bind(this);
     this.handleAddingNote = this.handleAddingNote.bind(this);
     this.handleGoingBack = this.handleGoingBack.bind(this);
@@ -38,31 +39,33 @@ class NewIssueForm extends Component {
   }
 
   handleChangingIssueType() {
-    this.setState(prevState => ({ issueNextStep: !prevState.issueNextStep }))
+    this.setState(prevState => ({ issueNextStep: !prevState.issueNextStep }));
   }
 
   handleGoingBack() {
-    if(this.state.issueNextStep && this.state.issueUrgancyStepDone) {
-      this.setState({ issueUrgancyStepDone: false })
+    if (this.state.issueNextStep && this.state.issueUrgancyStepDone) {
+      this.setState({ issueUrgancyStepDone: false });
     } else {
-      this.setState({ issueNextStep: false })
+      this.setState({ issueNextStep: false });
     }
   }
 
   handleAddingNote() {
-    this.setState(prevState => ({ issueAddingNote: !prevState.issueAddingNote }))
+    this.setState(prevState => ({
+      issueAddingNote: !prevState.issueAddingNote
+    }));
   }
 
   handleSavingNote() {
-    this.handleAddingNote()
-    this.setState(prevState => ({ issueNoteDone: !prevState.issueNoteDone }))
+    this.handleAddingNote();
+    this.setState(prevState => ({ issueNoteDone: !prevState.issueNoteDone }));
   }
 
   handleMovingToNextIssueStep() {
-    if(this.state.issueNextStep) {
-      this.setState({ issueUrgancyStepDone: true })
+    if (this.state.issueNextStep) {
+      this.setState({ issueUrgancyStepDone: true });
     } else {
-      this.setState({ issueNextStep: true })
+      this.setState({ issueNextStep: true });
     }
   }
 
@@ -83,52 +86,47 @@ class NewIssueForm extends Component {
           {() => (
             <div>
               <div className="actions">
-                {
-                  // Left Side Header Buttons
-                  !this.state.issueChangeType && !this.state.issueNextStep ?
-                    <button
-                      type="button"
-                      aria-label="Cancel"
-                      className="action action--strong action--left">
-                      Cancel
-                    </button> :
-                    null
-                }
-                {this.state.issueNextStep ?
+                {// Left Side Header Buttons
+                !this.state.issueChangeType && !this.state.issueNextStep ? (
+                  <button
+                    type="button"
+                    aria-label="Cancel"
+                    className="action action--strong action--left">
+                    Cancel
+                  </button>
+                ) : null}
+                {this.state.issueNextStep ? (
                   <button
                     type="button"
                     aria-label="Back"
                     className="action action--strong action--left"
                     onClick={this.handleGoingBack}>
                     Back
-                  </button> :
-                  null
-                }
-                {
-                  // Right Side Header Buttons
-                  !this.state.issueUrgancyStepDone || this.state.issueUrgancyStepDone && !this.state.issueAddingNote ?
-                    <button
-                      type="button"
-                      aria-label="Next"
-                      className="action action--strong action--right"
-                      onClick={this.handleMovingToNextIssueStep}
-                      disabled={!this.state.issue}>
-                      Next
-                    </button> :
-                  null
-                }
-                {
-                  this.state.issueAddingNote ?
-                    <button
-                      type="button"
-                      aria-label="Next"
-                      className="action action--strong action--right"
-                      onClick={this.handleSavingNote}
-                      disabled={!this.state.issue}>
-                      Save
-                    </button> :
-                  null
-                }
+                  </button>
+                ) : null}
+                {// Right Side Header Buttons
+                !this.state.issueUrgancyStepDone ||
+                (this.state.issueUrgancyStepDone &&
+                  !this.state.issueAddingNote) ? (
+                  <button
+                    type="button"
+                    aria-label="Next"
+                    className="action action--strong action--right"
+                    onClick={this.handleMovingToNextIssueStep}
+                    disabled={!this.state.issue}>
+                    Next
+                  </button>
+                ) : null}
+                {this.state.issueAddingNote ? (
+                  <button
+                    type="button"
+                    aria-label="Next"
+                    className="action action--strong action--right"
+                    onClick={this.handleSavingNote}
+                    disabled={!this.state.issue}>
+                    Save
+                  </button>
+                ) : null}
               </div>
               <Header.Label label="New Issue" type="basic" />
             </div>
@@ -136,95 +134,91 @@ class NewIssueForm extends Component {
         </Header>
 
         <section className="main width-wrapper">
-          {
-           !this.state.issueNextStep ?
-              <fieldset>
-                <div className="message message--light">
-                  <p>
-                    <strong>Step 1: Select Issue</strong>
-                  </p>
-                </div>
-                {this.issueOptions.map(option => {
-                  const key = Object.keys(option)[0];
-                  return (
-                    <Input
-                      id={`settings-issue-${key}`}
-                      key={key}
-                      label={option[key]}
-                      model={this.state.issue}
-                      name="issue"
-                      onChange={this.handleChange}
-                      type="radio"
-                      value={option[key]}
-                    />
-                  );
-                })}
-              </fieldset> :
-            null
-          }
-          {
-            this.state.issueNextStep && !this.state.issueUrgancyStepDone ?
-              <fieldset>
-                <div className="message message--light">
-                  <p>
-                    <strong>Step 2: Select urgency level.</strong>
-                  </p>
-                </div>
-                <div className="message">
-                  <p>This will help our JOIN staff prioritize open tickets.</p>
-                </div>
-                <RadioArray
-                  name="urgency"
-                  model={this.state.urgency}
-                  onChange={this.handleChange}
-                  options={[
-                    {
-                      id: 'low',
-                      label: 'low',
-                      value: 'low'
-                    },
-                    {
-                      id: 'med',
-                      label: 'med',
-                      value: 'med'
-                    },
-                    {
-                      id: 'high',
-                      label: 'high',
-                      value: 'high'
-                    }
-                  ]}
+          {!this.state.issueNextStep ? (
+            <fieldset>
+              <div className="message message--light">
+                <p>
+                  <strong>Step 1: Select Issue</strong>
+                </p>
+              </div>
+              {this.issueOptions.map(option => {
+                const key = Object.keys(option)[0];
+                return (
+                  <Input
+                    id={`settings-issue-${key}`}
+                    key={key}
+                    label={option[key]}
+                    model={this.state.issue}
+                    name="issue"
+                    onChange={this.handleChange}
+                    type="radio"
+                    value={option[key]}
                   />
-              </fieldset> :
-            null
-          }
-          {
-            this.state.issueUrgancyStepDone && !this.state.issueAddingNote ?
-              <fieldset>
-                <div className="message message--light">
-                  <p>
-                    <strong>Step 3: Add attachments</strong>
-                  </p>
-                </div>
-                <Input type="button" onClick={() => {this.handleAddingNote()}}>
-                  Add Note
-                </Input>
-                <Input type="button" url="/tickets">
-                  Add Photo
-                </Input>
-              </fieldset> :
-              null
-          }
-          {
-            this.state.issueAddingNote ?
-              <fieldset>
-                <div className="message message--light">
-                  <p>Note Demo</p>
-                </div>
-                <textarea placeholder="Add a note..." rows="8" />
-              </fieldset> :
-              null
-          }
+                );
+              })}
+            </fieldset>
+          ) : null}
+          {this.state.issueNextStep && !this.state.issueUrgancyStepDone ? (
+            <fieldset>
+              <div className="message message--light">
+                <p>
+                  <strong>Step 2: Select urgency level.</strong>
+                </p>
+              </div>
+              <div className="message">
+                <p>This will help our JOIN staff prioritize open tickets.</p>
+              </div>
+              <RadioArray
+                name="urgency"
+                model={this.state.urgency}
+                onChange={this.handleChange}
+                options={[
+                  {
+                    id: 'low',
+                    label: 'low',
+                    value: 'low'
+                  },
+                  {
+                    id: 'med',
+                    label: 'med',
+                    value: 'med'
+                  },
+                  {
+                    id: 'high',
+                    label: 'high',
+                    value: 'high'
+                  }
+                ]}
+              />
+            </fieldset>
+          ) : null}
+          {this.state.issueUrgancyStepDone && !this.state.issueAddingNote ? (
+            <fieldset>
+              <div className="message message--light">
+                <p>
+                  <strong>Step 3: Add attachments</strong>
+                </p>
+              </div>
+              <Input
+                type="button"
+                onClick={() => {
+                  this.handleAddingNote();
+                }}>
+                Add Note
+              </Input>
+              <Input type="button" url="/tickets">
+                Add Photo
+              </Input>
+            </fieldset>
+          ) : null}
+          {this.state.issueAddingNote ? (
+            <fieldset>
+              <div className="message message--light">
+                <p>Note Demo</p>
+              </div>
+              <textarea placeholder="Add a note..." rows="8" />
+            </fieldset>
+          ) : null}
         </section>
       </div>
     );
