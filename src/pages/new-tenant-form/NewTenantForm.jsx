5b78851e3d966f076ff2ef22105978e0c523ac7e
Bobby Martin <robert.martin.619@gmail.com>
2018-08-23 16:10:35 -0700

    Add new tenant form page

---
+++ src/pages/new-tenant-form/NewTenantForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class NewTenantForm extends Component {
+  render() {
+    return (
+      <div className="admin page">test</div>
+    )
+  }
+}
+
+export default NewTenantForm;
