383fae0221d62f213f9d9f36b1d128b240be22b0
Bobby Martin <robert.martin.619@gmail.com>
2018-08-27 15:00:24 -0700

    Add add new manager form and functionality with some like testing

--- src/pages/new-tenant-form/NewTenantForm.jsx
+++ src/pages/new-tenant-form/NewTenantForm.jsx
@@ -4,7 +4,7 @@ import Input from '../../components/input/Input';
 import Icon from '../../components/icon/Icon';
 // import SearchForm from '../../components/search-form/SearchForm';
 import { ROLES, ROUTES } from '../../constants/constants';
-import { dummyUser, properties } from '../../data';
+import { dummyUser, properties, propertyManagers } from '../../data';
 
 import './NewTenantForm.scss'
 
@@ -12,27 +12,31 @@ class NewTenantForm extends Component {
   constructor(props) {
     super(props);
 
-    this.handlePropertySearch = this.handlePropertySearch.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
     this.handleAddingNewPropertyManager = this.handleAddingNewPropertyManager.bind(this);
     this.handleAddingNewProperty = this.handleAddingNewProperty.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
       user: dummyUser,
-      propertySearch: '',
       addingNewProperty: false,
       addingNewPropertyManager: false
     }
   }
 
-  handlePropertySearch(event) {
-    this.setState({propertySearch: event.target.value})
-  }
-
-  handleAddingNewProperty() {
-    this.setState(prevState => ({addingNewPropertyManager: !prevState.addingNewPropertyManager}))
+  handleSearch(event) {
+    const { target } = event;
+    const { id } = target;
+    const { value } = target;
+    this.setState({
+      [id]: value
+    })
   }
 
   handleAddingNewPropertyManager() {
+    this.setState(prevState => ({addingNewPropertyManager: !prevState.addingNewPropertyManager}))
+  }
+
+  handleAddingNewProperty() {
     this.setState(prevState => ({addingNewProperty: !prevState.addingNewProperty}))
   }
 
@@ -44,15 +48,21 @@ class NewTenantForm extends Component {
     this.setState({
       [name]: value
     });
-    this.setState({ propertySearch: '' })
+    this.setState({ propertyName: '' })
+    this.setState({ propertyManagerName: '' })
   }
 
   render() {
-    const { user, propertySearch, propertySelected, addingNewProperty, addingNewPropertyManager } = this.state;
+    const { user, propertyName, propertySelected, propertyManagerSelected, addingNewProperty, addingNewPropertyManager, propertyManagerName } = this.state;
     const propertySearched = properties.filter(property => {
       const propertyNameAndAddress = `${property.name} ${property.address}`.toLowerCase();
-      return propertyNameAndAddress.includes(propertySearch)
+      return propertyNameAndAddress.includes(propertyName)
     })
+    const propertyManagerSearched = propertyManagers.filter(manager => {
+      const managerNameAndPhone = `${manager.name} ${manager.number}`.toLowerCase();
+      return managerNameAndPhone.includes(propertyManagerName)
+    })
+    const manager = propertyManagerSearched.find(manager => manager.id === propertyManagerSelected)
     const property = propertySearched.find(property => property.id === propertySelected);
     return (
       <div className="admin page">
@@ -60,6 +70,9 @@ class NewTenantForm extends Component {
         <div>
           <div className="width-wrapper">
             <h1 className="newTenantFormHeading">Add a New Tenant</h1>
+
+            {/* Add new tenant form section
+            ======================================= */}
             <h2 className="newTenantFormHeading">Tenant</h2>
             <fieldset>
               <Input
@@ -84,61 +97,123 @@ class NewTenantForm extends Component {
                 </select>
               </div>
             </fieldset>
+
+            {/* Add new property form
+            ======================================= */}
             <h2 className="newTenantFormHeading">Property</h2>
             <fieldset>
               <Input
                 id="propertyName"
-                placeholder="Property Name"
+                placeholder="Search Property Name"
                 label="Property"
                 type="text"
-                onChange={this.handlePropertySearch}
-                value={this.state.propertySearch} />
-              <div className="propertySearchResults">
-                {propertySearch !== '' && !addingNewProperty ?
-                  propertySearched.map(property => (
-                  <div key={property.id} className="propertyResult">
-                    <button
-                      id="propertySelect"
-                      type="button"
-                      name="propertySelected"
-                      onClick={this.handleChange}
-                      value={property.id}>
-                      {property.name} {property.address}
-                    </button>
-                  </div>
-                )) : null}
-                {!propertySelected && propertySearch === '' && (
-                  <div className="addNewLink" onClick={this.handleAddingNewProperty} role="presentation">
-                    <span className="addIcon"><Icon icon="plus"/></span>
-                    Add new Property
-                  </div>
-                )}
-                {addingNewProperty && (
+                onChange={this.handleSearch}
+                value={this.state.propertyName} />
+            </fieldset>
+            <div className="propertySearchResults">
+              {propertyName !== '' && (
+                propertySearched.map(property => (
+                <div key={property.id} className="propertyResult">
+                  <button
+                    id="propertySelect"
+                    type="button"
+                    name="propertySelected"
+                    onClick={this.handleChange}
+                    value={property.id}>
+                    {property.name} {property.address}
+                  </button>
+                </div>
+              )))}
+              {(!propertySelected &&
+                !addingNewProperty) && (
+                <div className="addNewLink" onClick={this.handleAddingNewProperty} role="presentation">
+                  <span className="addIcon"><Icon icon="plus"/></span>
+                  Add new Property
+                </div>
+              )}
+              {(addingNewProperty &&
+                !propertySelected) && (
+                <fieldset>
+                  <Input
+                    id="newPropertyName"
+                    placeholder="Property Name"
+                    label="Property Name"
+                    type="text" />
+                  <Input
+                    id="newPropertyAddress"
+                    placeholder="Property Address"
+                    label="Street Address"
+                    type="text" />
+                  <Input
+                    id="newPropertyCity"
+                    placeholder="City"
+                    label="City"
+                    type="text" />
+                  <Input
+                    id="newPropertyZip"
+                    placeholder="Zip"
+                    label="Zip"
+                    type="number" />
+                </fieldset>
+              )}
+              {propertySelected ?
+                <div className="newTenantProperty">
                   <div>
-                    <Input
-                      id="newPropertyName"
-                      placeholder="Property Name"
-                      label="Property Name"
-                      type="text" />
-                    <Input
-                      id="newPropertyAddress"
-                      placeholder="Property Address"
-                      label="Street Address"
-                      type="text" />
-                    <Input
-                      id="newPropertyCity"
-                      placeholder="City"
-                      label="City"
-                      type="text" />
-                    <Input
-                      id="newPropertyZip"
-                      placeholder="Zip"
-                      label="Zip"
-                      type="number" />
+                    <h3>{property.name.length > 0 ? property.name : null}</h3>
+                    <p>{property.address.length > 0 ? property.address : null}</p>
                   </div>
-                )}
-                {(addingNewPropertyManager && propertySelected) && (
+                </div>
+              : null}
+              {propertyManagerSelected && (
+                <div className="newTenantProperty">
                   <div>
+                    <h3>{manager.name.length > 0 && manager.name}</h3>
+                    <p>{manager.number.length > 0 && manager.number}</p>
+                  </div>
+                </div>
+              )}
+              {propertySelected && (
+                <div className="addNewLink" onClick={this.handleAddingNewPropertyManager} role="presentation">
+                  <span className="addIcon"><Icon icon="plus"/></span>
+                  Add new manager
+                </div>
+              )}
+            </div>
+
+            {/* Add new property manager form
+            ======================================= */}
+            {(addingNewPropertyManager &&
+              propertySelected &&
+              !propertyManagerSelected) && (
+              <div>
+                <h2 className="newTenantFormHeading">Property Manager</h2>
+                <div className="propertySearchResults">
+                  <fieldset>
+                    <Input
+                      id="propertyManagerName"
+                      placeholder="Search Manager Name"
+                      label="Property Manager"
+                      type="text"
+                      onChange={this.handleSearch}
+                      value={this.state.propertyManagerName} />
+                  </fieldset>
+                  {propertyManagerName !== '' ?
+                    propertyManagerSearched.map(manager => (
+                    <div key={manager.id} className="propertyResult">
+                      <button
+                        id="propertyManagerSelect"
+                        type="button"
+                        name="propertyManagerSelected"
+                        onClick={this.handleChange}
+                        value={manager.id}>
+                        {manager.name} {manager.number}
+                      </button>
+                    </div>
+                  )) : null}
+                </div>
+                {(propertyManagerName === '' &&
+                  !propertyManagerSelected) && (
+                  <fieldset>
                     <Input
                       id="newPropertyManagerName"
                       placeholder="Name"
@@ -154,22 +229,10 @@ class NewTenantForm extends Component {
                       placeholder="email@email.com"
                       label="Email"
                       type="text" />
-                  </div>
+                  </fieldset>
                 )}
-                {propertySelected ?
-                  <div className="newTenantProperty">
-                    <div>
-                      <h3>{property.name.length > 0 ? property.name : null}</h3>
-                      <p>{property.address.length > 0 ? property.address : null}</p>
-                    </div>
-                    <div className="addNewLink" onClick={this.handleAddingNewPropertyManager} role="presentation">
-                      <span className="addIcon"><Icon icon="plus"/></span>
-                      Add new manager
-                    </div>
-                  </div>
-                : null}
               </div>
-            </fieldset>
+            )}
           </div>
         </div>
       </div>
