fd21ee2fe4222574ee21bec8f475b4e33c4d09b9
Bobby Martin <robert.martin.619@gmail.com>
2018-08-28 12:16:53 -0700

    Add base funcitonality

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -16,51 +16,77 @@ class Search extends Component {
         id: 1,
         name: 'Burt Macklin',
         address: 'Quanico FBI HQ'
+      },
+      {
+        id: 2,
+        name: 'Janet Snakehole',
+        address: 'The Snakehole Lounge'
       }
     ]
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleSelection = this.handleSelection.bind(this);
+    this.handleNewSearch = this.handleNewSearch.bind(this);
 
     this.state = {}
+  }
+
+  handleSelection(event) {
+    this.setState({
+      [event.target.id]: event.target.textContent
+    });
+    this.setState({ searchText: event.target.textContent })
+  }
+
+  handleNewSearch(event) {
+    if(this.state.searchResult && this.state.searchText) {
+      event.target.classList.add('active')
+    }
+    this.setState({ searchText: '' })
   }
 
   handleChange(event) {
     const { target } = event;
     const { id } = target;
     const { value } = target;
-    if(value) {
-      this.setState({
-        [id]: value
-      });
-    } else {
-      this.setState({
-        [id]: target.textContent
-      });
-    }
 
+    this.setState({ searchResult: ''})
+
+    this.setState({
+      [id]: value
+    });
   }
 
   render() {
     const { testData } = this;
     // const { searchTerms } = this.props;
-    const { searchText } = this.state;
+    const { searchText, searchResult } = this.state;
     const filterSearch = testData.filter(data => {
       const nameAndAddress = `${data.name} ${data.address}`.toLowerCase();
       return nameAndAddress.includes(searchText)
     })
     return (
       <div className="searchContainer">
-        <input type="text" className={!searchText ? "searchBarFirst active" : "searchBarFirst"} onChange={this.handleChange} id="searchText" />
+        <input
+          type="text"
+          id="searchText"
+          className="searchBarFirst"
+          onClick={this.handleNewSearch}
+          onChange={this.handleChange}
+          value={searchText} />
+        {searchResult && (
+          <div className="pastSearch">
+            {searchResult}
+          </div>
+        )}
         {searchText && (
-          <div>
-            <input type="text" className="searchBarSecond" onChange={this.handleChange} id="newSearchText" />
-            {/* <div className="text">test</div> */}
+          <div className="searchResultsContainer">
             {filterSearch.map(term =>
               <div
                 className="results"
                 key={term.id}
                 id="searchResult"
-                onClick={this.handleChange}
+                onClick={this.handleSelection}
                 role="presentation">
                 {term.name}
               </div>
