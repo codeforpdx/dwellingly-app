f7becb18662f78ce5dd52e5e6701ac3473309331
Bobby Martin <robert.martin.619@gmail.com>
2018-08-28 10:25:48 -0700

    Add css and active classes for search bar component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
+import './Search.scss';
+
 class Search extends Component {
   constructor(props) {
     super(props)
     this.testData = [
       {
+        id: 0,
         name: 'Paul Flart',
         address: 'The Mall Security'
       },
       {
+        id: 1,
         name: 'Burt Macklin',
         address: 'Quanico FBI HQ'
       }
@@ -24,28 +28,42 @@ class Search extends Component {
     const { target } = event;
     const { id } = target;
     const { value } = target;
+    if(value) {
+      this.setState({
+        [id]: value
+      });
+    } else {
+      this.setState({
+        [id]: target.textContent
+      });
+    }
 
-    this.setState({
-      [id]: value
-    });
   }
 
   render() {
     const { testData } = this;
     // const { searchTerms } = this.props;
     const { searchText } = this.state;
-    const filterSearch = testData.filter(data => data.name.includes(searchText))
+    const filterSearch = testData.filter(data => {
+      const nameAndAddress = `${data.name} ${data.address}`.toLowerCase();
+      return nameAndAddress.includes(searchText)
+    })
     return (
       <div className="searchContainer">
-        {!searchText && (
-          <input type="text" className="searchBar" onChange={this.handleChange} id="searchText" />
-        )}
+        <input type="text" className={!searchText ? "searchBarFirst active" : "searchBarFirst"} onChange={this.handleChange} id="searchText" />
         {searchText && (
           <div>
-            <input type="text" className="searchBar" onChange={this.handleChange} id="newSearchText" />
+            <input type="text" className="searchBarSecond" onChange={this.handleChange} id="newSearchText" />
             {/* <div className="text">test</div> */}
             {filterSearch.map(term =>
-              <div className="results" id="searchResults" onClick={this.handleChange} role="presentation">{term.name}</div>
+              <div
+                className="results"
+                key={term.id}
+                id="searchResult"
+                onClick={this.handleChange}
+                role="presentation">
+                {term.name}
+              </div>
             )}
           </div>
         )}
@@ -55,9 +73,10 @@ class Search extends Component {
 }
 
 // Search.propTypes = {
-//   searchTerms: PropTypes.shape({
-//     test: PropTypes.string
-//   }).isRequired
+//   // searchTerms: PropTypes.shape({
+//   //   test: PropTypes.string
+//   // }).isRequired
 // }
+
 
 export default Search
