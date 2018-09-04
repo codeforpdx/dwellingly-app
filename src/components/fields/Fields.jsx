99d38f668a3e70fd70e2fbad6c18707fbee01f4d
Bobby Martin <robert.martin.619@gmail.com>
2018-09-04 12:43:54 -0700

    Add working input types array

--- src/components/fields/Fields.jsx
+++ src/components/fields/Fields.jsx
@@ -3,19 +3,34 @@ import PropTypes from 'prop-types';
 import Input from '../input/Input';
 
 class Fields extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    }
+  }
+
   render() {
-    const { label } = this.props;
+    const { label, inputTypes } = this.props;
+
     return (
       <fieldset>
-        <Input
-          label={label} />
+        {inputTypes.map((inputType) =>
+          <Input
+            key={inputType}
+            label={label}
+            type={inputType}  />
+        )}
       </fieldset>
     )
   }
 }
 
 Fields.propTypes = {
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  inputTypes: PropTypes.arrayOf(
+    PropTypes.string
+  ).isRequired
 }
 
 export default Fields
