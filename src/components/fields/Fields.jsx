85cf164c5e27c80ad536d39639a9c76ff5981a7c
Bobby Martin <robert.martin.619@gmail.com>
2018-09-04 14:03:15 -0700

    Make Fields component stateless

--- src/components/fields/Fields.jsx
+++ src/components/fields/Fields.jsx
@@ -1,33 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Input from '../input/Input';
 
-class Fields extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-    }
-  }
-
-  render() {
-    const { id, label, inputTypes, name, onChange } = this.props;
-
-    return (
-      <fieldset>
-        {inputTypes.map((inputType, index) =>
-          <Input
-            key={id[index]}
-            id={`${id[index]}-${index}`}
-            label={label[index]}
-            name={name[index]}
-            onChange={onChange}
-            type={inputType}  />
-        )}
-      </fieldset>
-    )
-  }
-}
+const Fields = ({ id, label, inputTypes, name, onChange }) => (
+  <fieldset>
+    {inputTypes.map((inputType, index) =>
+      <Input
+        key={id[index]}
+        id={`${id[index]}-${index}`}
+        label={label[index]}
+        name={name[index]}
+        onChange={onChange}
+        type={inputType}  />
+    )}
+  </fieldset>
+)
 
 Fields.propTypes = {
   id: PropTypes.arrayOf(
