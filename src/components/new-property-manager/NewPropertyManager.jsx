44a35e5e1dd86cba5a1d9829c34a91c419ed58cf
Bobby Martin <robert.martin.619@gmail.com>
2018-09-05 14:57:39 -0700

    Add onChange to fieldset and remove onChange from inputs

--- src/components/new-property-manager/NewPropertyManager.jsx
+++ src/components/new-property-manager/NewPropertyManager.jsx
@@ -3,35 +3,31 @@ import PropTypes from 'prop-types';
 import Input from '../input/Input';
 
 const NewPropertyManager = ({ onChange }) => (
-  <fieldset>
+  <fieldset onChange={onChange}>
     <Input
       type="text"
       id="newPropertyManagerFirstName"
       name="newPropertyManagerFirstName"
       label="First Name"
-      placeholder="First Name"
-      onChange={onChange} />
+      placeholder="First Name" />
     <Input
       type="text"
       id="newPropertyManagerLastName"
       name="newPropertyManagerLastName"
       label="Last Name"
-      placeholder="Last Name"
-      onChange={onChange} />
+      placeholder="Last Name" />
     <Input
       type="tel"
       id="newPropertyManagerPhone"
       name="newPropertyManagerPhone"
       label="Phone"
-      placeholder="ex. 503-555-1234"
-      onChange={onChange} />
+      placeholder="ex. 503-555-1234" />
     <Input
       type="email"
       id="newPropertyManagerEmail"
       name="newPropertyManagerEmail"
       label="Email"
-      placeholder="email@email.com"
-      onChange={onChange} />
+      placeholder="email@email.com" />
   </fieldset>
 );
 
