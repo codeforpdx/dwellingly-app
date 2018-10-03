9573aa3b89fb7c96827722697d9bfadb357f6dc5
Bobby Martin <robert.martin.619@gmail.com>
2018-10-03 16:58:31 -0700

    Add multi select component

---
+++ src/components/multi-select/MultiSelect.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Search from '../search/Search';
+
+class MultiSelect extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+
+    this.handleUpdatingSelected = this.handleUpdatingSelected.bind(this);
+  }
+
+  handleUpdatingSelected(data) {
+    this.setState(prevState => ({selected: [...prevState.selected, data]}))
+  }
+
+  render() {
+    const { placeholder, data, onSearchSelection } = this.props;
+    return (
+      <div>
+        <Search
+          onSearchSelection={onSearchSelection}
+          onUpdatingSelected={this.handleUpdatingSelected}
+          placeholder={placeholder}
+          searchData={data}
+          multiple />
+      </div>
+    )
+  }
+}
+
+MultiSelect.propTypes = {
+  placeholder: PropTypes.string,
+  data: PropTypes.shape([]).isRequired,
+  onSearchSelection: PropTypes.func.isRequired
+}
+MultiSelect.defaultProps = {
+  placeholder: undefined
+}
+
+export default MultiSelect;
