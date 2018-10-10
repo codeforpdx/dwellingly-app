63a566014eddeb8370c76cf59fba688e0f58d13d
Bobby Martin <robert.martin.619@gmail.com>
2018-10-10 14:39:40 -0700

    Add new input fields to match designs

--- src/components/new-property/NewProperty.jsx
+++ src/components/new-property/NewProperty.jsx
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 import Input from '../input/Input';
 import Search from '../search/Search';
 
-const NewProperty = ({ onChange, onSave, data, placeholder }) => (
-  <fieldset onChange={onChange}>
+const NewProperty = ({ id, onChange, onSave, onSearch, data, placeholder }) => (
+  <fieldset id={id} onChange={onChange}>
     <Input
       type="text"
       id="newPropertyName"
       name="newPropertyName"
-      label="Property Name"
+      label="Name"
       placeholder="Property Name" />
     <Input
       type="text"
       id="newPropertyAddress"
       name="newPropertyAddress"
-      label="Property Address"
+      label="Address"
       placeholder="Property Address" />
     <Input
       type="text"
@@ -24,26 +24,41 @@ const NewProperty = ({ onChange, onSave, data, placeholder }) => (
       label="City"
       placeholder="City" />
     <Input
+      type="text"
+      id="newPropertyState"
+      name="newPropertyState"
+      label="State"
+      placeholder="State" />
+    <Input
       type="number"
       id="newPropertyZip"
       name="newPropertyZip"
       label="Zip"
       placeholder="Zip" />
+    <Input
+      type="number"
+      id="newPropertyUnits"
+      name="newPropertyUnits"
+      label="Units"
+      placeholder="Units" />
     <Search
       searchData={data}
-      placeholder={placeholder} />
+      placeholder={placeholder}
+      onSearchSelection={onSearch}
+      filterSubset={["firstName", "lastName"]} />
     <button
       type="submit"
       onClick={onSave}
-      className="btn"
-      disabled={onChange}>ADD NEW PROPERTY</button>
+      className="btn">ADD NEW PROPERTY</button>
   </fieldset>
 );
 
 NewProperty.propTypes = {
+  id: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
   data: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   onChange: PropTypes.func.isRequired,
+  onSearch: PropTypes.func.isRequired,
   onSave: PropTypes.func.isRequired
 }
 
