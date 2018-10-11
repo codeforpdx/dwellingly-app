2b6d89bb1d49416058457ac94110df6b8bd5bbdc
Bobby Martin <robert.martin.619@gmail.com>
2018-10-11 15:54:06 -0700

    Add new edit fields for subtext

--- src/components/forms/EmergencyNumberEdit.jsx
+++ src/components/forms/EmergencyNumberEdit.jsx
@@ -7,21 +7,51 @@ const EmergencyNumberEdit = (props) => {
     props.onCancellingEditEmergencyNumber();
   }
   return (
-    <div id={props.id}>
-      <input name="contact" placeholder={props.contact} onChange={props.onEditingEmergencyNumber} type="text" />
-      <input name="phoneNumberOne" placeholder={props.numberOne} onChange={props.onEditingEmergencyNumber} type="text" />
-      {props.numberTwo !== null && props.numberTwo !== ''
-        && (
-          <input name="phoneNumberTwo" placeholder={props.numberTwo} onChange={props.onEditingEmergencyNumber} type="text" />
-        )
-      }
-      {props.numberThree !== null && props.numberThree !== ''
-        && (
-          <input name="phoneNumberThree" placeholder={props.numberThree} onChange={props.onEditingEmergencyNumber} type="text" />
-        )
-      }
-      <button type="submit" className="btn" onClick={() => onSubmit()}>Save</button>
-      <button type="button" className="btn btn--danger" onClick={props.onCancellingEditEmergencyNumber}>Cancel</button>
+    <div id={props.id} className="emergencyNumberEditContainer">
+      <input name="contact" placeholder={props.contact} onChange={props.onEditingEmergencyText} type="text" />
+      <div className="emergencyNumberEditInputs">
+        <fieldset name="phoneNumberOne" onChange={props.onEditingEmergencyNumber}>
+          <input
+            name="subtext"
+            type="text"
+            placeholder={props.numberOne.subtext !== '' ? props.numberOne.subtext : 'Enter number info'} />
+          <input
+            name="number"
+            placeholder={props.numberOne.number}
+            type="text" />
+        </fieldset>
+        {props.numberTwo !== null && props.numberTwo !== ''
+          && (
+            <div name="phoneNumberTwo">
+              <input
+              name="subtext"
+              placeholder={props.numberTwo.subtext !== '' ? props.numberTwo.subtext : 'Enter number info'}
+              onChange={props.onEditingEmergencyNumber} />
+              <input
+                name="number"
+                placeholder={props.numberTwo.number}
+                onChange={props.onEditingEmergencyNumber}
+                type="text" />
+            </div>
+          )
+        }
+        {props.numberThree !== null && props.numberThree !== ''
+          && (
+            <input
+              name="phoneNumberThree"
+              placeholder={props.numberThree.number}
+              onChange={props.onEditingEmergencyNumber}
+              type="text" />
+          )
+        }
+      </div>
+      <div className="btn--group">
+        <div className="btn--group-inner">
+          <button type="submit" className="btn" onClick={() => onSubmit()}>Save</button>
+          <button type="button" className="btn btn--disabled">Delete</button>
+        </div>
+        <button type="button" className="btn btn--danger" onClick={props.onCancellingEditEmergencyNumber}>Cancel</button>
+      </div>
     </div>
   )
 };
@@ -29,9 +59,22 @@ const EmergencyNumberEdit = (props) => {
 EmergencyNumberEdit.propTypes = {
   id: PropTypes.string.isRequired,
   contact: PropTypes.string.isRequired,
-  numberOne: PropTypes.string.isRequired,
-  numberTwo: PropTypes.string,
-  numberThree: PropTypes.string,
+  numberOne: PropTypes.shape({
+    ext: PropTypes.string,
+    number: PropTypes.string,
+    subtext: PropTypes.string
+  }).isRequired,
+  numberTwo: PropTypes.shape({
+    ext: PropTypes.string,
+    number: PropTypes.string,
+    subtext: PropTypes.string
+  }),
+  numberThree: PropTypes.shape({
+    ext: PropTypes.string,
+    number: PropTypes.string,
+    subtext: PropTypes.string
+  }),
+  onEditingEmergencyText: PropTypes.func.isRequired,
   onCancellingEditEmergencyNumber: PropTypes.func.isRequired,
   onEditingEmergencyNumber: PropTypes.func.isRequired,
   onSavingNewEmergencyNumber: PropTypes.func.isRequired
