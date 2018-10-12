377b9d041df0e04cf47081ce71c827b821022ba0
Bobby Martin <robert.martin.619@gmail.com>
2018-10-12 10:19:33 -0700

    Add delete method for emergency numbers

--- src/dux/emergencyNumbers.js
+++ src/dux/emergencyNumbers.js
@@ -5,6 +5,7 @@ export const FETCHING_EMERGENCY_NUMBERS_SUCCEEDED = 'emergency/FETCHING_EMERGENC
 export const FETCHING_EMERGENCY_NUMBERS_ERROR = 'emergency/FETCHING_EMERGENCY_NUMBERS_ERROR';
 export const EDIT_EMERGENCY_NUMBER = 'emergency/EDIT_EMERGENCY_NUMBER';
 export const CREATE_EMERGENCY_NUMBER = 'emergency/CREATE_EMERGENCY_NUMBER';
+export const DELETE_EMERGENCY_NUMBER = 'emergency/DELETE_EMERGENCY_NUMBER';
 export const ARCHIVE_EMERGENCY_NUMBER = 'emergency/ARCHIVE_EMERGENCY_NUMBER';
 
 // Initial State
@@ -39,9 +40,10 @@ export default (state = initialState, action) => {
       numberArrCopy[targetObj] = {
         id: action.id,
         contact: action.contact,
-        phoneNumberOne: action.phoneNumberOne,
-        phoneNumberTwo: action.phoneNumberTwo,
-        phoneNumberThree: action.phoneNumberThree,
+        subtext: action.subtext,
+        phoneNumberOne: { ...action.phoneNumberOne },
+        phoneNumberTwo: { ...action.phoneNumberTwo },
+        phoneNumberThree: { ...action.phoneNumberThree },
       };
       return {
         ...state,
@@ -52,6 +54,13 @@ export default (state = initialState, action) => {
       return {
         ...state
       }
+    case DELETE_EMERGENCY_NUMBER: {
+      const numberId = action.id
+      return {
+        ...state,
+        numbers: state.numbers.filter(({ id }) => id !== numberId)
+      }
+    }
     case ARCHIVE_EMERGENCY_NUMBER: {
       const numberId = action.id
       return {
@@ -77,7 +86,7 @@ export const getEmergencyNumbersCollection = (data) => (dispatch) => {
     isFetchingDataFromFirebase: false
   })
 }
-
+// Edit Emergency Number
 export const editEmergencyNumber = (data) => (dispatch) => {
   dispatch({
     type: EDIT_EMERGENCY_NUMBER,
@@ -88,7 +97,7 @@ export const editEmergencyNumber = (data) => (dispatch) => {
     phoneNumberThree: data.phoneNumberThree
   })
 }
-
+// Create Emergency Number
 export const createEmergencyNumber = (data) => (dispatch) => {
   dispatch({
     type: CREATE_EMERGENCY_NUMBER,
@@ -99,7 +108,14 @@ export const createEmergencyNumber = (data) => (dispatch) => {
     phoneNumberThree: data.phoneNumberThree
   })
 }
-
+// Delete Emergency Number
+export const deleteEmergencyNumber = (data) => (dispatch) => {
+  dispatch({
+    type: DELETE_EMERGENCY_NUMBER,
+    id: data
+  })
+}
+// Archive Emergency Number
 export const archiveEmergencyNumber = (data) => (dispatch) => {
   dispatch({
     type: ARCHIVE_EMERGENCY_NUMBER,
@@ -157,6 +173,25 @@ export const editingEmergencyNumber = data => async dispatch => {
   }
 }
 
+// Delete Emergency Number
+export const onDeletingEmergencyNumber = async data => {
+  const response = await fetch(`${SETTINGS.FIREBASE_API}${ROUTES.EMERGENCY_NUMBERS}/${data}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application.json'
+    },
+    body: JSON.stringify(data)
+  })
+  return response;
+}
+export const deletingEmergencyNumber = data => async dispatch => {
+  try {
+    await onDeletingEmergencyNumber(data);
+    dispatch(deleteEmergencyNumber(data));
+  } catch(e) {
+    console.log(e);
+  }
+}
 // Archive Emergency Number
 export const onArchivingEmergencyNumber = async data => {
   const response = await fetch(`${SETTINGS.FIREBASE_API}${ROUTES.EMERGENCY_NUMBERS}/${data.id}`, {
