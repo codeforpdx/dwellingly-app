e302dee600751e34eb9faeee1d70de7698a3c586
Krystal Foster <kmfoster777@gmail.com>
2019-01-23 16:55:22 -0800

    arrange dummy data

--- src/pages/property-dashboard/PropertyDashboard.jsx
+++ src/pages/property-dashboard/PropertyDashboard.jsx
@@ -15,11 +15,47 @@ const PropertyDashboard = ({ intl }) => (
         </div>
       )}
     </Header>
-    <section className="main width-wrapper">
+    <section className="property-dashboard-header">
       <div className="add-new">
-        <p className="property-header">PROPERTIES</p>
-        <button type="button" className="btn btn--lrg">+ ADD NEW</button>
+        <p className="property-header">PROPERTIES<button type="button" className="btn btn--lrg add-new-btn">+ ADD NEW</button></p>
       </div>
+      <input
+        type="text"
+        className="property-search"
+        placeholder="search" />
+    </section>
+    <section className="property-dashboard-header property-table">
+      <div className="gray-bar">
+        <button type="button" className="btn archive-btn">ARCHIVE</button>
+      </div>
+      <div className="table-header">
+      <table>
+        <tr className="table-list">
+          <td><input type="checkbox" />Name</td>
+          <td>Property Manager</td>
+          <td>Address</td>
+          <td>Tenants</td>
+        </tr>
+      </table>
+      </div>
+      <table>
+        <tr className="table-list">
+          <td><input type="checkbox" />name</td>
+          <td>property manager</td>
+          <td>address</td>
+          <td>tenants</td>
+        </tr>
+      </table>
+      <hr/>
+      <table>
+        <tr className="table-list">
+          <td><input type="checkbox" />name</td>
+          <td>property manager</td>
+          <td>address</td>
+          <td>tenants</td>
+        </tr>
+      </table>
+      <hr/>
     </section>
   </div>
 );
