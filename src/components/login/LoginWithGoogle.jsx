22fbdc647055ad103c70508aa38fb13432c20014
Bobby Martin <robert.martin.619@gmail.com>
2019-02-05 15:08:31 -0800

    Add icon to login button

--- src/components/login/LoginWithGoogle.jsx
+++ src/components/login/LoginWithGoogle.jsx
@@ -2,20 +2,21 @@ import React from 'react';
 import { intlShape, injectIntl } from 'react-intl';
 import { auth } from '../../firebase';
 import { LOGIN } from '../../translations/messages';
+import Icon from '../icon/Icon';
 
-const LoginWithGoogle = ({intl}) => (
+const LoginWithGoogle = ({ intl }) => (
   <div className="form-meta">
     <button
       type="button"
       className="btn btn--lg btn--strong"
       onClick={auth.doSignInWithGoogle}>
-        {intl.formatMessage(LOGIN.LABEL_GOOGLE_LOGIN)}
+      <Icon icon="google" /> {intl.formatMessage(LOGIN.LABEL_GOOGLE_LOGIN)}
     </button>
   </div>
 );
 
 LoginWithGoogle.propTypes = {
-  intl: intlShape.isRequired,
+  intl: intlShape.isRequired
 };
 
 export default injectIntl(LoginWithGoogle);
