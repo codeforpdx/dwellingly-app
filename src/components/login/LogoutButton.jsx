199aaa2b362c56f5096e748c845f608f0aef1419
Bobby Martin <robert.martin.619@gmail.com>
2019-02-07 12:02:26 -0800

    Add transparent class to logout button

--- src/components/login/LogoutButton.jsx
+++ src/components/login/LogoutButton.jsx
@@ -3,17 +3,17 @@ import { intlShape, injectIntl } from 'react-intl';
 import { auth } from '../../firebase';
 import { LOGIN } from '../../translations/messages';
 
-const Logout = ({intl}) => (
-    <button
+const Logout = ({ intl }) => (
+  <button
     type="button"
-    className="btn btn--lg btn--strong"
+    className="btn btn--lg btn--transparent btn--strong"
     onClick={auth.doSignOut}>
-      {intl.formatMessage(LOGIN.LABEL_LOGOUT)}
+    {intl.formatMessage(LOGIN.LABEL_LOGOUT)}
   </button>
 );
 
 Logout.propTypes = {
-  intl: intlShape.isRequired,
+  intl: intlShape.isRequired
 };
 
 export default injectIntl(Logout);
