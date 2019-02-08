7282d5311fd2e2428260199f08fe1aa1e390800b
Krystal Foster <kmfoster777@gmail.com>
2019-02-08 11:54:16 -0800

    implement initial data-grid and start sass

--- src/pages/property-dashboard/PropertyDashboard.jsx
+++ src/pages/property-dashboard/PropertyDashboard.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import { intlShape, injectIntl } from 'react-intl';
 import { COMMON } from '../../translations/messages';
 import Header from '../../components/header/Header';
@@ -11,11 +14,26 @@ import Icon from '../../components/icon/Icon';
 import { getProperties } from '../../dux/properties';
 import { getPropertyManagers } from '../../dux/propertyManagers';
 
+
 class PropertyDashboard extends Component {
   constructor(props) {
     super(props);
-    this.getPropertyData = this.getPropertyData.bind(this);
-    this.getTableRow = this.getTableRow.bind(this);
+    this.state = {
+      defaultColDef: {
+        filter: true // set filtering on for all cols
+      },
+      columnDefs: [
+        {headerName: "Name", field: "name", filter: "agTextColumnFilter", sortable: true, checkboxSelection: true, unSortIcon: true},
+        {headerName: "Property Manager", field: "city", cellClass: 'my-class', sortable: true, unSortIcon: true },
+        {headerName: "Address", field: "addressOne", sortable: true, unSortIcon: true },
+        {headerName: "Tenants", field: "numberOfUnits", sortable: true, unSortIcon: true },
+        {headerName: "Date Added", field: "state", sortable: true, unSortIcon: true }
+      ]
+    }
+    // this.jsFunction = this.jsFunction.bind(this);
+    // this.doesExternalFilterPass = this.doesExternalFilterPass.bind(this);
+    // this.getPropertyData = this.getPropertyData.bind(this);
+    // this.getTableRow = this.getTableRow.bind(this);
   }
 
   componentWillMount() {
@@ -25,26 +43,38 @@ class PropertyDashboard extends Component {
     dispatch(getPropertyManagers());
   }
 
-  getPropertyData() {  
-    // Do we need to update the current data query to include date the property (not lease?) is saved to db, as well as number of residents? Not all properties will have leases. 
-    // Are we missing a foreign key to link managers to properties?  
-    // If we are thinking of using server-side pagination, I'm not sure we should be mapping through multiple objects in the front-end to find those fields 
-    const properties = this.props.properties.properties.length > 0 ? this.props.properties.properties : [];
-    return properties.map(property => (this.getTableRow(property)))
-  }
+  // getPropertyData() {  
+  //   // Do we need to update the current data query to include date the property (not lease?) is saved to db, as well as number of residents? Not all properties will have leases. 
+  //   // Are we missing a foreign key to link managers to properties?  
+  //   // If we are thinking of using server-side pagination, I'm not sure we should be mapping through multiple objects in the front-end to find those fields 
+  //   const properties = this.props.properties.properties.length > 0 ? this.props.properties.properties : [];
+  //   return properties.map(property => (this.getTableRow(property)))
+  // }
   
-  getTableRow (property) {
-    console.log(this.props);
-    return (
-      <tr>
-        <td><input type="checkbox" />{property.name}</td>
-        <td>{property.city}</td>
-        <td>{property.addressOne} <br/> {property.addressTwo}</td>
-        <td>{property.numberOfUnits}</td>
-        <td>{property.state}</td>
-      </tr>
-    )
-  }
+  // getTableRow (property) {
+  //   console.log(this.props);
+  //   return (
+  //     <tr>
+  //       <td><input type="checkbox" />{property.name}</td>
+  //       <td>{property.city}</td>
+  //       <td>{property.addressOne} <br/> {property.addressTwo}</td>
+  //       <td>{property.numberOfUnits}</td>
+  //       <td>{property.state}</td>
+  //     </tr>
+  //   )
+  // }
+  
+  // jsFunction(filterVal) {
+  //   this.filterVal= filterVal;
+  //   this.state.api.onFilterChanged(); 
+  // }
+  // 
+  // doesExternalFilterPass(node) {
+  //     if (this.filterVal) {
+  //         return node.data.this.state.columnDefs === this.filterVal;
+  //      }
+  //     return true; // default case return all rows
+  //    }
 
   render() {
     const { intl } = this.props;
@@ -67,10 +97,11 @@ class PropertyDashboard extends Component {
           <form action="" className="search" onSubmit={this.handleSearch}>
             <div className="icon-wrapper">
               <Icon icon="search" />
-              <input
+              <input      
+                // onChange={this.jsFunction(this.filterVal)}
                 type="text"
                 className="property-search"
-                placeholder="search properties by name, property, or status . . ." />
+                placeholder="Search properties by name, property, or status . . ." />
             </div>
           </form>
           <div>ARCHIVED:<button className="switch__btn" type="button">hide</button></div>
@@ -81,22 +112,30 @@ class PropertyDashboard extends Component {
           <div className="ghost-dot"> .</div>
           <button type="button" className="btn archive-btn"><Icon icon="archive"/> ARCHIVE</button>
         </div>
-        <div className="table-wrapper">
-          <table>
-            <tr>
-              <th><input type="checkbox" /> Name</th>
-              <th>Property Manager</th>
-              <th>Address</th>
-              <th>Tenants</th>
-              <th>Date Added</th>
-            </tr>
-            {this.getPropertyData()}
-          </table>
+        {
+        // <div className="table-wrapper">
+        //   <table>
+        //     <tr>
+        //       <th><input type="checkbox" /> Name</th>
+        //       <th>Property Manager</th>
+        //       <th>Address</th>
+        //       <th>Tenants</th>
+        //       <th>Date Added</th>
+        //     </tr>
+        //     {this.getPropertyData()}
+        //   </table>
+        // </div>
+      }
+        <div className="ag-grid-wrapper">
+          <AgGridReact
+            defaultColDef={this.state.defaultColDef}
+            columnDefs={this.state.columnDefs}
+            rowData={this.props.properties.properties.length > 0 ? this.props.properties.properties : []}
+            pagination
+            paginationPageSize={10}
+          />    
         </div>
       </section>
-      <div className="next-button">
-        <button type="button" className="btn next-btn">NEXT</button>
-      </div>
     </div>
   )}
 };
