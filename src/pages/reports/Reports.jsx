f4448a751c4b1c3795ad5bd2e5670e327085dab9
ccook987 <ccook987@gmail.com>
2019-02-13 12:38:35 -0800

    Add additional rows with icons and placeholder text.

--- src/pages/reports/Reports.jsx
+++ src/pages/reports/Reports.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../../components/header/Header';
+import Icon from '../../components/icon/Icon';
 import Navigation from '../../components/navigation/Navigation';
 import './Reports.scss';
 
@@ -16,27 +17,125 @@ render() {
 
   return(
     <div className="admin page">
-      <Header>
-        {() => (
+        <div>
+          <Header>
+            {() => (
+              <div>
+                <Navigation />
+                <Header.Label
+                  label="JOIN Messenger Administration"
+                  type="basic"
+                  />
+              </div>
+            )}
+          </Header>
           <div>
-            <Navigation />
-            <Header.Label
-              label="JOIN Messenger Administration"
-              type="basic"
-              />
+            <div className="width-wrapper">
+              <div className="name-header">
+                <h2 className="property-manager-name">
+                  Reports
+                </h2>
+                </div>
+                <section className="contact-details-section">
+                  <div >
+                    <table className="accordion__table">
+                      <tr>
+                        <td>
+                          <Icon icon="heart" />
+                        </td>
+                        <td id="contact-table-cell">
+                          25
+                        </td>
+                        <td>
+                          Compliments
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Icon icon="comment" />
+                        </td>
+                        <td>
+                          12
+                        </td>
+                        <td>
+                          Unpaid Rent
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Icon icon="comment" />
+                        </td>
+                        <td>
+                          3
+                        </td>
+                        <td>
+                          Other Financial
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Icon icon="comment" />
+                        </td>
+                        <td>
+                          0
+                        </td>
+                        <td>
+                          Unauthorized Guests
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Icon icon="comment" />
+                        </td>
+                        <td>
+                          1
+                        </td>
+                        <td>
+                          Unit Conditions
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Icon icon="comment" />
+                        </td>
+                        <td>
+                          0
+                        </td>
+                        <td>
+                          Rule Violations
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Icon icon="comment" />
+                        </td>
+                        <td>
+                          2
+                        </td>
+                        <td>
+                          Noise
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Icon icon="comment" />
+                        </td>
+                        <td>
+                          1
+                        </td>
+                        <td>
+                          Property Damage
+                        </td>
+                      </tr>
+                    </table>
+                  </div>
+                </section>
+              </div>
+            </div>
           </div>
-        )}
-      </Header>
-      <div>
-        <div className="width-wrapper">
-          <h2 className="admin--header align--left">
-            Reports
-          </h2>
-        </div>
       </div>
-    </div>
-  )
-}
+    );
+  }
 }
 
 export default Reports;
