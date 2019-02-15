443ce841c4b5bbb5e2f02009272fa10402baaebc
ccook987 <ccook987@gmail.com>
2019-02-15 10:15:33 -0800

    Update styles on page title.

--- src/pages/reports/Reports.jsx
+++ src/pages/reports/Reports.jsx
@@ -26,10 +26,10 @@ class Reports extends Component {
     }));
   }
 
-render() {
+  render() {
 
-  return(
-    <div className="admin page">
+    return(
+      <div className="admin page">
         <div>
           <Header>
             {() => (
@@ -45,122 +45,122 @@ render() {
           <div>
             <div className="width-wrapper">
               <div className="name-header-container">
-                <h2 className="admin--header">
+                <h2 className="reports-title">
                   Reports
                 </h2>
-                </div>
-                <section className="contact-details-section">
-                  <div>
-                    <div className="table-header-container">
-                      <div className="table-header-left">
-                        <p className="table-header-text">OPENED FROM</p>
-                        <select className="table-header-select">
-                          <option>01/01/2018 - 12/31/2018</option>
-                        </select>
-                      </div>
-                      <div className="table-header-right">
-                        <p className="table-header-text">Average time to close: </p>
-                        <p className="table-header-text">2 days, 4 hours</p>
-                      </div>
-                    </div>
-                    <table id="reports-table" className="accordion__table">
-                      <tr className="table-row" onClick={this.toggleReportsModal}>
-                        <td className="icon-container">
-                          <Icon icon="heart" />
-                        </td>
-                        <td id="reports-issue-table-cell">
-                          25
-                        </td>
-                        <td>
-                          Compliments
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="icon-container">
-                          <Icon icon="comment" />
-                        </td>
-                        <td>
-                          12
-                        </td>
-                        <td>
-                          Unpaid Rent
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="icon-container">
-                          <Icon icon="comment" />
-                        </td>
-                        <td>
-                          3
-                        </td>
-                        <td>
-                          Other Financial
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="icon-container">
-                          <Icon icon="comment" />
-                        </td>
-                        <td>
-                          0
-                        </td>
-                        <td>
-                          Unauthorized Guests
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="icon-container">
-                          <Icon icon="comment" />
-                        </td>
-                        <td>
-                          1
-                        </td>
-                        <td>
-                          Unit Conditions
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="icon-container">
-                          <Icon icon="comment" />
-                        </td>
-                        <td>
-                          0
-                        </td>
-                        <td>
-                          Rule Violations
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="icon-container">
-                          <Icon icon="comment" />
-                        </td>
-                        <td>
-                          2
-                        </td>
-                        <td>
-                          Noise
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="icon-container">
-                          <Icon icon="comment" />
-                        </td>
-                        <td>
-                          1
-                        </td>
-                        <td>
-                          Property Damage
-                        </td>
-                      </tr>
-                    </table>
-                  </div>
-                </section>
               </div>
+              <section className="contact-details-section">
+                <div>
+                  <div className="table-header-container">
+                    <div className="table-header-left">
+                      <p className="table-header-text">OPENED FROM</p>
+                      <select className="table-header-select">
+                        <option>01/01/2018 - 12/31/2018</option>
+                      </select>
+                    </div>
+                    <div className="table-header-right">
+                      <p className="table-header-text">Average time to close: </p>
+                      <p className="table-header-text">2 days, 4 hours</p>
+                    </div>
+                  </div>
+                  <table id="reports-table" className="accordion__table">
+                    <tr className="table-row" onClick={this.toggleReportsModal}>
+                      <td className="icon-container">
+                        <Icon icon="heart" />
+                      </td>
+                      <td id="reports-issue-table-cell">
+                        25
+                      </td>
+                      <td>
+                        Compliments
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="icon-container">
+                        <Icon icon="comment" />
+                      </td>
+                      <td>
+                        12
+                      </td>
+                      <td>
+                        Unpaid Rent
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="icon-container">
+                        <Icon icon="comment" />
+                      </td>
+                      <td>
+                        3
+                      </td>
+                      <td>
+                        Other Financial
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="icon-container">
+                        <Icon icon="comment" />
+                      </td>
+                      <td>
+                        0
+                      </td>
+                      <td>
+                        Unauthorized Guests
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="icon-container">
+                        <Icon icon="comment" />
+                      </td>
+                      <td>
+                        1
+                      </td>
+                      <td>
+                        Unit Conditions
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="icon-container">
+                        <Icon icon="comment" />
+                      </td>
+                      <td>
+                        0
+                      </td>
+                      <td>
+                        Rule Violations
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="icon-container">
+                        <Icon icon="comment" />
+                      </td>
+                      <td>
+                        2
+                      </td>
+                      <td>
+                        Noise
+                      </td>
+                    </tr>
+                    <tr>
+                      <td className="icon-container">
+                        <Icon icon="comment" />
+                      </td>
+                      <td>
+                        1
+                      </td>
+                      <td>
+                        Property Damage
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+              </section>
             </div>
           </div>
-          <ReportsTicketModal
-            show={this.state.showModal}
-            onClose={this.toggleReportsModal}/>
+        </div>
+        <ReportsTicketModal
+          show={this.state.showModal}
+          onClose={this.toggleReportsModal}/>
       </div>
     );
   }
