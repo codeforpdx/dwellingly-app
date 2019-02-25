aea14be5240c88769b3ccce44f102b2b18f0a671
ccook987 <ccook987@gmail.com>
2019-02-25 13:13:13 -0800

    Update tests for EmergencyNumberEdit component.

--- src/components/forms/EmergencyNumberEdit.test.js
+++ src/components/forms/EmergencyNumberEdit.test.js
@@ -2,31 +2,30 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import EmergencyNumberEdit from './EmergencyNumberEdit';
 
-const EmergencyNumberEditTest = (props) =>
-<EmergencyNumberEdit
-{...props}
-/>;
-
-
 describe('EmergencyNumberEdit Component', () => {
-  const props = {
-    numberOne: '',
-    numberTwo: '',
-    numberThree: '',
-  }
-  it('correctly renders EmergencyNumberEdit component', () => {
+  it('renders phoneNumberOne fieldset on load', () => {
+    const props = {
+      numberOne: {},
+    }
     const EmergencyNumberEditComponent = renderer.create(<EmergencyNumberEdit {...props} />).toJSON();
     expect(EmergencyNumberEditComponent).toMatchSnapshot();
   });
-  it('has a div with className emergencyNumberEditContainer', () => {
-    const firstDiv = shallow(<EmergencyNumberEdit {...props} />).find('div').at(0);
-    expect(firstDiv.hasClass('emergencyNumberEditContainer')).toEqual(true);
+
+  it('renders phoneNumberTwo fieldset if applicable', () => {
+    const props = {
+      numberOne: {},
+      numberTwo: {},
+    }
+    const EmergencyNumberEditComponent = renderer.create(<EmergencyNumberEdit {...props} />).toJSON();
+    expect(EmergencyNumberEditComponent).toMatchSnapshot();
   });
-  // it('user text is echoed onChange', () => {
-  //   const wrapper = shallow(<EmergencyNumberEdit {...props} onEditingEmergencyText={() => {}} />)
-  //   wrapper.find('input').at(0).simulate('change', {
-  //     target: { value: '503-888-1234' }
-  //   });
-  //   expect(wrapper.find('input').at(0).props().value).toEqual('503-888-1234');
-  // });
+  it('renders phoneNumberThree fieldset if applicable', () => {
+    const props = {
+      numberOne: {},
+      numberTwo: {},
+      numberThree: {},
+    }
+    const EmergencyNumberEditComponent = renderer.create(<EmergencyNumberEdit {...props} />).toJSON();
+    expect(EmergencyNumberEditComponent).toMatchSnapshot();
+  });
 });
