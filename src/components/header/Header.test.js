9b78171a6e1074eed0d24173006706e546bb0437
ccook987 <ccook987@gmail.com>
2019-02-25 14:19:56 -0800

    Add test for Header component.

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -3,8 +3,17 @@ import renderer from 'react-test-renderer';
 import Header from './Header';
 
 describe('Header Component', () => {
-  it('correctly renders Header component', () => {
+  it('correctly renders', () => {
     const HeaderComponent = renderer.create(<Header />).toJSON();
+    expect(HeaderComponent).toMatchSnapshot();
+  });
+  it('correctly renders label passed down as props', () => {
+    const props = {
+      label: 'test-label',
+      type: 'test-type',
+      variant: 'test-variant',
+    }
+    const HeaderComponent = renderer.create(<Header {...props} />).toJSON();
     expect(HeaderComponent).toMatchSnapshot();
   });
 });
