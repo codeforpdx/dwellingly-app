fc70076a48b5154dcd172a0ba9b3d3f88f5b18cf
ccook987 <ccook987@gmail.com>
2019-02-25 16:47:59 -0800

    Add tests for IssueSummary component.

--- src/components/issue/IssueSummary.test.js
+++ src/components/issue/IssueSummary.test.js
@@ -3,14 +3,27 @@ import renderer from 'react-test-renderer';
 import IssueSummary from './IssueSummary';
 
 describe('IssueSummary Component', () => {
-  it('renders checkbox icon if issueSent=true', () => {
+  it('renders correctly when issueNote prop exists', () => {
     const props = {
-      issue: '',
-      urgency: '',
       issueNote: '',
     }
     const IssueSummaryComponent = renderer.create(<IssueSummary {...props}/>).toJSON();
     expect(IssueSummaryComponent).toMatchSnapshot();
   });
-
+  it('renders issue text passed down as props', () => {
+    const props = {
+      issue: 'property damage',
+      issueNote: '',
+    }
+    const IssueSummaryComponent = renderer.create(<IssueSummary {...props}/>).toJSON();
+    expect(IssueSummaryComponent).toMatchSnapshot();
+  });
+  it('renders urgency className and text passed down as props', () => {
+    const props = {
+      urgency: 'high',
+      issueNote: '',
+    }
+    const IssueSummaryComponent = renderer.create(<IssueSummary {...props}/>).toJSON();
+    expect(IssueSummaryComponent).toMatchSnapshot();
+  });
 });
