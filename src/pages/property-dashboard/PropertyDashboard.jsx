8109651dee83cefcf243bfc6414303f712762e1d
Bobby Martin <robert.martin.619@gmail.com>
2019-02-28 12:17:40 -0800

    Delete console log and add prettier formatting

--- src/pages/property-dashboard/PropertyDashboard.jsx
+++ src/pages/property-dashboard/PropertyDashboard.jsx
@@ -17,25 +17,66 @@ class PropertyDashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       gridOptions: {
+      gridOptions: {
         rowHeight: 55,
         headerHeight: 60,
         defaultColDef: {
           filter: true
         },
         columnDefs: [
-          {headerName: "Name", field: "name", filter: "agTextColumnFilter", width: 180, sortable: true, checkboxSelection: true, unSortIcon: true, rowDrag: false, suppressMovable:true },
-          {headerName: "Property Manager", field: "city", width: 180, sortable: true, unSortIcon: true, rowDrag: false, suppressMovable:true },
-          {headerName: "Address", field: "addressOne", width:180, sortable: true, unSortIcon: true, rowDrag: false, suppressMovable:true },
-          {headerName: "Tenants", field: "numberOfUnits", width: 180,sortable: true, unSortIcon: true, rowDrag: false, suppressMovable:true },
-          {headerName: "Date Added", field: "state", width: 180, sortable: true, unSortIcon: true, rowDrag: false, suppressMovable:true }
+          {
+            headerName: 'Name',
+            field: 'name',
+            filter: 'agTextColumnFilter',
+            width: 180,
+            sortable: true,
+            checkboxSelection: true,
+            unSortIcon: true,
+            rowDrag: false,
+            suppressMovable: true
+          },
+          {
+            headerName: 'Property Manager',
+            field: 'city',
+            width: 180,
+            sortable: true,
+            unSortIcon: true,
+            rowDrag: false,
+            suppressMovable: true
+          },
+          {
+            headerName: 'Address',
+            field: 'addressOne',
+            width: 180,
+            sortable: true,
+            unSortIcon: true,
+            rowDrag: false,
+            suppressMovable: true
+          },
+          {
+            headerName: 'Tenants',
+            field: 'numberOfUnits',
+            width: 180,
+            sortable: true,
+            unSortIcon: true,
+            rowDrag: false,
+            suppressMovable: true
+          },
+          {
+            headerName: 'Date Added',
+            field: 'state',
+            width: 180,
+            sortable: true,
+            unSortIcon: true,
+            rowDrag: false,
+            suppressMovable: true
+          }
         ]
-    }
+      }
+    };
   }
-}
 
   componentWillMount() {
-    console.log(this.props);
     const { dispatch } = this.props;
     dispatch(getProperties());
     dispatch(getPropertyManagers());
@@ -43,53 +84,73 @@ class PropertyDashboard extends Component {
 
   render() {
     const { intl } = this.props;
-    return(  
-    <div className="page">
-      <Header>
-        {() => (
-          <div>
-            <Navigation />
-            <Header.Label label={intl.formatMessage(COMMON.HELLO)} type="basic" />
-          </div>
-        )}
-      </Header>
-      <section className="property-dashboard-header">
-        <div className="add-new">
-          <h2 className="property-header">PROPERTIES</h2>
-          <Link to="admin/add-new-property"><button type="button" className="btn btn--lrg add-new-btn"><Icon icon="plus"/> ADD NEW</button></Link>
-        </div>
-        <div className="search-and-archive">
-          <form action="" className="search" onSubmit={this.handleSearch}>
-            <div className="icon-wrapper">
-              <Icon icon="search" />
-              <input      
-                type="text"
-                className="property-search"
-                placeholder="Search properties by name, property, or status . . ." />
+    return (
+      <div className="page">
+        <Header>
+          {() => (
+            <div>
+              <Navigation />
+              <Header.Label
+                label={intl.formatMessage(COMMON.HELLO)}
+                type="basic"
+              />
             </div>
-          </form>
-          <div>ARCHIVED:<button className="switch__btn" type="button">hide</button></div>
-        </div>
-      </section>
-      <section className="property-dashboard-header property-table">
-        <div className="gray-dash-bar">
-          <div className="ghost-dot"> .</div>
-          <button type="button" className="btn archive-btn"><Icon icon="archive"/> ARCHIVE</button>
-        </div>
-        <div className="ag-grid-wrapper">
-          <AgGridReact
-            gridOptions={this.state.gridOptions}
-            defaultColDef={this.state.defaultColDef}
-            columnDefs={this.state.columnDefs}
-            rowData={this.props.properties.properties.length > 0 ? this.props.properties.properties : []}
-            pagination
-            paginationPageSize={10}
-          />    
-        </div>
-      </section>
-    </div>
-  )}
-};
+          )}
+        </Header>
+        <section className="property-dashboard-header">
+          <div className="add-new">
+            <h2 className="property-header">PROPERTIES</h2>
+            <Link to="admin/add-new-property">
+              <button type="button" className="btn btn--lrg add-new-btn">
+                <Icon icon="plus" /> ADD NEW
+              </button>
+            </Link>
+          </div>
+          <div className="search-and-archive">
+            <form action="" className="search" onSubmit={this.handleSearch}>
+              <div className="icon-wrapper">
+                <Icon icon="search" />
+                <input
+                  type="text"
+                  className="property-search"
+                  placeholder="Search properties by name, property, or status . . ."
+                />
+              </div>
+            </form>
+            <div>
+              ARCHIVED:
+              <button className="switch__btn" type="button">
+                hide
+              </button>
+            </div>
+          </div>
+        </section>
+        <section className="property-dashboard-header property-table">
+          <div className="gray-dash-bar">
+            <div className="ghost-dot"> .</div>
+            <button type="button" className="btn archive-btn">
+              <Icon icon="archive" /> ARCHIVE
+            </button>
+          </div>
+          <div className="ag-grid-wrapper">
+            <AgGridReact
+              gridOptions={this.state.gridOptions}
+              defaultColDef={this.state.defaultColDef}
+              columnDefs={this.state.columnDefs}
+              rowData={
+                this.props.properties.properties.length > 0
+                  ? this.props.properties.properties
+                  : []
+              }
+              pagination
+              paginationPageSize={10}
+            />
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
 
 PropertyDashboard.propTypes = {
   dispatch: PropTypes.func.isRequired,
@@ -103,18 +164,18 @@ PropertyDashboard.propTypes = {
   propertyManagers: PropTypes.shape({
     propertyManagers: PropTypes.arrayOf(PropTypes.object)
   })
-}
+};
 
 PropertyDashboard.defaultProps = {
-  properties: {properties: []},
-  archivedProperties: {archivedProperties: []},
-  propertyManagers: {propertyManagers: []},
+  properties: { properties: [] },
+  archivedProperties: { archivedProperties: [] },
+  propertyManagers: { propertyManagers: [] }
 };
 
 const mapStateToProps = state => ({
   properties: state.properties,
   archivedProperties: state.archivedProperties,
-  propertyManagers: state.propertyManagers,
-})
+  propertyManagers: state.propertyManagers
+});
 
 export default injectIntl(connect(mapStateToProps)(PropertyDashboard));
