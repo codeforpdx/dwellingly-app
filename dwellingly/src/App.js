0a118627184f4cc3fa794d60adeed6b240e780bf
htharker42 <htharker@gmail.com>
2019-12-05 00:09:25 -0800

    customized landing page for code for pdx/dwellingly

--- dwellingly/src/App.js
+++ dwellingly/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './c4pdx.gif';
+import dwellinglylogo from './dwellingly.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={dwellinglylogo} className="" alt="dwellingly" /> 
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.codeforpdx.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Code for PDX
         </a>
+        <p>
+          Community Built Civic Tech.
+        </p>
       </header>
     </div>
   );
