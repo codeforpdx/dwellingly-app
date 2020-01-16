378eb55ac79266af075f2628b69fd24e69d37a79
Max Stern <stern.maxwell@gmail.com>
2020-01-16 11:51:09 -0800

    header component reorg

--- dwellingly/src/App.js
+++ dwellingly/src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import logo from './c4pdx.gif';
-import dwellinglylogo from './dwellingly.png'
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={dwellinglylogo} className="" alt="dwellingly" /> 
-        <img src={logo} className="App-logo" alt="logo" />
-      
-        <a
-          className="App-link"
-          href="http://www.codeforpdx.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code for PDX
-        </a>
-        <p>
-          Community Built Civic Tech.
-        </p>
-      </header>
+    <div className='App'>
+      <Header />
     </div>
   );
 }
