1cd52affe9d5dc51f2257a1f6e82e0d2ae3d939a
Max Stern <stern.maxwell@gmail.com>
2020-01-23 11:23:44 -0800

    additional basic component tests

--- dwellingly/src/components/Header/header.test.js
+++ dwellingly/src/components/Header/header.test.js
@@ -8,10 +8,21 @@ const setUp = (props = {}) => {
 };
 
 describe('Header Component', () => {
+  it('should be true', () => {
+    const foo = true;
+    expect(foo).toBe(true);
+  });
+
   it('Should render without errors', () => {
     const component = setUp();
     const wrapper = component.find('.App-header');
     expect(wrapper.length).toBe(1);
+  });
+
+  it('Should render exactly 2 images', () => {
+    const component = setUp();
+    const wrapper = component.find('img');
+    expect(wrapper.length).toBe(2);
   });
 
   it('Should render a logo', () => {
