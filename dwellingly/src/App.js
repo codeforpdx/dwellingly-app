93d86c3a56c55a51ec31b582331f1f8cbded0035
Max Stern <stern.maxwell@gmail.com>
2020-01-30 11:15:52 -0800

    Header component, for feature #77

--- dwellingly/src/App.js
+++ dwellingly/src/App.js
@@ -3,28 +3,27 @@ import './App.scss';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { LoginForm } from './views/login';
 import { Home } from './views/home';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
-        <header className='App-header'>
-          <div className='App-navbar'>
-            <ul>
-              <li>
-                <Link to='/login'>Login</Link>
-              </li>
-              <li>
-                <Link to='/signup'>Signup</Link>
-              </li>
-            </ul>
-          </div>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/login' component={LoginForm} />
-            <Route exact path='/signup' />
-          </Switch>
-        </header>
+        <Header />
+        <ul>
+          <li>
+            <Link to='/signup'>Signup</Link>
+          </li>
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={LoginForm} />
+          <Route exact path='/signup' />
+        </Switch>
       </div>
     </BrowserRouter>
   );
