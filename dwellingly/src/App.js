49954b47365a37cbdb341c16de419e87192f2e97
Max Stern <stern.maxwell@gmail.com>
2020-01-29 19:34:04 -0800

    fixing app.css merge

--- dwellingly/src/App.js
+++ dwellingly/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { LoginForm } from './pages/login';
 import { Home } from './pages/home';
@@ -7,18 +7,22 @@ import { Home } from './pages/home';
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header className="App-header">
-            <div className="App-navbar">
-              <ul>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/signup">Signup</Link></li>
-              </ul>
-            </div>
+      <div className='App'>
+        <header className='App-header'>
+          <div className='App-navbar'>
+            <ul>
+              <li>
+                <Link to='/login'>Login</Link>
+              </li>
+              <li>
+                <Link to='/signup'>Signup</Link>
+              </li>
+            </ul>
+          </div>
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={LoginForm} />
-            <Route exact path="/signup" />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={LoginForm} />
+            <Route exact path='/signup' />
           </Switch>
         </header>
       </div>
