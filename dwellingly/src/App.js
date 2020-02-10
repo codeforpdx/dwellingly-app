32114e7b2357a93c517b7044b68e549cd11f27b4
Chris Boe <cboe@navexglobal.com>
2020-02-09 20:52:24 -0800

    Change home route to private

--- dwellingly/src/App.js
+++ dwellingly/src/App.js
@@ -3,29 +3,31 @@ import './App.scss';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { LoginForm } from './views/login';
 import { Home } from './views/home';
+import { PrivateRoute } from './Auth';
 import { Terms } from './views/terms';
-import Header from './components/Header';
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
-        <Header />
-        <ul>
-          <li>
-            <Link to='/signup'>Signup</Link>
-          </li>
-          <li>
-            <Link to='/login'>Login</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={LoginForm} />
-          <Route exact path='/signup' />
-          <Route exact path='/terms' component={Terms} />
-        </Switch>
+        <header className='App-header'>
+          <div className='App-navbar'>
+            <ul>
+              <li>
+                <Link to='/login'>Login</Link>
+              </li>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+            </ul>
+          </div>
+          <Switch>
+            <PrivateRoute exact path='/' component={Home} />
+            <Route exact path='/login' component={LoginForm} />
+            <Route exact path='/terms' component={Terms} />
+          </Switch>
+        </header>
       </div>
     </BrowserRouter>
   );
