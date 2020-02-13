3d491c841c6840f56583aa008098af3e76a76937
Chris Boe <cboe@navexglobal.com>
2020-02-12 20:20:27 -0800

    Add empty dashboard page

---
+++ dwellingly/src/views/dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Admin Dashboard</h1>
+            </div>
+        )
+    }
+}
