3a968869323872322701404a510cdc4aac1dd958
Chris Boe <cboe@navexglobal.com>
2020-02-12 20:32:11 -0800

    Attach fields to state

--- dwellingly/src/views/login.js
+++ dwellingly/src/views/login.js
@@ -7,9 +7,15 @@ import dwellinglyLogo from '../assets/images/dwellingly_logo.png';
 export class LoginForm extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            username: null,
+            password: null
+        }
     }
 
     onSubmit = () => {
+        console.log("logged in!")
         tempAuth.authenticate();
     }
 
@@ -17,6 +23,10 @@ export class LoginForm extends React.Component {
         return (
             <div className="login__container">
                 <Formik
+                    initialValues={{
+                        username: "",
+                        password: ""
+                    }}
                     onSubmit={ () => {
                         this.onSubmit();
                     }}
@@ -31,6 +41,7 @@ export class LoginForm extends React.Component {
                                             className="form-field"
                                             type="text"
                                             name="username"
+                                            value={this.state.username}
                                             placeholder="Username"
                                             required
                                         />
@@ -38,9 +49,9 @@ export class LoginForm extends React.Component {
                                             className="form-field"
                                             type="password"
                                             name="password"
+                                            value={this.state.password}
                                             placeholder="Password"
                                             required
-
                                         />
                                         <div></div>
                                         <button className="login-button" type="submit">LOG IN</button>
@@ -50,7 +61,7 @@ export class LoginForm extends React.Component {
                                                 <span className="login__or_text">OR</span>
                                             </div>
                                         </div>
-                                        <button className="login-button" type="submit">LOG IN WITH GOOGLE</button>
+                                        <button className="login-button">LOG IN WITH GOOGLE</button>
                                         <div className="login__account_container">
                                             <a href="" className="login__account_forgot_password">Forgot Password</a>
                                             <a href="" className="login__account__create">Create an Account</a>
