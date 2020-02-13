482df0c4fe4aa98f85212038877fc306b3ace3af
Chris Boe <cboe@navexglobal.com>
2020-02-12 20:20:27 -0800

    Add empty dashboard page

--- dwellingly/src/App.js
+++ dwellingly/src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { LoginForm } from './views/login';
 import { Home } from './views/home';
+import { Dashboard } from './views/dashboard';
 import { PrivateRoute } from './Auth';
 
 
@@ -26,8 +27,9 @@ function App() {
           </header> */}
 
           <Switch>
-            <PrivateRoute exact path='/' component={Home} />
             <Route exact path='/login' component={LoginForm} />
+            <PrivateRoute exact path='/' component={Home} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
           </Switch>
       </div>
     </BrowserRouter>
