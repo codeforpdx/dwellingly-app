470f533afe29bf227d88b15b9354ee51610a232a
Mike Heavers <mheavers@gmail.com>
2020-02-15 19:19:48 -0800

    fine tune rough styling for login page begin implementing BEM css naming convention separate login css into separate login page .scss file

--- dwellingly/src/components/Header/index.js
+++ dwellingly/src/components/Header/index.js
@@ -20,6 +20,21 @@ const Header = props => {
       </ul>
     </div>
     </header>
+
+    // <header className='App-header'>
+    //   <img src={dwellinglylogo} className='' alt='dwellingly' />
+    //   <img src={PDXlogo} className='App-logo' alt='logo' />
+
+    //   <a
+    //     className='App-link'
+    //     href='http://www.codeforpdx.org'
+    //     target='_blank'
+    //     rel='noopener noreferrer'
+    //   >
+    //     Code for PDX
+    //   </a>
+    //   <p>Community Built Civic Tech.</p>
+    // </header>
   );
 };
 
