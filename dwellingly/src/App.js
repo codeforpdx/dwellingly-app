6f75d26449b67abfa6f4e92ec70d953444106047
Max Stern <stern.maxwell@gmail.com>
2020-02-16 16:36:38 -0800

    terms & conditions view

--- dwellingly/src/App.js
+++ dwellingly/src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import { LoginForm } from './views/login';
 import { Home } from './views/home';
+import { Terms } from './views/terms';
 import Header from './components/Header';
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
           <Route exact path='/' component={Home} />
           <Route exact path='/login' component={LoginForm} />
           <Route exact path='/signup' />
+          <Route exact path='/terms' component={Terms} />
         </Switch>
       </div>
     </BrowserRouter>
