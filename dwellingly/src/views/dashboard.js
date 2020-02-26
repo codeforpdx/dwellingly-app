17b82edf3a785ef0efa625c9234e365f269c2073
Mike Heavers <mheavers@gmail.com>
2020-02-26 13:22:44 -0800

    begin implementing dashboard: styling, layout add font awesome add typography sass file

--- dwellingly/src/views/dashboard.js
+++ dwellingly/src/views/dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import dwellinglylogo from '../assets/images/dwellingly_logo_white.png';
 
 export class Dashboard extends React.Component {
     constructor(props) {
@@ -7,8 +8,24 @@ export class Dashboard extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Admin Dashboard</h1>
+            <div className="dashboard__container">
+                <div className="dashboard__sidebar_container">
+                    <div className="dashboard__sidebar_user_container">
+                    <span className="icon dashboard__sidebar_user_icon">
+                        <i className="fas fa-user"></i>
+                    </span>
+                        <span className="dashboard__sidebar_user_name">Raymond Holt</span>
+                    </div>
+                </div>
+                <div className="dashboard__main_container">
+                    <div className="dashboard__header">
+                        <img className="dashboard__header_logo" src={dwellinglylogo}/>
+                    </div>
+                    <div className="dashboard__main">
+                        <h2 className="page-title">Admin Dashboard</h2>
+                    </div>
+                </div>
+                
             </div>
         )
     }
