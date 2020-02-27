54832e53f3e145a4fa593c9b100fa4fd28c8e4db
Chris Boe <cboe@navexglobal.com>
2020-02-26 17:11:01 -0800

    Add context to manage auth state

--- dwellingly/src/views/login.js
+++ dwellingly/src/views/login.js
@@ -1,79 +1,77 @@
 import React from 'react';
 import { Form, Field, Formik } from 'formik';
-import { tempAuth } from '../Auth';
 import dwellinglyLogo from '../assets/images/dwellingly_logo.png';
-
+import { UserContext } from '../App';
 
 export class LoginForm extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            username: null,
-            password: null
-        }
+    this.state = {
+      username: undefined,
+      password: undefined
     }
+  }
 
-    onSubmit = () => {
-        console.log("logged in!")
-        tempAuth.authenticate();
-    }
+  render() {
+    return (
+      <UserContext.Consumer>
+        {({ login }) => (
+          <div className="login__container">
+            <Formik
+              initialValues={{
+                username: "",
+                password: ""
+              }}
+              onSubmit={({ username, password }) => {
+                login(username, password);
+              }}
+              enableReinitialize={true}
+              render={
+                (props) => {
+                  return (
+                    <>
+                      <Form className="login__form-container">
+                        <img className="login__logo" src={dwellinglyLogo}></img>
+                        <Field
+                          className="form-field login__form-field"
+                          type="text"
+                          name="username"
+                          value={this.state.username}
+                          placeholder="Username"
+                          required
+                        />
+                        <Field
+                          className="form-field login__form-field"
+                          type="password"
+                          name="password"
+                          value={this.state.password}
+                          placeholder="Password"
+                          required
+                        />
+                        <div></div>
+                        <button className="login__button" type="submit">LOG IN</button>
+                        <div className="login__or_container">
+                          <div className="login__or">
+                            <span className="login__divider"></span>
+                            <span className="login__or_text">OR</span>
+                          </div>
+                        </div>
+                        <button className="login__button login__button--google">LOG IN WITH GOOGLE</button>
+                        <div className="login__account_container">
+                          <a href="" className="login__text_link login__account_forgot_password">Forgot Password</a>
+                          <a href="" className="login__text_link login__account_create">Create an Account</a>
+                        </div>
 
-    render() {
-        return (
-            <div className="login__container">
-                <Formik
-                    initialValues={{
-                        username: "",
-                        password: ""
-                    }}
-                    onSubmit={ () => {
-                        this.onSubmit();
-                    }}
-                    enableReinitialize={true}
-                    render={
-                        ( props ) => {
-                            return (
-                                <>
-                                    <Form className="login__form-container">
-                                        <img className="login__logo" src={dwellinglyLogo}></img>
-                                        <Field 
-                                            className="form-field login__form-field"
-                                            type="text"
-                                            name="username"
-                                            value={this.state.username}
-                                            placeholder="Username"
-                                            required
-                                        />
-                                        <Field
-                                            className="form-field login__form-field"
-                                            type="password"
-                                            name="password"
-                                            value={this.state.password}
-                                            placeholder="Password"
-                                            required
-                                        />
-                                        <div></div>
-                                        <button className="login__button" type="submit">LOG IN</button>
-                                        <div className="login__or_container">
-                                            <div className="login__or">
-                                                <span className="login__divider"></span>
-                                                <span className="login__or_text">OR</span>
-                                            </div>
-                                        </div>
-                                        <button className="login__button login__button--google">LOG IN WITH GOOGLE</button>
-                                        <div className="login__account_container">
-                                            <a href="" className="login__text_link login__account_forgot_password">Forgot Password</a>
-                                            <a href="" className="login__text_link login__account_create">Create an Account</a>
-                                        </div>
-
-                                    </Form>
-                                </>
-                            );
-                        }
-                    }
-                />
-            </div>
-        )
-    }
+                      </Form>
+                    </>
+                  );
+                }
+              }
+            />
+          </div>
+        )}
+      </UserContext.Consumer>
+    )
+  }
 }
