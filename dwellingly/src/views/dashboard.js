aca3c69717120cc1e9df3271bd0b2577256dd294
Mike Heavers <mheavers@gmail.com>
2020-03-03 15:54:02 -0800

    stub out modal for handling access

--- dwellingly/src/views/dashboard.js
+++ dwellingly/src/views/dashboard.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import dwellinglylogo from '../assets/images/dwellingly_logo_white.png';
-import {MODULE_DATA, ACCESS_REQUEST_DATA} from '../components/DashboardModule/data';
+import { MODULE_DATA, ACCESS_REQUEST_DATA } from '../components/DashboardModule/data';
 import DashboardModule from '../components/DashboardModule';
 import Collapsible from '../components/Collapsible';
 import RequestItem from '../components/RequestItem';
@@ -8,98 +8,129 @@ import RequestItem from '../components/RequestItem';
 export class Dashboard extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            modalActive: true
+        }
     }
 
-    handleAddClick(id){
-        console.log('add',id);
+    handleAddClick = (id)=> {
+        console.log('handle add access',id);
+        //TODO: route to add page with id (or switch the click handler within requestItem to a traditional link)
     }
 
-    handleDeclineClick(id){
-        console.log('decline',id);
+    handleDeclineClick = (id)=> {
+        //console.log('decline',id);
+        this.setState({
+            modalActive: true
+        })
+    }
+
+    handleDenyAccess(doDeny) {
+        this.setState({modalActive: false});
+        if (doDeny){
+            console.log('handle deny access');
+            //TODO: handle deny access
+        }
     }
 
     render() {
         return (
-            <div className="dashboard__container">
-                <div className="dashboard__sidebar_container">
-                    <div className="dashboard__sidebar_user_container">
-                        <span className="icon dashboard__sidebar_user_icon">
-                            <i className="fas fa-user"></i>
-                        </span>
-                        <span className="dashboard__sidebar_user_name">Raymond Holt</span>
-                    </div>
-                </div>
-                <div className="dashboard__main_container">
-                    <div className="dashboard__header">
-                        <img className="dashboard__header_logo" src={dwellinglylogo} />
-                    </div>
-                    <div className="dashboard__main">
-                        <h2 className="page-title">Admin Dashboard</h2>
-                        <div className="dashboard__modules_container">
-                            <DashboardModule
-                                data={MODULE_DATA.openTickets}
-                            />
-                            <DashboardModule
-                                data={MODULE_DATA.reports}
-                            />
-                            <DashboardModule
-                                data={MODULE_DATA.managers}
-                            />
+            <>
+                <div className="dashboard__container">
+                    <div className="dashboard__sidebar_container">
+                        <div className="dashboard__sidebar_user_container">
+                            <span className="icon dashboard__sidebar_user_icon">
+                                <i className="fas fa-user"></i>
+                            </span>
+                            <span className="dashboard__sidebar_user_name">Raymond Holt</span>
                         </div>
-                        <Collapsible
-                            title="New Staff Assignments"
-                            count="3"
-                        >
-                            <div className="collapsible__row columns">
-                                <div className="collapsible__col column">Tenant Name</div>
-                                <div className="collapsible__col column">
-                                    Meerkat Manner<br />
-                                    <span className="subtext">Property Manager Name</span>
-                                </div>
-                                <div className="dashboard__colapsible_col column">
-                                    <div className="select is-rounded">
-                                        <select>
-                                            <option>Staff Name</option>
-                                            <option>Staff Name 2</option>
-                                            <option>Staff Name 3</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="collapsible__row columns">
-                                <div className="collapsible__col column">Tenant Name</div>
-                                <div className="collapsible__col column">
-                                    Meerkat Manner<br />
-                                    <span className="subtext">Property Manager Name</span>
-                                </div>
-                                <div className="dashboard__colapsible_col column">
-                                    <div className="select is-rounded">
-                                        <select>
-                                            <option>Staff Name</option>
-                                            <option>Staff Name 2</option>
-                                            <option>Staff Name 3</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                        </Collapsible>
-                        <Collapsible
-                            title="Request for Access"
-                        >
-                            {
-                                ACCESS_REQUEST_DATA.map((requestItemData,index)=>{
-                                    return (<RequestItem key={`requestItem--${index}`} data={requestItemData} onDeclineClick={this.handleDeclineClick} onAddClick={this.handleAddClick}/>);
-                                })
-                            }
-                        </Collapsible>
                     </div>
-                    <footer className="dashboard__footer">
-                        <p className="dashboard__footer_logo_text">
-                            <span className="bold">JOIN</span> 2018
+                    <div className="dashboard__main_container">
+                        <div className="dashboard__header">
+                            <img className="dashboard__header_logo" src={dwellinglylogo} />
+                        </div>
+                        <div className="dashboard__main">
+                            <h2 className="page-title">Admin Dashboard</h2>
+                            <div className="dashboard__modules_container">
+                                <DashboardModule
+                                    data={MODULE_DATA.openTickets}
+                                />
+                                <DashboardModule
+                                    data={MODULE_DATA.reports}
+                                />
+                                <DashboardModule
+                                    data={MODULE_DATA.managers}
+                                />
+                            </div>
+                            <Collapsible
+                                title="New Staff Assignments"
+                                count="3"
+                            >
+                                <div className="collapsible__row columns">
+                                    <div className="collapsible__col column">Tenant Name</div>
+                                    <div className="collapsible__col column">
+                                        Meerkat Manner<br />
+                                        <span className="subtext">Property Manager Name</span>
+                                    </div>
+                                    <div className="dashboard__colapsible_col column">
+                                        <div className="select is-rounded">
+                                            <select>
+                                                <option>Staff Name</option>
+                                                <option>Staff Name 2</option>
+                                                <option>Staff Name 3</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="collapsible__row columns">
+                                    <div className="collapsible__col column">Tenant Name</div>
+                                    <div className="collapsible__col column">
+                                        Meerkat Manner<br />
+                                        <span className="subtext">Property Manager Name</span>
+                                    </div>
+                                    <div className="dashboard__colapsible_col column">
+                                        <div className="select is-rounded">
+                                            <select>
+                                                <option>Staff Name</option>
+                                                <option>Staff Name 2</option>
+                                                <option>Staff Name 3</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Collapsible>
+                            <Collapsible
+                                title="Request for Access"
+                            >
+                                {
+                                    ACCESS_REQUEST_DATA.map((requestItemData, index) => {
+                                        return (<RequestItem key={`requestItem--${index}`} data={requestItemData} onDeclineClick={this.handleDeclineClick} onAddClick={this.handleAddClick} />);
+                                    })
+                                }
+                            </Collapsible>
+                        </div>
+                        <footer className="dashboard__footer">
+                            <p className="dashboard__footer_logo_text">
+                                <span className="bold">JOIN</span> 2018
                         </p>
-                    </footer>
+                        </footer>
+                    </div>
                 </div>
-            </div>
+                <div class={`modal ${this.state.modalActive && 'is-active'}`}>
+                    <div class="modal-background" onClick={()=>{this.handleDenyAccess(false)}}></div>
+                    <div class="modal-content">
+                        <div className="modal__message_container">
+                            <div className="modal__message">
+                                <h4>Are you sure you want to decline access?</h4>
+                            </div>
+                            <div className="modal__button_container">
+                                <button className="button is-primary is-rounded" onClick={() => { this.handleDenyAccess(true) }}>YES</button>
+                                <button className="button is-dark is-rounded" onClick={() => { this.handleDenyAccess(false) }}>NO</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
         )
     }
 }
