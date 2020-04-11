d860aeb372185eea03257b8c0c8e4e4c42942864
Chris Boe <cboe@navexglobal.com>
2020-04-10 18:19:56 -0700

    Show first name and last name in left nav header

--- src/components/NavigationMenu/navigationMenu.js
+++ src/components/NavigationMenu/navigationMenu.js
@@ -13,6 +13,7 @@ import {
 import { useLocation, Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 import "bulma/css/bulma.css";
+import { UserContext } from '../../App';
 
 
 export class LogOutButton extends React.Component {
@@ -74,36 +75,39 @@ export const NavMenu = () => {
   }
 
   return (
-    <div className="is-hidden-mobile is-sidebar-menu bg-blue">
-      <div className="menu">
-        <ul className="menu-list">
-          <div className="is-username-link">
-            <MenuLink name="{User Name}" isBold icon={faUserAlt} href="/home"/>
-          </div>
-          <MenuLink name="Dashboard" isBold icon={faColumns} href="/dashboard"/>
-          
-          <MenuLink name="Add New" isBold icon={faPlusCircle} href="/add"/>
-          <div className="is-child-link">
-            <MenuLink name="Tenant" href="/add/tenant" />
-            <MenuLink name="Property" href="/add/property" />
-            <MenuLink name="Property Manager" href="/add/manager" />
-          </div>
+    <UserContext.Consumer>
+      {({ user }) => (
+        <div className="is-hidden-mobile is-sidebar-menu bg-blue">
+          <div className="menu">
+            <ul className="menu-list">
+              <div className="is-username-link">
+                <MenuLink name={`${user.firstName} ${user.lastName}`} isBold icon={faUserAlt} href="/home"/>
+              </div>
+              <MenuLink name="Dashboard" isBold icon={faColumns} href="/dashboard"/>
+              <MenuLink name="Add New" isBold icon={faPlusCircle} href="/add"/>
+              <div className="is-child-link">
+                <MenuLink name="Tenant" href="/add/tenant" />
+                <MenuLink name="Property" href="/add/property" />
+                <MenuLink name="Property Manager" href="/add/manager" />
+              </div>
 
-          <MenuLink name="Manage" isBold icon={faUserCog} href="/manage"/>
-          <div className="is-child-link">
-            <MenuLink name="Tenants" href="/manage/tenants" />
-            <MenuLink name="Properties" href="/manage/properties" />
-            <MenuLink name="Property Managers" href="/manage/managers" />
-          </div>
+              <MenuLink name="Manage" isBold icon={faUserCog} href="/manage"/>
+              <div className="is-child-link">
+                <MenuLink name="Tenants" href="/manage/tenants" />
+                <MenuLink name="Properties" href="/manage/properties" />
+                <MenuLink name="Property Managers" href="/manage/managers" />
+              </div>
 
-          <MenuLink name="Tickets" isBold icon={faTicketAlt} href="/tickets"/>
-          <MenuLink name="Reports" isBold icon={faBook} href="/reports" />
-          <MenuLink name="JOIN Staff" isBold icon={faUserAlt} href="/staff" />
-          <MenuLink name="Emergency Numbers" isBold icon={faPhoneAlt} href="/emergency" />
-          <MenuLink name="Settings" isBold icon={faCog} href="/settings" />
-        </ul>
-      </div>
-      <LogOutButton />
-    </div>
+              <MenuLink name="Tickets" isBold icon={faTicketAlt} href="/tickets"/>
+              <MenuLink name="Reports" isBold icon={faBook} href="/reports" />
+              <MenuLink name="JOIN Staff" isBold icon={faUserAlt} href="/staff" />
+              <MenuLink name="Emergency Numbers" isBold icon={faPhoneAlt} href="/emergency" />
+              <MenuLink name="Settings" isBold icon={faCog} href="/settings" />
+            </ul>
+          </div>
+          <LogOutButton />
+        </div>
+      )}
+    </UserContext.Consumer>
   );
 }
