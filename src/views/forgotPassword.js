c7bb4944f7439290286282b98ad823ad5c03200c
[Marie-Ehrman] <[marie.e.ehrman@gmail.com]>
2020-04-22 18:16:34 -0700

    Initial Setup of Forgot PW route
    Needs functionality and fine tuning!

--- src/views/forgotPassword.js
+++ src/views/forgotPassword.js
@@ -6,14 +6,25 @@ import { UserContext } from '../App';
 import * as axios from 'axios';
 import Header from '../components/Header';
 
+//TODO:
+    // get the top line in the email field to go away
+    // move the request button to the left
+    // request password reset button functionality
+    // recover account link functionality ... ?
+
+
+
 const validationSchema = Yup.object().shape({
     email: Yup.string()
         .email("*Please enter a valid email")
-        .required("*Name is required"),
+        .required("*Email is required"),
 });
 
 const formHandler = (data, context) => {
-    axios.post('http://localhost:5000/properties', data, { headers: {"Authorization" : `Bearer ${context.user.accessJwt}`} })
+    axios.post('http://localhost:5000/properties',
+                data,
+                { headers: {"Authorization" : `Bearer ${context.user.accessJwt}`}
+    })
         .then(function(response){
             alert("Property Added!");
         })
@@ -28,6 +39,7 @@ export class ForgotPassword extends Component {
         super(props);
         
         this.state = {
+            email:'',
 
         }
     }    
@@ -61,7 +73,8 @@ export class ForgotPassword extends Component {
                             {({ handleSubmit, handleChange, values, errors, touched, isValid, isSubmitting }) => (
                                 <div className="form-container forgot-password__main_container">
                                     <p className="para-text">
-                                      Forgot your password? It happens to all of us. To recover your password, follow the instructions that relate to your account.</p>
+                                      Forgot your password? It happens to all of us. To recover your password, follow the instructions that relate to your account.
+                                    </p>
                                     <h1 className="section-title">EMAIL/PASSWORD USERS</h1>
                                     <Form className="forgot-password__form-container" onSubmit={handleSubmit}>
                                         <div className="form-row columns">
@@ -76,14 +89,20 @@ export class ForgotPassword extends Component {
                                             />
                                             {errors.name ? (<div className="error-message">{errors.name}</div>) : null}
                                         </div>
-                                        <div className="container-footer">
-                                            <button className={`${isValid && "active"} save_button button is-rounded`} type="submit" disabled={isSubmitting}>REQUEST PASSWORD RESET</button>
+                                        <div className="forgot-password__container-footer">
+                                            <button
+                                                className={`${isValid && "active"} save_button button is-rounded`}
+                                                type="submit"
+                                                disabled={isSubmitting}>
+                                                    REQUEST PASSWORD RESET
+                                            </button>
+                                            <div className="forgot-password__google-users">
+                                                <NavLink to="/login">Return to log in</NavLink>
+                                                <h1 className="section-title">GOOGLE ACCOUNT USERS</h1>
+                                                <NavLink to="">Recover your account</NavLink>
+                                            </div>
                                         </div>
-                                        <NavLink to="/login">Return to log in</NavLink>
-                                        <h1 className="section-title">GOOGLE ACCOUNT USERS</h1>
-                                        <NavLink to="">Recover your account</NavLink>
                                     </Form>
-
                                 </div>
                                 )}
                         </Formik>
