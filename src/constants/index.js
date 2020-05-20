005a79808af00a7a138683ef76c18f7d3c9eb5c3
Zachary Fregin <zach.fregin@gmail.com>
2020-05-20 10:07:54 -0700

    Initial Push
    Ticket modal components

---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+export const CARD_TYPES = {
+  CLOSED: 'closed',
+  FORM: 'form',
+  LARGE: 'lg',
+  SMALL: 'sm',
+  STATUS: 'status',
+  TICKET: 'ticket'
+};
+
+export const STATUS_OPTIONS = {
+  NEW: 'New',
+  IN_PROGRESS: 'In Progress',
+  RESOLVED: 'Resolved',
+  REOPEN: 'Reopen'
+};
