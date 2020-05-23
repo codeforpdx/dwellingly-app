6cecc49858fb3d6850d3319b11b1e8c766b57283
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-05-23 12:47:14 -0700

    make LogOutButton bulma navbar-item

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,14 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import dwellinglylogo from '../../assets/images/dwellingly_logo_white.png';
 
-const Header = props => {
+const Header = (props) => {
+	const LogOutButton = () => (
+		<UserContext.Consumer>
+			{({ logout }) => (
+				<button
+					className="button is-dark is-rounded is-small"
+					onClick={logout}
+				>
+					LOG OUT
+				</button>
+			)}
+		</UserContext.Consumer>
+	);
 
-  return (
-    <header className='navbar bg-gradient'>
-      <div className='navbar-brand'>
-        <Link className='navbar-item' to='/'>
-          <img src={dwellinglylogo} alt='dwellingly logo' />
+	return (
+    <header className="navbar bg-gradient">
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/">
+          <img src={dwellinglylogo} alt="dwellingly logo" />
         </Link>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="is-logout-button">
+            <LogOutButton />
+          </div>
+        </div>
       </div>
     </header>
   );
