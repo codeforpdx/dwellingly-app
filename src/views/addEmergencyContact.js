667126b5991221212d1c4716bd9815f9c7d8d2ed
Dave Trost <davetrost@gmail.com>
2020-05-27 19:10:25 -0700

    dynamic operation for "add new phone number" functionality

--- src/views/addEmergencyContact.js
+++ src/views/addEmergencyContact.js
@@ -135,14 +135,18 @@ const AddEmergencyContact = () => {
                             <FieldArray 
                                 name="numbers"
                                 render={numbersArrayFields => {
-                                    const addRow = () => numbersArrayFields.push({ number: "", numtype: "", extension: "" });
+                                    const addRowValid = (values.numbers[0].number !== "") && (!errors.numbers || !errors.numbers[values.numbers.length - 1].number);
+                                    const addRow = () => addRowValid && numbersArrayFields.push({ number: "", numtype: "", extension: "" });
+                                    
                                     return(
                                         <>
                                             {values.numbers.map((_, i) => (
                                                 <NumberSubForm key={i} i={i} values={values} errors={errors} handleChange={handleChange} />
                                             ))}
-                                            <div className="is-rounded form-add-rows" onClick={addRow}>
-                                                <i className="fas fa-plus-circle"></i> Add New Phone Number
+                                            <div className="form-add-rows-container">
+                                                <span className={`${addRowValid ? "active" : ""} is-rounded form-add-rows`} onClick={addRow}>
+                                                    <i className="fas fa-plus-circle"></i> Add New Phone Number
+                                                </span>
                                             </div>
                                         </>
                                     );
