6a23b1b4884670fa9aaf36fb085cf7d30216a3ba
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-06-17 21:36:23 -0700

    add keys to managers.js, add jsx for manager contact info

--- src/views/manager.js
+++ src/views/manager.js
@@ -5,10 +5,16 @@ import * as axios from "axios";
 import { PROPERTY_MANAGER_DATA } from "./dummyData/pManagerData";
 
 const Manager = () => {
-	const { id } = useParams();
-	const dummyDataManagerInfo = PROPERTY_MANAGER_DATA.find(manager => manager.id === id);
-	
+  const { id } = useParams();
+  // can remove this once api is configured to return properties and tenants
+  const dummyDataManagerInfo = PROPERTY_MANAGER_DATA.find(
+    (manager) => manager.id === id
+  );
+
   const [manager, setManager] = useState(dummyDataManagerInfo);
+
+  const contactInfoFields = ["firstName", "lastName", "phone", "email"];
+  const contactInfoTitles = ["First Name", "Last Name", "Phone", "Email"];
 
   // use getManager once /users/?id api endpoint returns properties and tenants for property managers
   const getManager = (context) => {
@@ -27,7 +33,25 @@ const Manager = () => {
       });
   };
 
-  return <div>manager</div>;
+  return (
+    <div className="manager__container">
+      <div className="manager__main">
+        <h2 className="page-title">{manager.fullName}</h2>
+        <div className="manager__contact-info-container">
+          {contactInfoFields.map((field, index) => (
+            <div className="manager__contact-info-container__row">
+              <span className="__row--title">
+                {contactInfoTitles[index]}
+              </span>
+              <span className="__row--value">
+                {manager[field]}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Manager;
