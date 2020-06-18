c42185def7ef0252a5778d35150484b79880819d
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-06-17 19:56:08 -0700

    add / to route in managers.js, set initial state in manager.js

--- src/views/manager.js
+++ src/views/manager.js
@@ -2,27 +2,32 @@ import React, { useEffect, useState } from "react";
 import UserContext from "../UserContext";
 import { Link, useParams } from "react-router-dom";
 import * as axios from "axios";
+import { PROPERTY_MANAGER_DATA } from "./dummyData/pManagerData";
 
 const Manager = () => {
 	const { id } = useParams();
-	const [manager, setManager] = useState({});
+	const dummyDataManagerInfo = PROPERTY_MANAGER_DATA.find(manager => manager.id === id);
+	
+  const [manager, setManager] = useState(dummyDataManagerInfo);
 
-	const getManager = (context) => {
-		axios
-			.get(`${process.env.REACT_APP_API_URL}/users/?id=${id}`, {
-				headers: { Authorization: `Bearer ${context.user.accessJwt}` },
-			})
-			.then((response) => {
-				setManager({ manager: response.data.manager });
-			})
-			.catch((error) => {
-				alert(error);
-				console.log(error);
-			});
-	};
+  // use getManager once /users/?id api endpoint returns properties and tenants for property managers
+  const getManager = (context) => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/users/?id=${id}`, {
+        headers: {
+          Authorization: `Bearer ${context.user.accessJwt}`,
+        },
+      })
+      .then((response) => {
+        setManager({ manager: response.data.manager });
+      })
+      .catch((error) => {
+        alert(error);
+        console.log(error);
+      });
+  };
 
-	console.log(manager);
-	return <div>manager {id}</div>;
+  return <div>manager</div>;
 };
 
 export default Manager;
