ca3c46cecdef96a11eb966bbf2a88691a36867d6
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-06-26 14:21:00 -0700

    add invite-button on managers.js, create _managers.scss

--- src/views/managers.js
+++ src/views/managers.js
@@ -4,126 +4,144 @@ import UserContext from "../UserContext";
 import { Link } from "react-router-dom";
 import * as axios from "axios";
 import { PROPERTY_MANAGER_DATA } from "./dummyData/pManagerData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const columns = [
-	{
-		dataField: "fullName",
-		formatter: (cell, row, rowIndex, formatExtraData) => {
-			return <Link key={row.id} to={`/manage/manager/${row.id}`}>{row.fullName}</Link>;
-		},
-		text: "Name",
-		sort: true,
-		headerStyle: () => {
-			return { width: "20%" };
-		},
-	},
-	{
-		dataField: "properties",
-		formatter: (cell, row, rowIndex, formatExtraData) => {
-			return <ul>
-				{row.properties.map(property => <li key={property}>{property}</li>)}
-			</ul>
-		},
-		text: "Properties",
-		sort: true,
-		headerStyle: () => {
-			return { width: "20%" };
-		},
-	},
-	{
-		dataField: "email",
-		text: "Email",
-		sort: true,
-		headerStyle: () => {
-			return { width: "20%" };
-		},
-	},
-	{
-		dataField: "status",
-		text: "Status",
-		sort: true,
-		headerStyle: () => {
-			return { width: "10%" };
-		},
-	},
-	{
-		dataField: "lastUsage",
-		text: "Last Usage",
-		sort: true,
-		headerStyle: () => {
-			return { width: "10%" };
-		},
-	},
+  {
+    dataField: "fullName",
+    formatter: (cell, row, rowIndex, formatExtraData) => {
+      return (
+        <Link key={row.id} to={`/manage/manager/${row.id}`}>
+          {row.fullName}
+        </Link>
+      );
+    },
+    text: "Name",
+    sort: true,
+    headerStyle: () => {
+      return { width: "20%" };
+    },
+  },
+  {
+    dataField: "properties",
+    formatter: (cell, row, rowIndex, formatExtraData) => {
+      return (
+        <ul>
+          {row.properties.map((property) => (
+            <li key={property}>{property}</li>
+          ))}
+        </ul>
+      );
+    },
+    text: "Properties",
+    sort: true,
+    headerStyle: () => {
+      return { width: "20%" };
+    },
+  },
+  {
+    dataField: "email",
+    text: "Email",
+    sort: true,
+    headerStyle: () => {
+      return { width: "20%" };
+    },
+  },
+  {
+    dataField: "status",
+    text: "Status",
+    sort: true,
+    headerStyle: () => {
+      return { width: "10%" };
+    },
+  },
+  {
+    dataField: "lastUsage",
+    text: "Last Usage",
+    sort: true,
+    headerStyle: () => {
+      return { width: "10%" };
+    },
+  },
 ];
 
 const selectRow = {
-	mode: "checkbox",
-	clickToSelect: true,
-	sort: true,
-	headerColumnStyle: () => {
-		return { width: "5%" };
-	},
+  mode: "checkbox",
+  clickToSelect: true,
+  sort: true,
+  headerColumnStyle: () => {
+    return { width: "5%" };
+  },
 };
 
 export class Managers extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			properties: [],
-		};
+    this.state = {
+      properties: [],
+    };
 
-		this.getProperties = this.getProperties.bind(this);
-	}
+    this.getProperties = this.getProperties.bind(this);
+  }
 
-	componentDidMount() {
-		this.getProperties(this.context);
-	}
+  componentDidMount() {
+    this.getProperties(this.context);
+  }
 
-	getProperties = (context) => {
-		axios
-			.get(`${process.env.REACT_APP_API_URL}/properties`, {
-				headers: { Authorization: `Bearer ${context.user.accessJwt}` },
-			})
-			.then((response) => {
-				this.setState({ properties: response.data.properties });
-			})
-			.catch((error) => {
-				alert(error);
-				console.log(error);
-			});
-	};
+  getProperties = (context) => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/properties`, {
+        headers: { Authorization: `Bearer ${context.user.accessJwt}` },
+      })
+      .then((response) => {
+        this.setState({ properties: response.data.properties });
+      })
+      .catch((error) => {
+        alert(error);
+        console.log(error);
+      });
+  };
 
-	render() {
-		return (
-			<UserContext.Consumer>
-				{(session) => {
-					this.context = session;
-					return (
-						<div className="properties__container">
-							<div className="section-header">
-								<h2 className="page-title">Properties</h2>
-								<Link className="button is-rounded" to="/add/property">
-									+ ADD NEW
-								</Link>
-							</div>
-							<div className="search-section">
-								<input></input>
-							</div>
-							<div className="properties-list">
-								<BootstrapTable
-									keyField="id"
-									data={PROPERTY_MANAGER_DATA}
-									columns={columns}
-									selectRow={selectRow}
-									bootstrap4={true}
-									headerClasses="table-header"
-								/>
-							</div>
-						</div>
-					);
-				}}
-			</UserContext.Consumer>
-		);
-	}
+  render() {
+    return (
+      <UserContext.Consumer>
+        {(session) => {
+          this.context = session;
+          return (
+            <div className="managers__container">
+              <div className="section-header">
+                <h2 className="page-title">Property Managers</h2>
+                <Link className="button is-rounded" to="/add/property">
+                  + ADD NEW
+                </Link>
+              </div>
+              <div className="search-section">
+                <input></input>
+              </div>
+              {/* What's the expected onClick behavior? */}
+              <div className="invite-button--container">
+                <button className="button is-rounded" type="submit">
+                  <FontAwesomeIcon
+                    className="button__envelope-icon"
+                    icon={faEnvelope}
+                  />{" "}
+                  Invite
+                </button>
+              </div>
+              <BootstrapTable
+                keyField="id"
+                data={PROPERTY_MANAGER_DATA}
+                columns={columns}
+                selectRow={selectRow}
+                bootstrap4={true}
+                headerClasses="table-header"
+              />
+            </div>
+          );
+        }}
+      </UserContext.Consumer>
+    );
+  }
 }
