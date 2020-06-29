06d5df3a0cf232fd234444549ffbefebc415b4c6
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-06-29 15:24:37 -0700

    add styles to manager, render Formik when isEditing

--- src/views/manager.js
+++ src/views/manager.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import UserContext from "../UserContext";
-import { Link, useParams } from "react-router-dom";
+import { useFormik } from "formik";
+import { useParams } from "react-router-dom";
 import * as axios from "axios";
 import { PROPERTY_MANAGER_DATA } from "./dummyData/pManagerData";
 
@@ -12,6 +13,29 @@ const Manager = () => {
   );
 
   const [manager, setManager] = useState(dummyDataManagerInfo);
+  const [isEditing, setEditingStatus] = useState(false);
+
+  const handleEditToggle = () => setEditingStatus(!isEditing);
+  const formik = useFormik({
+    initialValues: {
+      firstName: manager.firstName,
+      lastName: manager.lastName,
+      phone: manager.phone,
+      email: manager.email,
+    },
+    onSubmit: (values) => {
+      // TODO: make POST to API with updatedValues
+      const updatedValues = JSON.stringify(values, null, 2);
+      setManager({
+        ...manager,
+        firstName: values.firstName,
+        lastName: values.lastName,
+        phone: values.phone,
+        email: values.email,
+      });
+      setEditingStatus(false);
+    },
+  });
 
   const contactInfoFields = ["firstName", "lastName", "phone", "email"];
   const contactInfoTitles = ["First Name", "Last Name", "Phone", "Email"];
@@ -34,21 +58,43 @@ const Manager = () => {
   };
 
   return (
+    // If isEditing = true render Formik version allowing user to edit
+    // Will need to handle submitting Formik, which I'll learn about
+    // Will need to handle validating fields, which I'll learn about
     <div className="manager__container">
       <div className="manager__main">
-        <h2 className="page-title">{manager.fullName}</h2>
-        <div className="manager__contact-info-container">
-          {contactInfoFields.map((field, index) => (
-            <div className="manager__contact-info-container__row">
-              <span className="__row--title">
-                {contactInfoTitles[index]}
-              </span>
-              <span className="__row--value">
-                {manager[field]}
-              </span>
-            </div>
-          ))}
+        <div className="page-title">
+          <h2>{manager.fullName}</h2>
+          <div className="rounded" onClick={handleEditToggle}>
+            <i className="fas fa-pen icon"></i>
+          </div>
         </div>
+        {isEditing ? (
+          <form onSubmit={formik.handleSubmit}>
+            {Object.keys(formik.values).map(value =>
+              <div key={value}>
+                <label htmlFor={value}>{value}</label>
+                <input 
+                  name={value}
+                  type="text"
+                  onChange={formik.handleChange}
+                  value={formik.values[value]}
+                />
+              </div>
+            )}
+            
+            <button type="submit">Save Changes</button>
+          </form>
+        ) : (
+          <div className="manager__contact-info-container">
+            {contactInfoFields.map((field, index) => (
+              <div className="manager__contact-info-container__row">
+                <span className="__row--title">{contactInfoTitles[index]}</span>
+                <span className="__row--value">{manager[field]}</span>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
