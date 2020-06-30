affde61ca7bc883bed95851b8c251568ad190a35
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-06-30 11:54:41 -0700

    convert useFormik to <Formik />

--- src/views/manager.js
+++ src/views/manager.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import UserContext from "../UserContext";
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import { useParams } from "react-router-dom";
 import * as axios from "axios";
 import { PROPERTY_MANAGER_DATA } from "./dummyData/pManagerData";
@@ -16,26 +16,6 @@ const Manager = () => {
   const [isEditing, setEditingStatus] = useState(false);
 
   const handleEditToggle = () => setEditingStatus(!isEditing);
-  const formik = useFormik({
-    initialValues: {
-      firstName: manager.firstName,
-      lastName: manager.lastName,
-      phone: manager.phone,
-      email: manager.email,
-    },
-    onSubmit: (values) => {
-      // TODO: make POST to API with updatedValues
-      const updatedValues = JSON.stringify(values, null, 2);
-      setManager({
-        ...manager,
-        firstName: values.firstName,
-        lastName: values.lastName,
-        phone: values.phone,
-        email: values.email,
-      });
-      setEditingStatus(false);
-    },
-  });
 
   const contactInfoFields = ["firstName", "lastName", "phone", "email"];
   const contactInfoTitles = ["First Name", "Last Name", "Phone", "Email"];
@@ -58,9 +38,6 @@ const Manager = () => {
   };
 
   return (
-    // If isEditing = true render Formik version allowing user to edit
-    // Will need to handle submitting Formik, which I'll learn about
-    // Will need to handle validating fields, which I'll learn about
     <div className="manager__container">
       <div className="title__section">
         <h2>{manager.fullName}</h2>
@@ -70,43 +47,84 @@ const Manager = () => {
       </div>
       <div className="contact__section">
         <h1 className="section-title">CONTACT</h1>
-        {isEditing ? (
-          <form
-            className="contact-details"
-            onSubmit={formik.handleSubmit}
-          >
-            {Object.keys(formik.values).map((value, index) => (
-              <div className="form-row columns" key={value}>
-                <label
-                  className="column is-one-quarter row-title"
-                  htmlFor={value}
-                >
-                  {contactInfoTitles[index]}
-                </label>
-                <input
-                  className="column is-three-quarters contact-form__field--editing"
-                  name={value}
-                  type="text"
-                  onChange={formik.handleChange}
-                  value={formik.values[value]}
-                />
-              </div>
-            ))}
-
-            <button type="submit">Save Changes</button>
-          </form>
-        ) : (
-          <div className="contact-details">
-            {contactInfoFields.map((field, index) => (
-              <div className="form-row columns">
-                <span className="column is-one-quarter row-title">
-                  {contactInfoTitles[index]}
-                </span>
-                <span className="column is-one-quarter contact-form__field--not-editing">{manager[field]}</span>
-              </div>
-            ))}
-          </div>
-        )}
+        <div className="contact-details">
+          {isEditing ? (
+            <Formik
+              initialValues={{
+                firstName: manager.firstName,
+                lastName: manager.lastName,
+                phone: manager.phone,
+                email: manager.email,
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                setSubmitting(true);
+                setManager({
+                  ...manager,
+                  firstName: values.firstName,
+                  lastName: values.lastName,
+                  phone: values.phone,
+                  email: values.email,
+                });
+                setEditingStatus(false);
+                {
+                  /* fake API call to update data */
+                }
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 500);
+              }}
+              validationSchema={validationSchema}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+              }) => (
+                <form onSubmit={handleSubmit}>
+                  {Object.keys(values).map((value, index) => (
+                    <div className="form-row columns" key={value}>
+                      <label
+                        className="column is-one-quarter row-title"
+                        htmlFor={value}
+                      >
+                        {contactInfoTitles[index]}
+                      </label>
+                      <input
+                        type="text"
+                        name={value}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values[value]}
+                        className="column is-three-quarters contact-form__field--editing"
+                      />
+                    </div>
+                  ))}
+                  <button type="submit" disable={isSubmitting}>
+                    Save Changes
+                  </button>
+                </form>
+              )}
+            </Formik>
+          ) : (
+            <>
+              {contactInfoFields.map((field, index) => (
+                <div className="form-row columns">
+                  <span className="column is-one-quarter row-title">
+                    {contactInfoTitles[index]}
+                  </span>
+                  <span className="column is-one-quarter contact-form__field--not-editing">
+                    {manager[field]}
+                  </span>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
