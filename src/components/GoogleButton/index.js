400bf7b57bced52d9b93bfba14344fb39296e6b4
marcusjensen15 <marcusjensen1515@gmail.com>
2020-08-31 11:39:27 -0700

    write documentation for Google Button component

--- src/components/GoogleButton/index.js
+++ src/components/GoogleButton/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './googlebutton.scss';
 
-//Write documentation here
+//Documentation
+
+// Simple asthetic component, can be exported as <GoogleButton/>
+// Component accepts one prop, called 'innerText'. 
+// innerText will either be 'LOG IN WITH GOOGLE' or 'SIGN UP WITH GOOGLE' depending on where it is imported
+// You can see working example within: src/views/login.js
 
 
 function GoogleButton(props){
@@ -17,9 +22,7 @@ return (
     <span className="login__button__google__text">
       {innerText}
     </span>
-
   </button>
-
     );
 };
 
