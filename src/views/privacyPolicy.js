8fae676eca081781e0c8cd7154e428833491115f
marcusjensen15 <marcusjensen1515@gmail.com>
2020-09-03 11:58:31 -0700

    mobile header now looking good

--- src/views/privacyPolicy.js
+++ src/views/privacyPolicy.js
@@ -1,12 +1,15 @@
 import React from "react";
+import dwellinglyLogoMobile from "../assets/images/dwellingly_logo_white.png";
+
 
 
 const PrivacyPolicy = () => {
 
 return(
-  <div>
-
-    <p>Hello from PrivacyPolicy</p>
+  <div className-="privacy-policy__container">
+    <div className = "privacy-policy__mobile-header">
+      <img className= "privacy-policy__mobile__logo" src={dwellinglyLogoMobile}></img>
+    </div>
   </div>
 
 
