5d780d94ef33f6e318defff8ba8fa497b336954f
marcusjensen15 <marcusjensen1515@gmail.com>
2020-09-03 14:42:10 -0700

    filled in privacy policy with placeholder text. everything looking good

--- src/views/privacyPolicy.js
+++ src/views/privacyPolicy.js
@@ -1,5 +1,6 @@
 import React from "react";
 import dwellinglyLogoMobile from "../assets/images/dwellingly_logo_white.png";
+import { Link } from "react-router-dom";
 
 
 
@@ -36,12 +37,42 @@ return(
           </ul>
           </p>
       <h4 className="privacy-policy__heading">PERSONAL INFORMATION WE COLLECT</h4>
+
+        <p className="privacy-policy__text"> 
+                I'm baby blog taxidermy roof party activated charcoal man bun. 
+              Fashion axe ramps actually, chicharrones flannel kickstarter mumblecore chartreuse kombucha shaman. 
+              Food truck lo-fi bespoke, twee activated charcoal disrupt DIY church-key cornhole chicharrones. 
+              Asymmetrical poke seitan lumbersexual authentic paleo keffiyeh trust fund marfa iceland letterpress you probably haven't heard of them. 
+              Artisan irony art party fixie vinyl, gochujang mustache yr succulents unicorn letterpress meggings craft beer kinfolk. 
+              Iceland everyday carry farm-to-table blue bottle direct trade messenger bag. 
+        </p>
       <h4 className="privacy-policy__heading">HOW DO WE USE YOUR PERSONAL INFORMATION?</h4>
+
+        <p className="privacy-policy__text"> 
+                I'm baby blog taxidermy roof party activated charcoal man bun. 
+              Fashion axe ramps actually, chicharrones flannel kickstarter mumblecore chartreuse kombucha shaman. 
+              Food truck lo-fi bespoke, twee activated charcoal disrupt DIY church-key cornhole chicharrones. 
+              Asymmetrical poke seitan lumbersexual authentic paleo keffiyeh trust fund marfa iceland letterpress you probably haven't heard of them. 
+              Artisan irony art party fixie vinyl, gochujang mustache yr succulents unicorn letterpress meggings craft beer kinfolk. 
+              Iceland everyday carry farm-to-table blue bottle direct trade messenger bag. 
+        </p>
       <h4 className="privacy-policy__heading">SHARING YOUR PERSONAL INFORMATION</h4>
+
+        <p className="privacy-policy__text"> 
+                I'm baby blog taxidermy roof party activated charcoal man bun. 
+              Fashion axe ramps actually, chicharrones flannel kickstarter mumblecore chartreuse kombucha shaman. 
+              Food truck lo-fi bespoke, twee activated charcoal disrupt DIY church-key cornhole chicharrones. 
+              Asymmetrical poke seitan lumbersexual authentic paleo keffiyeh trust fund marfa iceland letterpress you probably haven't heard of them. 
+              Artisan irony art party fixie vinyl, gochujang mustache yr succulents unicorn letterpress meggings craft beer kinfolk. 
+              Iceland everyday carry farm-to-table blue bottle direct trade messenger bag. 
+        </p>
 
 
     </div>
 
+    <div className="privacy-policy__signUpWrapper"> 
+       <div className="privacy-policy__signUpText"><Link to="/signup" >Sign Up</Link></div> 
+    </div>
 
   </div>
 
