471e19f3b2a5b3d20583a032ce5deaa4028f6ff0
Darren Bridenbeck <darren.bridenbeck@gmail.com>
2020-09-15 14:47:16 -0700

    finish styling, finish tests, move ForgotPassword into folder

--- src/views/ForgotPassword/index.js
+++ src/views/ForgotPassword/index.js
@@ -1,24 +1,11 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { Form, Field, Formik } from "formik";
 import { Link } from "react-router-dom";
 import * as Yup from "yup";
-import UserContext from "../UserContext";
 import * as axios from "axios";
-import Modal from "../components/Modal";
-import Button from "../components/Button";
-import dwellinglylogo from "../assets/images/dwellingly_logo_white.png";
-
-//TODO:
-// get the top line in the email field to go away - COME BACK TO THIS / NOTE IN PR
-// move the request button to the left - COME BACK TO THIS AFTER FORM COMPONENT IS MERGED - Ask anna about this
-// recover account link functionality ... ? - OH BOY!? Scratch head later on that one
-
-// Hook up API call to send email password and use response to generate modal text - YES!
-// 2. Do a database lookup to see if that email exists - THIS IS BLOCKED. NEED TO UPDATE USER RESOURCE'S GET TO ACCEPT EMAIL AS PARAMETER
-// 3. If it exists, generate modal text with a "hey check your email" kind of message
-// 4. If it doesn't exist, generate modal text with a "no email found" kind of message
-// 5. If it exists, you also need to send an email, dummy! What goes in that email? Ask Chris or Hugh.
-// 6. How the f-word do we generate a password? is this a backend thing?
+import Modal from "../../components/Modal";
+import Button from "../../components/Button";
+import dwellinglylogo from "../../assets/images/dwellingly_logo_white.png";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string()
@@ -27,7 +14,6 @@ const validationSchema = Yup.object().shape({
 });
 
 const ForgotPassword = () => {
-  const context = useContext(UserContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState({
     titleText: "",
@@ -35,29 +21,23 @@ const ForgotPassword = () => {
     hasButtons: false,
   });
 
-  const formHandler = (data) => {
-    console.log("its handling its biz!");
-    setModalContent({
-      titleText: "Success!",
-      contentText: "Please check your email to reset your password.",
-    });
-    setIsModalOpen(true);
-    //   axios
-    //     .post("http://localhost:5000/api/forgot-password", data)
-    //     .then(response => {
-    //       setModalContent({
-    //        titleText: "Success!",
-    //        contentText: "Please check your email to complete the process to reset your password."
-    //      });
-    //       setIsModalOpen(true);
-    //     })
-    //     .catch(error => {
-    //       setModalContent({
-    //        titleText: "Error!",
-    //        contentText: "I'm sorry, there was an error attempting to reset your password."
-    //      });
-    //       setIsModalOpen(true);
-    //     });
+  const formHandler = (data) => {    
+    axios
+      .post("http://localhost:5000/api/reset-password", data)
+      .then(response => {
+        setModalContent({
+          titleText: "Success!",
+          contentText: "Please check your email to reset your password."
+        });
+        setIsModalOpen(true);
+      })
+      .catch(error => {
+        setModalContent({
+          titleText: "Error!",
+          contentText: "I'm sorry, there was an error attempting to reset your password."
+        });
+        setIsModalOpen(true);
+      });
   };
 
   const closeModal = () => {
@@ -145,14 +125,15 @@ const ForgotPassword = () => {
             </div>
           )}
         </Formik>
-        {/* LEFT OFF TRYING TO STYLE THIS! */}
         <div className="forgot-password__google-users">
           <h1 className="forgot-password__section-title">
             GOOGLE ACCOUNT USERS
           </h1>
-          <Link to="">Recover your Google account</Link>
+          <a href="https://accounts.google.com/signin/recovery">
+            Recover your Google account
+          </a>
         </div>
-        <Link to="/login" className={"forgot-password__login-link"}>
+        <Link to="/login">
           Return to log in
         </Link>
       </div>
