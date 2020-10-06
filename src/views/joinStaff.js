122af3640ea4a1139a26d933d4f23d7c805ca238
github username <souzajohnlee@gmail.com>
2020-10-05 18:56:16 -0700

    fetch staff and admin data

--- src/views/joinStaff.js
+++ src/views/joinStaff.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { JoinStaffCard } from '../components/JoinStaffCard';
 import { JOIN_STAFF_DATA } from '../components/JoinStaffCard/data';
 import { Link } from 'react-router-dom';
@@ -7,6 +7,18 @@ import { Link } from 'react-router-dom';
 export const JoinStaff = () => {
 	const secondColumnStart = Math.floor(JOIN_STAFF_DATA.length / 3);
 	const thirdColumnStart = JOIN_STAFF_DATA.length - Math.floor(JOIN_STAFF_DATA.length / 3);
+
+	useEffect(() => {
+		const URLs = ['/api/user?r=3', '/api/user?r=4']
+		const fetchData = URL => {
+			return axios
+				.get(URL, auth_headers)
+				.then( res => {
+					setStaff(...staff, res.data.users)
+				})
+		}
+		Promise.all(URLs.map(url => fetchData(url)))
+	}, [])
 
 	return (
 		<>
