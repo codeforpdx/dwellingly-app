2933d912f954c1abdd0102846ecf757a2dd0cdce
github username <souzajohnlee@gmail.com>
2020-10-09 21:53:50 -0700

    correct import path

--- src/views/joinStaff.js
+++ src/views/joinStaff.js
@@ -3,7 +3,7 @@ import UserContext from '../UserContext';
 import axios from 'axios'
 import { JoinStaffCard } from '../components/JoinStaffCard';
 import { Link } from 'react-router-dom';
-import RoleEnum from './Enums/RoleEnum.js';
+import RoleEnum from '../Enums/RoleEnum.js';
 
 
 export const JoinStaff = () => {
