da0da469ed9e2e06798242abe7f0e97431000945
Scott <seharlan@gmail.com>
2020-10-14 20:20:55 -0700

    logout button color

--- src/components/Header/LogOutButton.js
+++ src/components/Header/LogOutButton.js
@@ -4,7 +4,7 @@ import UserContext from "../../UserContext";
 const LogOutButton = () => (
   <UserContext.Consumer>
     {({ logout }) => (
-      <button className="button is-dark is-rounded is-small" onClick={logout}>
+      <button className="button is-info is-rounded is-small" onClick={logout}>
         LOG OUT
       </button>
     )}
