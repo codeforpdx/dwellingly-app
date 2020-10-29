4514062d0cfb4e057383c3c5b15b39ad2ed06ebc
Ben Beekman <bbeekman@gmail.com>
2020-10-28 19:27:01 -0700

    noMatch page created

---
+++ src/views/noMatch/noMatch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const noMatch = () => {
+  return (
+    <>
+      <h1>404</h1>
+      <p>Beep, boop. Sorry, but no page matching your request was found.</p>
+    </>
+  );
+};
+
+export default noMatch;
