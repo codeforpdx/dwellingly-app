68b247ef398d41921dca0d66f08f40ac6d3536fc
Ben Beekman <bbeekman@gmail.com>
2020-10-28 20:08:29 -0700

    changed jsx file to js to match others

---
+++ src/views/noMatch/noMatch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const noMatch = () => {
+  return (
+    <>
+      <div className="noMatch__container">
+        <h1>404</h1>
+        <p>Beep, boop. Sorry, but no page matching your request was found.</p>
+      </div>
+    </>
+  );
+};
+
+export default noMatch;
