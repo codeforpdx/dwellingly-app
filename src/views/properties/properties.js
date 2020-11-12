8b6f70f05b9ede0b324bb82d614397704316ffda
Chris Boe <cboe@navexglobal.com>
2020-11-11 19:24:46 -0800

    Fix broken proeprties grid

--- src/views/properties/properties.js
+++ src/views/properties/properties.js
@@ -15,7 +15,7 @@ const columns = [{
     return { width: "20%" };
   }
 }, {
-  dataField: 'propertyManager',
+  dataField: 'propertyManagerNames',
   text: 'Property Managers',
   sort: true,
   headerStyle: () => {
@@ -85,8 +85,16 @@ export class Properties extends Component {
     axios.get("/api/properties", { headers: { "Authorization": `Bearer ${context.user.accessJwt}` } })
       .then((response) => {
         const { data: { properties } } = response;
-        properties.forEach(property => property.totalTenants = property.tenantIDs.length)
-        this.setState({ properties: properties });
+        let propertyRows = properties.map(property => ({
+          id: property.id,
+          name: property.name,
+          propertyManagerNames: property.propertyManagerName &&
+            property.propertyManagerName.join(", "),
+          address: property.address,
+          totalTenants: property.tenantIDs && property.tenantIDs.length,
+          created_at: property.created_at
+        }) );
+        this.setState({ properties: propertyRows });
       })
       .catch((error) => {
         alert(error);
@@ -107,7 +115,8 @@ export class Properties extends Component {
               </div>
 
               <Search
-                input={this.state.properties} outputLocation={this.state.filteredProperties}
+                input={this.state.properties}
+                outputLocation={this.state.filteredProperties}
                 isFilteredLocation={this.state.isFiltered}
                 setIsFilteredStateFalse={this.setIsFilteredPropertiesFalse}
                 setOutputState={this.setOutputState}
