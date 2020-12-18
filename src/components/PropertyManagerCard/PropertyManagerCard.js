4ef4cceda13edeacc320f2c6fa9a418e511783d1
Paul Stevens <twinkisan@hotmail.com>
2020-12-18 15:14:38 -0800

    allows edits of property information

--- src/components/PropertyManagerCard/PropertyManagerCard.js
+++ src/components/PropertyManagerCard/PropertyManagerCard.js
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 import './PropertyManagerCard.scss';
 
 
-function PropertyManagerCard({ manager }) {
-
+function PropertyManagerCard({ manager, isEditing }) {
+  console.log(isEditing)
   return (
     <div className="property-manager-box box">
+
       <Link key={manager.id} to={`/manage/manager/${manager.id}`}>
         <p className="bold">{`${manager.firstName} ${manager.lastName}`}</p>
       </Link>
+
       <p className="information">{manager.phone}</p>
       <p className="information">{manager.email}</p>
 
