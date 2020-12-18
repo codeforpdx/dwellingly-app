bf3b6fa4198fedaaaf3ee4455ff8cc2a8c5d130c
Paul Stevens <twinkisan@hotmail.com>
2020-12-17 16:31:51 -0800

    adds first editable form

--- src/views/Property/PropertyView.js
+++ src/views/Property/PropertyView.js
@@ -3,8 +3,26 @@ import { useParams } from "react-router-dom";
 import * as axios from "axios";
 import Toast from '../../utils/toast';
 import UserContext from '../../UserContext';
-import Tenant from "../Tenant";
+import * as Yup from "yup";
+import ToggleEditTable from "../../components/ToggleEditTable";
+import { useCalendarState } from "../../components/CalendarModal/CalendarModal";
 
+
+const validationSchema = Yup.object().shape({
+  propertyName: Yup.string()
+    .max(255, "Must be shorter than 255 Characters")
+    .required("Must enter Property Name"),
+  propertyAddress: Yup.string()
+    .max(255, "Must be shorter than 255 Characters")
+    .required("Address is required"),
+  propertyUnits: Yup.string()
+    .min(
+      0,
+      "*Property must contain at least 1 unit",
+    )
+    .max(3, "*Numbers can't be longer than 3 digits")
+    .required("*Number of units is required"),
+});
 
 
 const Property = () => {
@@ -13,12 +31,11 @@ const Property = () => {
   const { id } = useParams();
   const propertyName = id;
 
-  const initialState = {
-    property: null,
-  }
 
-  const [state, setState] = useState(initialState);
   const [isEditing, setEditingStatus] = useState(false);
+  const [property, setProperty] = useState('');
+  const calendarState = useCalendarState(property?.dateTimeStart, property?.dateTimeEnd)
+
 
   // Error handler for axios requests
   const axiosErrorHandler = (error) => {
@@ -42,20 +59,53 @@ const Property = () => {
     setEditingStatus(false);
   };
 
-  const getProperty = async () => {
 
-    const propertyResponse = await axios.get(`${process.env.REACT_APP_PROXY}/api/properties/${propertyName}`, { headers: { Authorization: `Bearer ${userContext.user.accessJwt}` } });
-    const property = propertyResponse.data;
-
-    setState({ property })
-
-  }
 
   useEffect(() => {
-    getProperty();
-  }, []);
+    const getProperty = async () => {
 
-  const getTableData = () => [
+      const propertyResponse = await axios.get(`${process.env.REACT_APP_PROXY}/api/properties/${propertyName}`, { headers: { Authorization: `Bearer ${userContext.user.accessJwt}` } });
+      const property = propertyResponse.data;
+
+      setProperty(property)
+    }
+
+    getProperty();
+  });
+
+  const onFormikSubmit = (values, { setSubmitting }) => {
+    setSubmitting(true);
+    const newValues = {
+      name: values.propertyName,
+      address: values.propertyAddress,
+      units: values.propertyUnits,
+    };
+    updateProperty(newValues);
+  };
+
+  const updateProperty = (payload) => {
+    axios
+      .patch(`${process.env.REACT_APP_PROXY}/api/properties/${payload.name}`,
+        payload,
+        { Authorization: `Bearer ${userContext.user.accessJwt}` }
+      )
+      .then(response => {
+        setProperty({
+          ...property,
+          name: response.data.name,
+          address: response.data.address,
+          units: response.data.units,
+        });
+        setEditingStatus(false);
+        Toast("Save successful!");
+      })
+      .catch((error) => {
+        Toast(error.message);
+      });
+  };
+
+
+  const getTableData = [
     {
       key: "propertyName",
       label: "Name",
@@ -78,7 +128,7 @@ const Property = () => {
   ]
 
 
-  const { property } = state;
+  // const { property } = state;
 
   return (
     <div className='main-container'>
@@ -101,7 +151,7 @@ const Property = () => {
               <h2 className="section-title">PROPERTY INFORMATION</h2>
             </div>
             <ToggleEditTable
-              tableData={getTableData()}
+              tableData={getTableData}
               validationSchema={validationSchema}
               isEditing={isEditing}
               submitHandler={onFormikSubmit}
