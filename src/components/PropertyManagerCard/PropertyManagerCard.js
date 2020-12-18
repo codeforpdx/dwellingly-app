6eb804c7badb0839d0bbcf07514529b9aa53f3ee
Paul Stevens <twinkisan@hotmail.com>
2020-12-18 09:38:04 -0800

    adds link to PropertyManagerCard

--- src/components/PropertyManagerCard/PropertyManagerCard.js
+++ src/components/PropertyManagerCard/PropertyManagerCard.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 
 function PropertyManagerCard({ manager }) {
 
   return (
     <>
-      <div>
+      <Link key={manager.id} to={`/manage/manager/${manager.id}`}>
         <p>{`${manager.firstName} ${manager.lastName}`}</p>
         <p>{manager.phone}</p>
         <p>{manager.email}</p>
-      </div>
+      </Link>
     </>
   )
 };
