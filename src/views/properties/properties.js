3fb9b8d063aa289e6deb06bf3f972dea30fdb46a
Dave Trost <davetrost@gmail.com>
2021-02-10 20:46:03 -0800

    functional component refactor!
    Maintain consistent table sizing even when no rows are displayed - use min-width

--- src/views/properties/properties.js
+++ src/views/properties/properties.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext, useState } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import UserContext from '../../UserContext';
 import { Link } from "react-router-dom";
@@ -7,9 +7,12 @@ import Search from '../../components/Search';
 import { ShowHideSwitch } from '../../components/ShowHideSwitch';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArchive } from '@fortawesome/free-solid-svg-icons';
+import useMountEffect from '../../utils/useMountEffect';
 import Toast from '../../utils/toast';
 
 import './properties.scss';
+
+const makeAuthHeaders = ({ user }) => ({ headers: { 'Authorization': `Bearer ${user.accessJwt}` } });
 
 const columns = [{
   dataField: 'name',
@@ -56,174 +59,137 @@ const columns = [{
   }
 }];
 
-export class Properties extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      properties: [],
-      isSearchActive: false,
-      showArchived: false,
-      searchedProperties: [],
-      displayProperties: [],
-      selectedProperties: [],
-      checkboxRenderCount: 0,
-    };
+const getDisplayProperties = (properties, showArchived) => properties.filter(p => showArchived || !p.archived);
 
-    this.getProperties = this.getProperties.bind(this);
-  }
+const formatPropertyData = (properties) => properties.map(p => ({
+  id: p.id,
+  archived: p.archived,
+  name: p.name,
+  propertyManagerNames: p.propertyManagerName && p.propertyManagerName.join(", "),
+  address: p.address,
+  totalTenants: p.tenantIDs && p.tenantIDs.length,
+  created_at: p.created_at
+}));
 
-  getDisplayProperties = (properties, showArchived) => properties.filter(p => showArchived || !p.archived);
-  
-  disableSearch = () => {
-    this.setState({ 
-      searchedProperties: this.state.properties,
-      isSearchActive: false,
-      displayProperties: this.getDisplayProperties(this.state.properties, this.state.showArchived),
-    });
+
+export const Properties = () => {
+  const userContext = useContext(UserContext);
+
+  const [allProperties, setAllProperties] = useState([]);
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [showArchived, setShowArchived] = useState(false);
+  const [searchedProperties, setSearchedProperties] = useState([]);
+  const [displayProperties, setDisplayProperties] = useState([]);
+  const [selectedProperties, setSelectedProperties] = useState([]);
+  const [checkboxRenderCount, setCheckboxRenderCount] = useState(0);
+
+  const handleToggleArchived = () => { 
+    const newShowArchived = !showArchived
+    setShowArchived(newShowArchived);
+    setDisplayProperties(getDisplayProperties(searchedProperties, newShowArchived));
   };
-  handleSearchOutput = (output, isTrue) => {
-    this.setState({
-      searchedProperties: output,
-      isSearchActive: isTrue,
-      displayProperties: this.getDisplayProperties(output, this.state.showArchived),
-    });
+  const handleDisableSearch = () => {
+    setSearchedProperties(allProperties);
+    setIsSearchActive(false);
+    setDisplayProperties(getDisplayProperties(allProperties, showArchived));
   };
-  
-  handleToggleArchived = () => this.setState({ 
-    showArchived: !this.state.showArchived,
-    displayProperties: this.getDisplayProperties(this.state.searchedProperties, !this.state.showArchived),
-  });
+  const handleSearchOutput = (output, isTrue) => {
+    setSearchedProperties(output);
+    setIsSearchActive(isTrue);
+    setDisplayProperties(getDisplayProperties(output, showArchived));
+  };
+  const handleSelectRow = (property) => setSelectedProperties([...selectedProperties, property]);
+  const handleDeselectRow = (property) => setSelectedProperties(selectedProperties.filter(p => p.id !== property.id));
+  const handleSelectAll = setSelectedProperties;
+  const handleDeselectAll = (_) => setSelectedProperties([]);
 
-  setSelectedProperties = selectedProperties => {
-    this.setState({ selectedProperties });
-  }
-  addToSelectedProperties = property => {
-    this.setState({ selectedProperties: [...this.state.selectedProperties, property] });
-  }
-  removeFromSelectedProperties = property => {
-    this.setState({ selectedProperties: this.state.selectedProperties.filter(p => p.id !== property.id) });
-  }
 
-  selectRow = {
-    mode: 'checkbox',
-    clickToSelect: true,
-    onSelect: (row, isSelect) => isSelect
-      ? this.addToSelectedProperties(row) 
-      : this.removeFromSelectedProperties(row)
-    ,
-    onSelectAll: (isSelect, rows) => isSelect
-      ? this.setSelectedProperties(rows)
-      : this.setSelectedProperties([]) 
-    ,
-    sort: true,
-    headerColumnStyle: () => {
-      return { width: "5%" };
-    }
-  }
-  
-  componentDidMount() {
-    this.getProperties(this.context);
-  }
-
-  setTableDataFromProperties = (properties) => {
-    const propertyRows = properties.map(property => ({
-      id: property.id,
-      archived: property.archived,
-      name: property.name,
-      propertyManagerNames: property.propertyManagerName && property.propertyManagerName.join(", "),
-      address: property.address,
-      totalTenants: property.tenantIDs && property.tenantIDs.length,
-      created_at: property.created_at
-    }));
-    this.setState({ 
-      properties: propertyRows,
-      searchedProperties: propertyRows,
-      displayProperties: this.getDisplayProperties(propertyRows, this.state.showArchived),
-    });
-  }
-
-  getProperties = (context) => {
-    axios.get("/api/properties", { headers: { "Authorization": `Bearer ${context.user.accessJwt}` } })
+  useMountEffect(() => {
+    axios.get("/api/properties", makeAuthHeaders(userContext))
       .then((response) => {
         const { data: { properties } } = response;
-        this.setTableDataFromProperties(properties)
+        const propertyRows = formatPropertyData(properties)
+        setAllProperties(propertyRows);
+        setSearchedProperties(propertyRows);
+        setDisplayProperties(getDisplayProperties(propertyRows, showArchived));
       })
       .catch((error) => {
         Toast(error.message, "error");
-      })
-  }
+      });
+  });
 
-  archiveProperties = () => {
-    const propertyIds = this.state.selectedProperties.map(p => p.id);
-    axios.patch(`/api/properties/archive`, { ids: propertyIds }, { headers: { "Authorization": `Bearer ${this.context.user.accessJwt}` } })
+
+  const archiveProperties = () => {
+    const propertyIds = selectedProperties.map(p => p.id);
+    axios.patch(`/api/properties/archive`, { ids: propertyIds }, makeAuthHeaders(userContext))
       .then((response) => {
         Toast(`Property Archived.`);
         const { data: { properties } } = response;
-        this.setTableDataFromProperties(properties)
-        this.setState({ 
-          selectedProperties: [],
-          checkboxRenderCount: this.state.checkboxRenderCount + 1
-        });
+        const propertyRows = formatPropertyData(properties);
+        setAllProperties(propertyRows);
+        setSearchedProperties(propertyRows);
+        setDisplayProperties(getDisplayProperties(propertyRows, showArchived));
+        setSelectedProperties([]);
+        setCheckboxRenderCount(checkboxRenderCount + 1);
       })
       .catch((error) => {
         Toast(error.message, "error");
       })
   }
 
-  render() {
-    return (
-      <UserContext.Consumer>
-        {session => {
-          this.context = session;
-          return (
-            <div className='main-container'>
-              <div className="properties">
-                <div className="section-header">
-                  <h2 className="page-title">Properties</h2>
-                  <Link className="button is-primary is-rounded ml-4" to="/add/property">+ ADD NEW</Link>
-                </div>
-                <div className="search-and-archive-container">
-                
-                  <Search
-                    input={this.state.properties}
-                    outputLocation={this.state.searchedProperties}
-                    isFilteredLocation={this.state.isSearchActive}
-                    setIsFilteredStateFalse={this.disableSearch}
-                    setOutputState={this.handleSearchOutput}
-                    placeholderMessage="Search by name, address, or property manager"
-                  />
-                  <ShowHideSwitch labelText="Archived:" isShowState={this.state.showArchived} handleToggleChange={this.handleToggleArchived}/>
-                </div>
-                <div className='bulk-actions-container py-3'>
-                  <button 
-                    className={`button is-rounded is-primary ml-3 ${this.state.selectedProperties.length && 'is-active-button'}`}
-                    onClick={this.archiveProperties}
-                  >
-                    <FontAwesomeIcon
-                      className="mr-3"
-                      icon={faArchive}
-                    />
-                    Archive Properties
-                  </button>
-                </div>
-                <div className="properties-list">
-                  <BootstrapTable
-                    key={`tables-of-properties--${this.state.checkboxRenderCount}`}
-                    keyField='id'
-                    data={this.state.displayProperties}
-                    columns={columns}
-                    selectRow={this.selectRow}
-                    defaultSortDirection="asc"
-                    bootstrap4={true}
-                    headerClasses="table-header"
-                  />
-                </div>
-              </div>
-            </div>
-          );
-        }}
-      </UserContext.Consumer>
-    );
-  }
+
+  return (
+    <div className='main-container'>
+      <div className="properties">
+        <div className="section-header">
+          <h2 className="page-title">Properties</h2>
+          <Link className="button is-primary is-rounded ml-4" to="/add/property">+ ADD NEW</Link>
+        </div>
+        <div className="search-and-archive-container">
+          <Search
+            input={allProperties}
+            outputLocation={searchedProperties}
+            isFilteredLocation={isSearchActive}
+            setIsFilteredStateFalse={handleDisableSearch}
+            setOutputState={handleSearchOutput}
+            placeholderMessage="Search by name, address, or property manager"
+          />
+          <ShowHideSwitch labelText="Archived:" isShowState={showArchived} handleToggleChange={handleToggleArchived}/>
+        </div>
+        <div className='bulk-actions-container py-3'>
+          <button 
+            className={`button is-rounded is-primary ml-3 ${selectedProperties.length && 'is-active-button'}`}
+            onClick={archiveProperties}
+          >
+            <FontAwesomeIcon
+              className="mr-3"
+              icon={faArchive}
+            />
+            Archive Properties
+          </button>
+        </div>
+        <div className="properties-list">
+          <BootstrapTable
+            key={`tables-of-properties--${checkboxRenderCount}`}
+            wrapperClasses='properties-list-wrapper'
+            keyField='id'
+            data={displayProperties}
+            columns={columns}
+            selectRow={({
+              mode: 'checkbox',
+              clickToSelect: true,
+              onSelect: (row, isSelect) => isSelect? handleSelectRow(row) : handleDeselectRow(row),
+              onSelectAll: (isSelect, rows) => isSelect? handleSelectAll(rows) : handleDeselectAll(rows),
+              sort: true,
+              headerColumnStyle: () => ({ width: "5%" }),
+            })}
+            defaultSortDirection="asc"
+            bootstrap4={true}
+            headerClasses="table-header"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
