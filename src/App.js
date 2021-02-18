5740313e26bd27379d41f03177becf5cfca9adb3
Dave Trost <davetrost@gmail.com>
2021-02-17 20:41:16 -0800

    import lines remove specification of extension (specifically for .jsx imports)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { NavMenu } from "./views/NavigationMenu/navigationMenu";
 import { Dashboard } from "./views/dashboard/dashboard";
 import { RequestAccess } from "./views/requestAccess/requestAccess";
 import { Properties } from "./views/properties/properties";
-import Property from "./views/Property/PropertyView.jsx"
+import Property from "./views/Property/PropertyView"
 import { Tenants } from "./views/tenants/tenants";
 import { Terms } from "./views/terms/terms";
 import { Tickets } from "./views/tickets/tickets";
