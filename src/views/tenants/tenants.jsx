d86d5b266ef5f0ae37fd3b9ca3e999b13629862a
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-03-24 13:46:50 -0700

    roverhauls tenants page to meet FIGMA requirements and adds ToolTip component

--- src/views/tenants/tenants.jsx
+++ src/views/tenants/tenants.jsx
@@ -1,140 +1,144 @@
-import React, { Component } from 'react';
+import React, { useContext, useState } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
-import UserContext from '../../UserContext';
 import axios from "axios";
 import { Link } from "react-router-dom";
-import Search from "../../components/Search/index";
+
+
+import UserContext from '../../UserContext';
+import useMountEffect from '../../utils/useMountEffect';
 import Toast from '../../utils/toast';
+import Search from "../../components/Search/index";
+import { ShowHideSwitch } from '../../components/ShowHideSwitch';
 
-const columns = [
-  {
-    dataField: "fullName",
-    formatter: (cell, row, rowIndex, formatExtraData) => {
-      return (
-        <Link key={row.id} to={`/manage/tenants/${row.id}`}>
-          {row.fullName}
-        </Link>
-      );
-    },
-    text: "Name",
-    sort: true,
-  },
-  {
-    dataField: "propertyName",
-    text: "Property",
-    sort: true,
-  },
-  {
-    dataField: "phone",
-    text: "Phone",
-    sort: true,
-  },
-];
+import { columns } from './tenantsFormStructure'
+import './tenants.scss'
 
-const selectRow = {
-  mode: "checkbox",
-  clickToSelect: true,
-  sort: true,
-  headerColumnStyle: () => {
-    return { width: "5%" };
-  },
-};
+const makeAuthHeaders = ({ user }) => ({ headers: { 'Authorization': `Bearer ${user.accessJwt}` } });
 
-// const pageButtonRenderer = ({
-//   page,
-//   active,
-//   disable,
-//   title,
-//   onPageChange
-// }) => {
-//   const handleClick = (e) => {
-//     e.preventDefault();
-//     onPageChange(page);
-//   };
-//   const activeStyle = {};
-//   if (active) {
-//     activeStyle.backgroundColor = 'black';
-//     activeStyle.color = 'white';
-//   } else {
-//     activeStyle.backgroundColor = 'gray';
-//     activeStyle.color = 'black';
-//   }
-//   if (typeof page === 'string') {
-//     activeStyle.backgroundColor = 'white';
-//     activeStyle.color = 'black';
-//   }
-//   return (
-//     <li className="page-item">
-//       <a href="#" onClick={ handleClick } style={ activeStyle }>{ page }</a>
-//     </li>
-//   );
-// };
+const addPropertyNames = ((tenants, allProperties) => {
+  return tenants.map(tenant => {
+    if (tenant.lease) {
+      const propertyIndex = allProperties.findIndex(property =>
+        property.id === tenant.lease.propertyID)
+      tenant.propertyName = allProperties[propertyIndex].name
+    }
+    return tenant
+  });
+});
 
-// const options = {
-//   pageButtonRenderer
-// };
+const formatTenantData = tenants => tenants.map(tenant => {
+  const { id, lease, phone, fullName } = tenant;
+  const staff = tenant.staff.map(staff => {
+    return `${staff.firstName} ${staff.lastName}`
+  })
+  return { id, lease, phone, fullName, staff }
+})
 
-export class Tenants extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      tenants: [],
-    };
-
-    this.getTenants = this.getTenants.bind(this);
+const sortTenantData = tenants => tenants.sort((a, b) => {
+  if (a.fullName < b.fullName) {
+    return -1;
   }
-
-  componentDidMount() {
-    this.getTenants(this.context);
+  if (a.fullName > b.fullName) {
+    return 1;
   }
+  return 0;
+})
 
-  getTenants = (context) => {
-    axios
-      .get(`/api/tenants`, {
-        headers: { Authorization: `Bearer ${context.user.accessJwt}` },
+const getDisplayTenants = (tenants, showHoused) =>
+  tenants.filter(tenant => showHoused || tenant.lease);
+
+export function Tenants() {
+  const userContext = useContext(UserContext)
+
+  const [allTenants, setAllTenants] = useState([]);
+  const [isSearchActive, setIsSearchActive] = useState([]);
+  const [showHoused, setShowHoused] = useState(false);
+  const [searchedTenants, setSearchedTenants] = useState([]);
+  const [displayTenants, setDisplayTenants] = useState([]);
+  // const [defaultSort, setDefaultSort] = useState(defaultSort)
+
+  useMountEffect(() => {
+    let allProperties;
+
+    axios.get(`/api/properties`, makeAuthHeaders(userContext))
+      .then(propertyResponse => {
+        const { data: { properties } } = propertyResponse;
+        allProperties = properties;
       })
+      .then(() =>
+        axios.get("/api/tenants", makeAuthHeaders(userContext))
+      )
       .then((response) => {
-        const tenants = response.data.tenants;
-        this.setState({ tenants });
+        const { data: { tenants } } = response;
+        const tenantRows = formatTenantData(tenants)
+        const tenantsWithProperties = addPropertyNames(tenantRows, allProperties)
+        const sortedTenants = sortTenantData(tenantsWithProperties)
+        setAllTenants(sortedTenants);
+        setSearchedTenants(sortedTenants);
+        setDisplayTenants(getDisplayTenants(sortedTenants, showHoused));
       })
       .catch((error) => {
-        Toast(error.message, "error");
-        console.log(error);
+        Toast(error.message, "error")
       });
-  };
+  })
 
-  render() {
-    return (
-      <UserContext.Consumer>
-        {session => {
-          this.context = session;
-          return (
-            <div className='main-container'>
-              <div>
-                <div className="section-header">
-                  <h2 className="page-title">Tenants</h2>
-                  <Link className="button is-primary is-rounded ml-4" to="/add/tenant">+ ADD NEW</Link>
-                </div>
-                <div className="search-section">
-                  <Search placeholderMessage="Search tenants by name, property, or JOIN staff" />
-                </div>
-                <div className="properties-list">
-                  <BootstrapTable
-                    keyField='id'
-                    data={this.state.tenants}
-                    columns={columns}
-                    selectRow={selectRow}
-                    bootstrap4={true}
-                    headerClasses="table-header"
-                    classes="table-responsive"
-                  />
-                </div>
-              </div>
-            </div>
-          );
-        }}
-      </UserContext.Consumer>
-    );
+  const handleToggleHoused = () => {
+    setShowHoused(!showHoused)
+    setDisplayTenants(getDisplayTenants(searchedTenants, !showHoused))
   }
-}
+
+  const handleDisableSearch = () => {
+    setSearchedTenants(allTenants);
+    setIsSearchActive(false);
+    setDisplayTenants(getDisplayTenants(allTenants, showHoused))
+  }
+
+  const handleSearchOutput = (output, isTrue) => {
+    setSearchedTenants(output);
+    setIsSearchActive(isTrue);
+    setDisplayTenants(getDisplayTenants(output, showHoused))
+  }
+
+  return (
+    <div className='main-container'>
+      <div className='tenants'>
+        <div className='section-header'>
+          <h2 className='page-title'>Tenants</h2>
+          <Link
+            className='button is-primary is-rounded ml-4'
+            to='/add/tenant'>+ ADD NEW</Link>
+        </div>
+        <div className='search-and-archive-container'>
+          <Search
+            input={allTenants}
+            outputLocation={searchedTenants}
+            isFilteredLocation={isSearchActive}
+            setIsFilteredStateFalse={handleDisableSearch}
+            setOutputState={handleSearchOutput}
+            placeholderMessage="Search tenants by name, property, or JOIN staff" />
+          <ShowHideSwitch
+            labelText="Unhoused:"
+            isShowState={showHoused}
+            handleToggleChange={handleToggleHoused}
+          />
+        </div>
+        <div className="tenants-list">
+          <BootstrapTable
+            key={`tables-of-tenants--${allTenants.length}`}
+            wrapperClasses='tenants-list-wrapper'
+            keyField='id'
+            data={displayTenants}
+            columns={columns}
+            defaultSorted={[
+              {
+                dataField: 'propertyName',
+                order: 'asc'
+              }]}
+            bootstrap4={true}
+            headerClasses='table-header'
+          />
+        </div>
+      </div>
+    </div>
+  )
+};
