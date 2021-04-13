6c3697425dee9b03ab32e11240c50b418835c689
Angie Rodriguez <angrodri902@gmail.com>
2021-04-13 00:02:55 -0700

    Refactor Tickets class component into functional component for hooks use

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import 'react-toastify/dist/ReactToastify.min.css';
+import "react-toastify/dist/ReactToastify.min.css";
 import "./App.scss";
-import axios from 'axios';
+import axios from "axios";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { LoginForm } from "./views/login/login";
 import SignupForm from "./views/signup/signup";
@@ -9,7 +9,7 @@ import { NavMenu } from "./views/NavigationMenu/navigationMenu";
 import { Dashboard } from "./views/dashboard/dashboard";
 import { RequestAccess } from "./views/requestAccess/requestAccess";
 import { Properties } from "./views/properties/properties";
-import Property from "./views/Property/PropertyView"
+import Property from "./views/Property/PropertyView";
 import { Tenants } from "./views/tenants/tenants";
 import { Terms } from "./views/terms/terms";
 import { Tickets } from "./views/tickets/tickets";
@@ -33,11 +33,11 @@ import Manager from "./views/Manager";
 import { JoinStaff } from "./views/joinStaff/joinStaff";
 import { AddStaffMember } from "./views/addStaffMember/addStaffMember";
 import { AddTenant } from "./views/AddTenant/addTenant";
-import { AddManager } from './views/addManager/addManager';
+import { AddManager } from "./views/addManager/addManager";
 import UserContext from "./UserContext";
 import Tenant from "./views/Tenant";
 import ChangePassword from "./views/Settings/changePassword";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import NoMatch from "./views/noMatch/noMatch";
 
 var refreshTimeout;
@@ -47,7 +47,7 @@ export class App extends React.Component {
     super(props);
 
     if (checkForStoredAccessToken()) {
-      let parsedJwt = parseJwt(window.localStorage['dwellinglyAccess']);
+      let parsedJwt = parseJwt(window.localStorage["dwellinglyAccess"]);
       this.state = {
         userSession: {
           isAuthenticated: true,
@@ -57,29 +57,28 @@ export class App extends React.Component {
           firstName: parsedJwt.user_claims.firstName,
           lastName: parsedJwt.user_claims.lastName,
           email: parsedJwt.user_claims.email,
-          phone: parsedJwt.user_claims.phone
-        }
+          phone: parsedJwt.user_claims.phone,
+        },
       };
-    }
-    else {
+    } else {
       this.state = {
         userSession: {
           isAuthenticated: false,
-          accessJwt: '',
-          refreshJwt: '',
-          identity: '',
-          firstName: '',
-          lastName: '',
-          email: '',
-          phone: '',
-        }
+          accessJwt: "",
+          refreshJwt: "",
+          identity: "",
+          firstName: "",
+          lastName: "",
+          email: "",
+          phone: "",
+        },
       };
     }
   }
 
   componentDidMount() {
     if (checkForStoredAccessToken()) {
-      let parsedJwt = parseJwt(window.localStorage['dwellinglyAccess']);
+      let parsedJwt = parseJwt(window.localStorage["dwellinglyAccess"]);
       this.setState(
         {
           userSession: {
@@ -90,7 +89,7 @@ export class App extends React.Component {
             firstName: parsedJwt.user_claims.firstName,
             lastName: parsedJwt.user_claims.lastName,
             email: parsedJwt.user_claims.email,
-            phone: parsedJwt.user_claims.phone
+            phone: parsedJwt.user_claims.phone,
           },
         },
         () => {
@@ -107,13 +106,13 @@ export class App extends React.Component {
   };
 
   login = (email, password) => {
-    auth.authenticate(email, password)
-      .then(response => {
-        if (response) {
-          window.localStorage['dwellinglyAccess'] = response.data.access_token;
-          window.localStorage['dwellinglyRefresh'] = response.data.refresh_token;
-          let parsedJwt = parseJwt(response.data.access_token);
-          this.setState({
+    auth.authenticate(email, password).then((response) => {
+      if (response) {
+        window.localStorage["dwellinglyAccess"] = response.data.access_token;
+        window.localStorage["dwellinglyRefresh"] = response.data.refresh_token;
+        let parsedJwt = parseJwt(response.data.access_token);
+        this.setState(
+          {
             userSession: {
               isAuthenticated: true,
               accessJwt: response.data.access_token,
@@ -122,114 +121,190 @@ export class App extends React.Component {
               firstName: parsedJwt.user_claims.firstName,
               lastName: parsedJwt.user_claims.lastName,
               email: parsedJwt.user_claims.email,
-              phone: parsedJwt.user_claims.phone
-            }
-          }, () => {
+              phone: parsedJwt.user_claims.phone,
+            },
+          },
+          () => {
             // Call to refresh the access token 3 minutes later
             setTimeout(this.refreshJwtPeriodically, 180000);
             this.updateAxiosDefaults();
-          });
-        } else {
-          this.notify('Failed to login', 'warn');
-        }
-      });
+          }
+        );
+      } else {
+        this.notify("Failed to login", "warn");
+      }
+    });
   };
 
   refreshJwtPeriodically = () => {
-    auth.refreshAccess(window.localStorage['dwellinglyRefresh'])
+    auth
+      .refreshAccess(window.localStorage["dwellinglyRefresh"])
       .then((response) => {
-        this.setState({
-          userSession: {
-            ...this.state.userSession,
-            isAuthenticated: true,
-            accessJwt: response.data.access_token,
-            /*
+        this.setState(
+          {
+            userSession: {
+              ...this.state.userSession,
+              isAuthenticated: true,
+              accessJwt: response.data.access_token,
+              /*
             userId: parsedJwt.userId,
             userFirst: parsedJwt.userFirst,
             userLast: parsedJwt.userLast,
             userEmail: parsedJwt.userEmail
             */
+            },
+          },
+          () => {
+            refreshTimeout && clearTimeout(refreshTimeout);
+            // Call to refresh the access token 3 minutes later
+            setTimeout(this.refreshJwtPeriodically, 180000);
+            this.updateAxiosDefaults();
           }
-        }, () => {
-          refreshTimeout && clearTimeout(refreshTimeout);
-          // Call to refresh the access token 3 minutes later
-          setTimeout(this.refreshJwtPeriodically, 180000);
-          this.updateAxiosDefaults();
-        });
+        );
         localStorage.setItem("dwellinglyAccess", response.data.access_token);
       })
-      .catch(error => {
+      .catch((error) => {
         console.log("Failed to refresh access token: " + error);
       });
   };
 
   logout = () => {
-    auth.signout()
-      .then(() => {
-        this.setState({
+    auth.signout().then(() => {
+      this.setState(
+        {
           userSession: {
             isAuthenticated: false,
-            accessJwt: '',
-            refreshJwt: '',
-            identity: '',
-            firstName: '',
-            lastName: '',
-            email: ''
-          }
-        }, () => {
-          window.location.replace('/login');
-        });
-      });
+            accessJwt: "",
+            refreshJwt: "",
+            identity: "",
+            firstName: "",
+            lastName: "",
+            email: "",
+          },
+        },
+        () => {
+          window.location.replace("/login");
+        }
+      );
+    });
   };
 
   /**
    * Configure defaults for all axios requests in the App
    */
   updateAxiosDefaults = () => {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${this.state.userSession.accessJwt}`;
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${this.state.userSession.accessJwt}`;
   };
 
   render() {
     return (
-      <UserContext.Provider value={{ user: { ...this.state.userSession }, handleSetUser: this.setUser, refreshJWT: this.refreshJwtPeriodically, login: this.login, logout: this.logout }} >
+      <UserContext.Provider
+        value={{
+          user: { ...this.state.userSession },
+          handleSetUser: this.setUser,
+          refreshJWT: this.refreshJwtPeriodically,
+          login: this.login,
+          logout: this.logout,
+        }}
+      >
         <BrowserRouter>
-          <div className='App'>
-            {this.state.userSession.isAuthenticated
-              && <><NavMenu />
-                <Header /></>}
+          <div className="App">
+            {this.state.userSession.isAuthenticated && (
+              <>
+                <NavMenu />
+                <Header />
+              </>
+            )}
 
             <Switch>
-              <Route exact path='/login' component={LoginForm} />
-              <Route exact path='/signup' component={SignupForm} />
-              <Route exact path='/terms' component={Terms} />
-              <Route exact path='/privacypolicy' component={PrivacyPolicy} />
-              <Route exact path='/forgot-password' component={ForgotPassword} />
-              <PrivateRoute exact path='/' component={() => <Redirect to="/dashboard" />} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute exact path='/home' component={() => <Redirect to="/dashboard" />} />
-              <PrivateRoute exact path='/add/tenant' component={AddTenant} />
-              <PrivateRoute exact path='/add/property' component={AddProperty} />
-              <PrivateRoute exact path='/add/manager' component={AddManager} />
-              <PrivateRoute exact path='/manage/tenants' component={Tenants} />
-              <PrivateRoute exact path='/manage/tenants/:id' component={Tenant} />
-              <PrivateRoute exact path='/add/emergencycontact' component={AddEmergencyContact} />
-              <PrivateRoute exact path='/edit/emergencycontact/:id' component={AddEmergencyContact} />
-              <PrivateRoute exact path='/manage/properties' component={Properties} />
-              <PrivateRoute exact path='/manage/properties/:id' component={Property} />
-              <PrivateRoute exact path='/manage/managers' component={Managers} />
-              <PrivateRoute exact path='/manage/managers/:id' component={Manager} />
-              <PrivateRoute exact path='/tickets' component={Tickets} />
-              <PrivateRoute exact path='/reports' component={Dashboard} />
-              <PrivateRoute exact path='/staff' component={JoinStaff} />
-              <PrivateRoute exact path='/staff/add' component={AddStaffMember} />
-              <PrivateRoute exact path='/emergency' component={EmergencyContacts} />
-              <PrivateRoute exact path='/settings' component={Settings} />
-              <PrivateRoute exact path='/changePassword' component={ChangePassword} />
-              <PrivateRoute exact path='/request-access/:id' component={RequestAccess} />
-              <Route path='*' component={NoMatch} />
+              <Route exact path="/login" component={LoginForm} />
+              <Route exact path="/signup" component={SignupForm} />
+              <Route exact path="/terms" component={Terms} />
+              <Route exact path="/privacypolicy" component={PrivacyPolicy} />
+              <Route exact path="/forgot-password" component={ForgotPassword} />
+              <PrivateRoute
+                exact
+                path="/"
+                component={() => <Redirect to="/dashboard" />}
+              />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/home"
+                component={() => <Redirect to="/dashboard" />}
+              />
+              <PrivateRoute exact path="/add/tenant" component={AddTenant} />
+              <PrivateRoute
+                exact
+                path="/add/property"
+                component={AddProperty}
+              />
+              <PrivateRoute exact path="/add/manager" component={AddManager} />
+              <PrivateRoute exact path="/manage/tenants" component={Tenants} />
+              <PrivateRoute
+                exact
+                path="/manage/tenants/:id"
+                component={Tenant}
+              />
+              <PrivateRoute
+                exact
+                path="/add/emergencycontact"
+                component={AddEmergencyContact}
+              />
+              <PrivateRoute
+                exact
+                path="/edit/emergencycontact/:id"
+                component={AddEmergencyContact}
+              />
+              <PrivateRoute
+                exact
+                path="/manage/properties"
+                component={Properties}
+              />
+              <PrivateRoute
+                exact
+                path="/manage/properties/:id"
+                component={Property}
+              />
+              <PrivateRoute
+                exact
+                path="/manage/managers"
+                component={Managers}
+              />
+              <PrivateRoute
+                exact
+                path="/manage/managers/:id"
+                component={Manager}
+              />
+              <PrivateRoute exact path="/tickets" component={Tickets} />
+              <PrivateRoute exact path="/reports" component={Dashboard} />
+              <PrivateRoute exact path="/staff" component={JoinStaff} />
+              <PrivateRoute
+                exact
+                path="/staff/add"
+                component={AddStaffMember}
+              />
+              <PrivateRoute
+                exact
+                path="/emergency"
+                component={EmergencyContacts}
+              />
+              <PrivateRoute exact path="/settings" component={Settings} />
+              <PrivateRoute
+                exact
+                path="/changePassword"
+                component={ChangePassword}
+              />
+              <PrivateRoute
+                exact
+                path="/request-access/:id"
+                component={RequestAccess}
+              />
+              <Route path="*" component={NoMatch} />
             </Switch>
-            {this.state.userSession.isAuthenticated
-              && <Footer />}
+            {this.state.userSession.isAuthenticated && <Footer />}
           </div>
         </BrowserRouter>
         <ToastContainer
--- src/views/tickets/tickets.jsx
+++ src/views/tickets/tickets.jsx
@@ -1,51 +1,62 @@
-import React, { Component } from 'react';
-import BootstrapTable from 'react-bootstrap-table-next';
-import paginationFactory from 'react-bootstrap-table2-paginator';
-import UserContext from '../../UserContext';
-import Accordion from '../../components/Accordion';
-import { TicketModal } from '../../components/TicketModal';
-import * as axios from 'axios';
+import React, { useState, useEffect, useContext } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import UserContext from "../../UserContext";
+import Accordion from "../../components/Accordion";
+import { TicketModal } from "../../components/TicketModal";
+import * as axios from "axios";
 import Search from "../../components/Search/index";
-import Toast from '../../utils/toast';
+import Toast from "../../utils/toast";
 
-import './tickets.scss';
+import "./tickets.scss";
 
-
-const pageButtonRenderer = ({
-  page,
-  active,
-  disable,
-  title,
-  onPageChange
-}) => {
+const pageButtonRenderer = ({ page, active, disable, title, onPageChange }) => {
   const handleClick = (e) => {
     e.preventDefault();
     onPageChange(page);
   };
-  if(title === 'previous page') {
+  if (title === "previous page") {
     return (
       <li key={title} className="page-item">
-        <a href="#" onClick={handleClick} title={title} className='button is-rounded is-small' >Prev</a>
+        <a
+          href="#"
+          onClick={handleClick}
+          title={title}
+          className="button is-rounded is-small"
+        >
+          Prev
+        </a>
       </li>
     );
   }
-  if(title === 'next page') {
+  if (title === "next page") {
     return (
       <li key={title} className="page-item">
-        <a href="#" onClick={handleClick} title={title} className='button is-rounded is-small' >Next</a>
+        <a
+          href="#"
+          onClick={handleClick}
+          title={title}
+          className="button is-rounded is-small"
+        >
+          Next
+        </a>
       </li>
     );
   }
-  if(active) {
+  if (active) {
     return (
       <li key={page} className="active page-item">
-        <a href="#" onClick={handleClick} title={title}>{page}</a>
+        <a href="#" onClick={handleClick} title={title}>
+          {page}
+        </a>
       </li>
     );
   }
   return (
     <li key={page} className="page-item">
-      <a href="#" onClick={handleClick} title={title}>{page}</a>
+      <a href="#" onClick={handleClick} title={title}>
+        {page}
+      </a>
     </li>
   );
 };
@@ -55,88 +66,87 @@ const options = {
   sizePerPage: 5,
   hideSizePerPage: true,
   hidePageListOnlyOnePage: true,
-  pageButtonRenderer
+  pageButtonRenderer,
 };
 
-export class Tickets extends Component {
-  constructor(props) {
-    super(props);
+export function Tickets(props) {
+  const [tickets, setTickets] = useState([]);
+  const [selectedTicket, setSelectedTicket] = useState(null);
+  const [filteredTickets, setFilteredTickets] = useState([]);
+  const [isFiltered, setIsFiltered] = useState(false);
+  const userContext = useContext(UserContext);
 
-    this.state = {
-      tickets: [],
-      selectedTicket: null,
-      filteredTickets: [],
-      isFiltered: false
-    };
+  const toggleTicketModal = (ticket) => {
+    setSelectedTicket((prevState) => (prevState ? null : ticket));
+  };
 
-    this.getTickets = this.getTickets.bind(this);
-    this.toggleTicketModal = this.toggleTicketModal.bind(this);
-  }
+  const columns = [
+    {
+      dataField: "id",
+      text: "Ticket",
+      sort: true,
+      formatter: (cell, row) => (
+        <button
+          onClick={() => toggleTicketModal(row)}
+          className="link-button cell-align-left"
+        >
+          <p className="cell-header">{row.tenant}</p>
+          <p className="cell-subheader">{row.issue}</p>
+        </button>
+      ),
+      headerStyle: () => {
+        return { width: "20%" };
+      },
+    },
+    {
+      dataField: "sender",
+      text: "Sender",
+      sort: true,
+      headerStyle: () => {
+        return { width: "20%" };
+      },
+    },
+    {
+      dataField: "assigned",
+      text: "Assigned To",
+      sort: true,
+      headerStyle: () => {
+        return { width: "20%" };
+      },
+    },
+    {
+      dataField: "status",
+      text: "Status",
+      sort: true,
+      headerStyle: () => {
+        return { width: "10%" };
+      },
+    },
+    {
+      dataField: "created_at",
+      text: "Created",
+      sort: true,
+      headerStyle: () => {
+        return { width: "15%" };
+      },
+    },
+    {
+      dataField: "updated_at",
+      text: "Updated",
+      sort: true,
+      headerStyle: () => {
+        return { width: "15%" };
+      },
+    },
+  ];
 
-  componentDidMount() {
-    this.getTickets(this.context);
-  }
-
-  toggleTicketModal(ticket) {
-    this.setState(prevState => ({
-      selectedTicket: prevState.selectedTicket ? null : ticket
-    }));
-  }
-
-  columns = [{
-    dataField: 'id',
-    text: 'Ticket',
-    sort: true,
-    formatter: (cell, row) => <button
-      onClick={() => this.toggleTicketModal(row)}
-      className="link-button cell-align-left">
-      <p className="cell-header">{row.tenant}</p>
-      <p className="cell-subheader">{row.issue}</p>
-    </button>,
-    headerStyle: () => {
-      return { width: "20%" };
-    }
-  }, {
-    dataField: 'sender',
-    text: 'Sender',
-    sort: true,
-    headerStyle: () => {
-      return { width: "20%" };
-    }
-  }, {
-    dataField: 'assigned',
-    text: 'Assigned To',
-    sort: true,
-    headerStyle: () => {
-      return { width: "20%" };
-    }
-  }, {
-    dataField: 'status',
-    text: 'Status',
-    sort: true,
-    headerStyle: () => {
-      return { width: "10%" };
-    }
-  }, {
-    dataField: 'created_at',
-    text: 'Created',
-    sort: true,
-    headerStyle: () => {
-      return { width: "15%" };
-    }
-  }, {
-    dataField: 'updated_at',
-    text: 'Updated',
-    sort: true,
-    headerStyle: () => {
-      return { width: "15%" };
-    }
-  }];
-
-  getTickets = (context) => {
-    axios.get(`/api/tickets`, { headers: { "Authorization": `Bearer ${context.user.accessJwt}` } })
+  const getTickets = (context) => {
+    axios
+      .get(`/api/tickets`, {
+        headers: { Authorization: `Bearer ${context.user.accessJwt}` },
+      })
       .then((response) => {
-        this.setState({ tickets: response.data.tickets });
+        setTickets(response.data.tickets);
       })
       .catch((error) => {
         Toast(error.message, "error");
@@ -144,89 +154,83 @@ export class Tickets extends Component {
       });
   };
 
-  setIsFilteredTicketsFalse = async () => {
-    await this.setState({ isFiltered: false });
+  const setIsFilteredTicketsFalse = () => {
+    setIsFiltered(false);
   };
 
-  setOutputState = async (output, isTrue) => {
-    await this.setState({
-      filteredTickets: output,
-      isFiltered: isTrue
-    });
+  const setOutputState = (output, isTrue) => {
+    setFilteredTickets(output);
+    setIsFiltered(isTrue);
   };
 
-  render() {
-    return (
-      <UserContext.Consumer>
-        {session => {
-          this.context = session;
-          return (
-            <div className='main-container'>
-              <div>
-                <div>
-                  <div className="section-header">
-                    <h2 className="page-title">Tickets</h2>
-                  </div>
-                  <Search
-                    input={this.state.tickets}
-                    outputLocation={this.state.filteredTickets}
-                    isFilteredLocation={this.state.isFiltered}
-                    setIsFilteredStateFalse={this.setIsFilteredTicketsFalse}
-                    setOutputState={this.setOutputState}
-                    placeholderMessage="Search by Ticket, Sender, Assignee, Status, or Date"
-                  />
-                  <Accordion
-                    icon={<i className="fas fa-filter"></i>}
-                    header="Filters"
-                  >
-                    <div className="section-row">
-                      <div className="filter-control">
-                        <label>Opened From</label>
-                        <input className="input is-rounded"></input>
-                      </div>
-                      <div className="filter-control">
-                        <label>Category</label>
-                        <div className="select is-rounded">
-                          <select>
-                            <option>All</option>
-                            <option>Complaints</option>
-                            <option>Maintenance</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="filter-control">
-                        <label>Status</label>
-                        <div className="buttons has-addons">
-                          <button className="button is-rounded btn-group">New </button>
-                          <button className="button is-rounded btn-group">In Progress</button>
-                          <button className="button is-rounded btn-group">Closed</button>
-                        </div>
-                      </div>
-                    </div>
-                  </Accordion>
-                  <div>
-                    <BootstrapTable
-                      keyField="id"
-                      data={this.state.isFiltered === true ? this.state.filteredTickets : this.state.tickets}
-                      columns={this.columns}
-                      pagination={paginationFactory(options)}
-                      defaultSortDirection="asc"
-                      bootstrap4={true}
-                      headerClasses="table-header"
-                      classes="full-size-table"
-                    />
-                  </div>
+  useEffect(() => {
+    getTickets(userContext);
+  }, [userContext]);
+
+  return (
+    <div className="main-container">
+      <div>
+        <div>
+          <div className="section-header">
+            <h2 className="page-title">Tickets</h2>
+          </div>
+          <Search
+            input={tickets}
+            outputLocation={filteredTickets}
+            isFilteredLocation={isFiltered}
+            setIsFilteredStateFalse={setIsFilteredTicketsFalse}
+            setOutputState={setOutputState}
+            placeholderMessage="Search by Ticket, Sender, Assignee, Status, or Date"
+          />
+          <Accordion icon={<i className="fas fa-filter"></i>} header="Filters">
+            <div className="section-row">
+              <div className="filter-control">
+                <label>Opened From</label>
+                <input className="input is-rounded"></input>
+              </div>
+              <div className="filter-control">
+                <label>Category</label>
+                <div className="select is-rounded">
+                  <select>
+                    <option>All</option>
+                    <option>Complaints</option>
+                    <option>Maintenance</option>
+                  </select>
                 </div>
-                <TicketModal
-                  show={this.state.selectedTicket}
-                  onClose={this.toggleTicketModal}
-                  ticket={this.state.selectedTicket}>
-                </TicketModal>
+              </div>
+              <div className="filter-control">
+                <label>Status</label>
+                <div className="buttons has-addons">
+                  <button className="button is-rounded btn-group">New </button>
+                  <button className="button is-rounded btn-group">
+                    In Progress
+                  </button>
+                  <button className="button is-rounded btn-group">
+                    Closed
+                  </button>
+                </div>
               </div>
             </div>
-          );
-        }}
-      </UserContext.Consumer>
-    );
-  }
+          </Accordion>
+          <div>
+            <BootstrapTable
+              keyField="id"
+              data={isFiltered === true ? filteredTickets : tickets}
+              columns={columns}
+              pagination={paginationFactory(options)}
+              defaultSortDirection="asc"
+              bootstrap4={true}
+              headerClasses="table-header"
+              classes="full-size-table"
+            />
+          </div>
+        </div>
+        <TicketModal
+          show={selectedTicket}
+          onClose={toggleTicketModal}
+          ticket={selectedTicket}
+        ></TicketModal>
+      </div>
+    </div>
+  );
 }
