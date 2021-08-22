ecff93a48f6384c1c1b6539e610d2d99ed7159a8
Nick Schimek <19519317+NickSchimek@users.noreply.github.com>
2021-08-22 20:39:33 +0900

    Setup system tests

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,11 @@
+describe('Login', function () {
+  it('logs in the user', function () {
+    cy.visit('http://localhost:3000/')
+
+    cy.get('input[name="email"]').type('user1@dwellingly.org')
+    cy.get('input[name="password"]').type('1234')
+    cy.get('button').contains('LOG IN').click()
+
+    cy.location('pathname').should('eq', '/dashboard')
+  })
+})
