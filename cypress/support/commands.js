5e6cd32416cb2d779686ed624777bd3850130508
Nick Schimek <19519317+NickSchimek@users.noreply.github.com>
2021-10-17 23:43:01 +0900

    Add test for granting a user access
    Related to issue #709

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,45 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (email, password) => {
+  cy.request({
+    method:'POST',
+    url:'api/login',
+    body: {
+      email,
+      password,
+    }
+  })
+    .its('body')
+    .then((body) => {
+      window.localStorage.setItem('dwellinglyAccess', body.access_token);
+      window.localStorage.setItem('dwellinglyRefresh', body.refresh_token);
+    })
+})
+
+Cypress.Commands.add('createPendingUser', () => {
+  cy.request({
+    method: 'POST',
+    url: 'api/register',
+    body: {
+      email: "pendingTestUser@example.com",
+      firstName: "Pending Test User",
+      lastName: "Pending Test User",
+      password: "12345678",
+      confirmPassword: "12345678",
+      phone: "503-555-5555"
+    },
+    failOnStatusCode: false
+  })
+})
+
+Cypress.Commands.add('deletePendingUser', () => {
+  cy.get('@pendingUserID').then(id => {
+    cy.request({
+      method: 'DELETE',
+      url: `api/user/${id}`,
+      headers: { 'Authorization' : `Bearer ${localStorage.dwellinglyAccess}` },
+    })
+  })
+})
