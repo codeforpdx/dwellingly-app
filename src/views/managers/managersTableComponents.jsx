1a7382b0b5d9ac168f2602d69c4af1ba4bbcccdf
cboe <cboe15@gmail.com>
2021-10-20 19:36:02 -0700

    Add search capabilities back to PM list page

--- src/views/managers/managersTableComponents.jsx
+++ src/views/managers/managersTableComponents.jsx
@@ -21,9 +21,7 @@ export const columns = [
     formatter: (cell, row) => {
       return (
         <ul>
-          {row.properties?.map((property) => (
-            <li key={property.name}>{property.name}</li>
-          ))}
+          {row.propertyNames}
         </ul>
       );
     },
