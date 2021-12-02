6364defc82e3bc9ba3c7ad7c40e375eff9672b1b
Kaue Reinbold <kaue.reinbold@outlook.com>
2021-12-02 10:17:08 -0300

    extract the convert data method and test it
    refs: #765

---
+++ src/views/PropertyManagerList/util/tests/index.test.js
@@ -0,0 +1,82 @@
+import { convertManagersDataForTable } from '../index'
+
+describe("Manager list util methods", () => {
+   it("should be Active status when 'archived' is false and has a valid 'lastActive'", () => {
+    const managersDataCopy = [
+      {
+        email: "user1@dwellingly.org",
+        phone: "1234567890",
+        password: "1234",
+        firstName: "user1",
+        lastName: "tester",
+        archived: false,
+        lastActive: "11/29/2021 12:25:44",
+        role: "admin",
+        properties: [],
+        tenants: []
+      }
+    ]
+    const [ result ] = convertManagersDataForTable(managersDataCopy)
+
+    expect(result.status).toEqual('Active')
+  });
+
+  it("should be Archived status when 'archived' is true and has a valid 'lastActive'", () => {
+    const managersDataCopy = [
+      {
+        email: "user1@dwellingly.org",
+        phone: "1234567890",
+        password: "1234",
+        firstName: "user1",
+        lastName: "tester",
+        archived: true,
+        lastActive: "11/29/2021 12:25:44",
+        role: "admin",
+        properties: [],
+        tenants: []
+      }
+    ]
+    const [ result ] = convertManagersDataForTable(managersDataCopy)
+
+    expect(result.status).toEqual('Archived')
+  });
+
+  it("should be Archived status when 'archived' is true and 'lastActive' is undefined", () => {
+    const managersDataCopy = [
+      {
+        email: "user1@dwellingly.org",
+        phone: "1234567890",
+        password: "1234",
+        firstName: "user1",
+        lastName: "tester",
+        archived: true,
+        role: "admin",
+        properties: [],
+        tenants: []
+      }
+    ]
+    const [ result ] = convertManagersDataForTable(managersDataCopy)
+
+    expect(result.status).toEqual('Archived')
+  });
+
+  it("should be Pending status when 'archived' is false and 'lastActive' is undefined", () => {
+    const managersDataCopy = [
+      {
+        email: "user1@dwellingly.org",
+        phone: "1234567890",
+        password: "1234",
+        firstName: "user1",
+        lastName: "tester",
+        archived: false,
+        role: "admin",
+        properties: [],
+        tenants: []
+      }
+    ]
+    const [ result ] = convertManagersDataForTable(managersDataCopy)
+    
+    expect(result.status).toEqual('Pending')
+  });
+});
+
