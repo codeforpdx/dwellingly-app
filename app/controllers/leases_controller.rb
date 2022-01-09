89bb6c4e8bce19e256d02dba049daa01717c4198
Nick Schimek <19519317+NickSchimek@users.noreply.github.com>
2022-01-09 10:38:11 +0900

    Remove html formats
    Removes all the html format responses since we only deal with json.

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -18,35 +18,24 @@ class LeasesController < ApplicationController
   def create
     @lease = Lease.new(lease_params)
 
-    respond_to do |format|
-      if @lease.save
-        format.html { redirect_to @lease, notice: "Lease was successfully created." }
-        format.json { render :show, status: :created, location: @lease }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @lease.errors, status: :unprocessable_entity }
-      end
+    if @lease.save
+      render :show, status: :created
+    else
+      render json: @lease.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @lease.update(lease_params)
-        format.html { redirect_to @lease, notice: "Lease was successfully updated." }
-        format.json { render :show, status: :ok, location: @lease }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @lease.errors, status: :unprocessable_entity }
-      end
+    if @lease.update(lease_params)
+      render :show, status: :ok
+    else
+      render json: @lease.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
     @lease.destroy
-    respond_to do |format|
-      format.html { redirect_to leases_url, notice: "Lease was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    head :no_content
   end
 
   private
