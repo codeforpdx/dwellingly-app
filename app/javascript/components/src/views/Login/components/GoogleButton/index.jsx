d9fede02b4009ecc9e3084758d53af5686c9928e
William Wang <73449575+RainbowBridge-William@users.noreply.github.com>
2022-01-19 14:18:54 -0800

    Missing-icon (#782)
    * Fixed Google Icon
    
    * Fix Indent

--- app/javascript/components/src/views/Login/components/GoogleButton/index.jsx
+++ app/javascript/components/src/views/Login/components/GoogleButton/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles/index.scss';
+import googleLogo from "../../../../../../../assets/images/google-dwellingly-favicon.png";
 
 //Documentation
 
@@ -12,19 +13,16 @@ import './styles/index.scss';
 function GoogleButton(props) {
   const { innerText } = props;
 
-
-
   return (
-    <button
-      className="login__button__google button is-primary is-rounded py-4 mt-2"
-    >
+      <button className="login__button__google button is-primary is-rounded py-4 mt-2">
+          <img
+              className="icon mr-3"
+              src={googleLogo}
+              alt="Dwellingly icon"
+          />
 
-      <img className="icon mr-3" src="../../../google-dwellingly-favicon.png" alt="Dwellingly icon" />
-
-      <span className="login__button__google__text">
-        {innerText}
-      </span>
-    </button>
+          <span className="login__button__google__text">{innerText}</span>
+      </button>
   );
 
 };
