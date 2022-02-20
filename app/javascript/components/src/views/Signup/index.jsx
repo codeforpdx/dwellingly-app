7bd7f505aa58370d3ae47c3453feb655a85f84b1
William Wang <73449575+RainbowBridge-William@users.noreply.github.com>
2022-02-20 15:32:33 -0800

    Signup-error-message (#783)
    * Ajusted API Call Catch
    
    * Handled Error
    
    * Cleaned Up Code
    
    * Changed Indent to 2

--- app/javascript/components/src/views/Signup/index.jsx
+++ app/javascript/components/src/views/Signup/index.jsx
@@ -7,12 +7,13 @@ import * as Yup from "yup";
 import UserContext from "../../contexts/UserContext";
 import dwellinglyLogo from "../../assets/images/dwellingly_logo.png";
 import dwellinglyLogoMobile from "../../assets/images/dwellingly_logo_white.png";
-import Modal from '../components/Modal';
+import Modal from "../components/Modal";
 
-import './styles/index.scss'
+import "./styles/index.scss";
 
 const SignupForm = ({ history }) => {
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [showErrorModal, setShowErrorModal] = useState(false);
   const context = useContext(UserContext);
 
   const signup = (
@@ -22,19 +23,28 @@ const SignupForm = ({ history }) => {
     phone,
     password,
     confirmPassword
-  ) => (
-    context.apiCall('post', '/register', {
-      firstName,
-      lastName,
-      email,
-      phone,
-      password,
-      confirmPassword,
-    }, { success: 'Account Created Successfully!' })
-    .then(_ => {
-      setShowConfirmationModal(true);
-    })
-  );
+  ) =>
+    context
+      .apiCall(
+        "post",
+        "/register",
+        {
+          firstName,
+          lastName,
+          email,
+          phone,
+          password,
+          confirmPassword,
+        },
+        { success: "Account Created Successfully!" }
+      )
+      .then((res) => {
+        if (res === false) {
+          setShowErrorModal(true);
+        } else {
+          setShowConfirmationModal(true);
+        }
+      });
 
   const validationSchema = Yup.object().shape({
     firstName: Yup.string()
@@ -54,14 +64,18 @@ const SignupForm = ({ history }) => {
     password: Yup.string()
       .required("Password is required")
       .min(8, "Minimum Length is 8 characters")
-      .matches(/[a-z]/, 'Password must contain at least one lowercase character')
-      .matches(/[A-Z]/, 'Password must contain one uppercase character')
-      .matches(/[a-zA-Z]+[^a-zA-Z\s]+/, 'Password must contain at least 1 number or special char (@,!,#, etc)'),
+      .matches(
+        /[a-z]/,
+        "Password must contain at least one lowercase character"
+      )
+      .matches(/[A-Z]/, "Password must contain one uppercase character")
+      .matches(
+        /[a-zA-Z]+[^a-zA-Z\s]+/,
+        "Password must contain at least 1 number or special char (@,!,#, etc)"
+      ),
     confirmPassword: Yup.string()
       .required("Password confirmation is required")
-      .oneOf([Yup.ref("password"), null],
-        "Passwords must match"
-      ),
+      .oneOf([Yup.ref("password"), null], "Passwords must match"),
   });
 
   const handleClick = () => {
@@ -70,161 +84,208 @@ const SignupForm = ({ history }) => {
 
   const closeConfirmationModal = () => {
     setShowConfirmationModal(false);
-  }
+  };
+
+  const closeErrorModal = () => {
+    setShowErrorModal(false);
+  };
 
   return (
     <UserContext.Consumer>
-      {({ user }) => (
-        user.isAuthenticated
-          ? <Redirect to="/dashboard" />
-          : (
-            <div className="signup__container">
-              <Formik
-                validationSchema={validationSchema}
-                initialValues={{
-                  firstName: "",
-                  lastName: "",
-                  email: "",
-                  phone: "",
-                  password: "",
-                  confirmPassword: "",
-                }}
-                onSubmit={({
-                  firstName, lastName, email, phone, password, confirmPassword,
-                }) => {
-                  signup(firstName, lastName, email, phone, password, confirmPassword);
-                }}
-                enableReinitialize
-              >
-                {
-                  () => (
-                    <div className="signup__form-container">
-                      <div className="signup__mobile-header">
-                        <header className="navbar bg-gradient">
-                          <Link className="navbar-item" id="header-logo" to="/">
-                            <img src={dwellinglyLogoMobile} alt="dwellingly logo" />
-                          </Link>
-                        </header>
-                      </div>
-                      <Form className="signup__form-field-container">
-                        <img className="signup__logo" src={dwellinglyLogo} alt="Dwellingly Logo" />
-                        <h2 className="signup__subtitle">
-                          Create an Account
-                        </h2>
-                        <h2 className="section-title signup__mobile-heading">
-                          Create an Account for Dwelling.ly
-                        </h2>
+      {({ user }) =>
+        user.isAuthenticated ? (
+          <Redirect to="/dashboard" />
+        ) : (
+          <div className="signup__container">
+            <Formik
+              validationSchema={validationSchema}
+              initialValues={{
+                firstName: "",
+                lastName: "",
+                email: "",
+                phone: "",
+                password: "",
+                confirmPassword: "",
+              }}
+              onSubmit={({
+                firstName,
+                lastName,
+                email,
+                phone,
+                password,
+                confirmPassword,
+              }) => {
+                signup(
+                  firstName,
+                  lastName,
+                  email,
+                  phone,
+                  password,
+                  confirmPassword
+                );
+              }}
+              enableReinitialize>
+              {() => (
+                <div className="signup__form-container">
+                  <div className="signup__mobile-header">
+                    <header className="navbar bg-gradient">
+                      <Link className="navbar-item" id="header-logo" to="/">
+                        <img src={dwellinglyLogoMobile} alt="dwellingly logo" />
+                      </Link>
+                    </header>
+                  </div>
+                  <Form className="signup__form-field-container">
+                    <img
+                      className="signup__logo"
+                      src={dwellinglyLogo}
+                      alt="Dwellingly Logo"
+                    />
+                    <h2 className="signup__subtitle">Create an Account</h2>
+                    <h2 className="section-title signup__mobile-heading">
+                      Create an Account for Dwelling.ly
+                    </h2>
 
-                        <div className="signup__form-cell-container">
-                          <Field
-                            className="form-field signup__form-field"
-                            type="text"
-                            name="firstName"
-                            placeholder="First Name"
-                            required
-                          />
+                    <div className="signup__form-cell-container">
+                      <Field
+                        className="form-field signup__form-field"
+                        type="text"
+                        name="firstName"
+                        placeholder="First Name"
+                        required
+                      />
 
-                          <ErrorMessage className="form-error" name="firstName" component="div" />
-                        </div>
+                      <ErrorMessage
+                        className="form-error"
+                        name="firstName"
+                        component="div"
+                      />
+                    </div>
 
-                        <div className="signup__form-cell-container">
-                          <Field
-                            className="form-field signup__form-field"
-                            type="text"
-                            name="lastName"
-                            placeholder="Last Name"
-                            required
-                          />
+                    <div className="signup__form-cell-container">
+                      <Field
+                        className="form-field signup__form-field"
+                        type="text"
+                        name="lastName"
+                        placeholder="Last Name"
+                        required
+                      />
 
-                          <ErrorMessage className="form-error" name="lastName" component="div" />
-                        </div>
+                      <ErrorMessage
+                        className="form-error"
+                        name="lastName"
+                        component="div"
+                      />
+                    </div>
 
-                        <div className="signup__form-cell-container">
-                          <Field
-                            className="form-field signup__form-field"
-                            type="text"
-                            name="email"
-                            placeholder="Email"
-                            required
-                          />
+                    <div className="signup__form-cell-container">
+                      <Field
+                        className="form-field signup__form-field"
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        required
+                      />
 
-                          <ErrorMessage className="form-error" name="email" component="div" />
-                        </div>
+                      <ErrorMessage
+                        className="form-error"
+                        name="email"
+                        component="div"
+                      />
+                    </div>
 
-                        <div className="signup__form-cell-container">
-                          <Field
-                            className="form-field signup__form-field"
-                            type="text"
-                            id="phone"
-                            name="phone"
-                            placeholder="Phone"
-                            required
-                          />
+                    <div className="signup__form-cell-container">
+                      <Field
+                        className="form-field signup__form-field"
+                        type="text"
+                        id="phone"
+                        name="phone"
+                        placeholder="Phone"
+                        required
+                      />
 
-                          <ErrorMessage className="form-error" name="phone" component="div" />
-                        </div>
+                      <ErrorMessage
+                        className="form-error"
+                        name="phone"
+                        component="div"
+                      />
+                    </div>
 
-                        <div className="signup__form-cell-container">
-                          <Field
-                            className="form-field signup__form-field"
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                            required
-                          />
+                    <div className="signup__form-cell-container">
+                      <Field
+                        className="form-field signup__form-field"
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        required
+                      />
 
-                          <ErrorMessage className="form-error" name="password" component="div" />
-                        </div>
+                      <ErrorMessage
+                        className="form-error"
+                        name="password"
+                        component="div"
+                      />
+                    </div>
 
-                        <div className="signup__form-cell-container">
-                          <Field
-                            className="form-field signup__form-field"
-                            type="password"
-                            name="confirmPassword"
-                            placeholder="Confirm Password"
-                            required
-                          />
+                    <div className="signup__form-cell-container">
+                      <Field
+                        className="form-field signup__form-field"
+                        type="password"
+                        name="confirmPassword"
+                        placeholder="Confirm Password"
+                        required
+                      />
 
-                          <ErrorMessage className="form-error" name="confirmPassword" component="div" />
-                        </div>
+                      <ErrorMessage
+                        className="form-error"
+                        name="confirmPassword"
+                        component="div"
+                      />
+                    </div>
 
-                        <div />
-                        <button className="button is-rounded is-primary is-small mt-3" type="submit">
-                          SIGN UP
-                        </button>
-                        <div className="signup__or_container">
-                          <div className="signup__or">
-                            <span className="signup__divider" />
-                            <span className="signup__or_text">
-                              OR
-                            </span>
-                          </div>
-                        </div>
-                      </Form>
-                      <button
-                        className="button is-rounded is-primary is-small mt-2"
-                        type="button"
-                        onClick={handleClick}>
-                        LOG IN
-                      </button>
-                      <div className="signup__privacyPolicyWrapper">
-                        <Link to="/privacypolicy" className="signup__privacyPolicyText">
-                          Privacy Policy
-                        </Link>
+                    <div />
+                    <button
+                      className="button is-rounded is-primary is-small mt-3"
+                      type="submit">
+                      SIGN UP
+                    </button>
+                    <div className="signup__or_container">
+                      <div className="signup__or">
+                        <span className="signup__divider" />
+                        <span className="signup__or_text">OR</span>
                       </div>
                     </div>
-                  )
-                }
-              </Formik>
-              {showConfirmationModal && <Modal
+                  </Form>
+                  <button
+                    className="button is-rounded is-primary is-small mt-2"
+                    type="button"
+                    onClick={handleClick}>
+                    LOG IN
+                  </button>
+                  <div className="signup__privacyPolicyWrapper">
+                    <Link
+                      to="/privacypolicy"
+                      className="signup__privacyPolicyText">
+                      Privacy Policy
+                    </Link>
+                  </div>
+                </div>
+              )}
+            </Formik>
+            {showConfirmationModal && (
+              <Modal
                 titleText="Account Created!"
                 content={
                   <div>
-                    <p>You have successfully made an account with Dwelling.ly.
-                      Your account must be approved by an Admin before you can use it.
+                    <p>
+                      You have successfully made an account with Dwelling.ly.
+                      Your account must be approved by an Admin before you can
+                      use it.
                     </p>
-                    <br/>
-                    <p>Keep your eye out for an approval email with instructions on how to access your account.</p>
+                    <br />
+                    <p>
+                      Keep your eye out for an approval email with instructions
+                      on how to access your account.
+                    </p>
                   </div>
                 }
                 hasButtons={false}
@@ -232,10 +293,27 @@ const SignupForm = ({ history }) => {
                 redirectButtonPath="/login"
                 redirectButtonText="Return to Login"
                 closeHandler={closeConfirmationModal}
-              />}
-            </div>
-          )
-      )}
+              />
+            )}
+            {showErrorModal && (
+              <Modal
+                titleText="Account Creation Failed"
+                content={
+                  <div>
+                    <p>
+                      We couldn't create your account at this time. Please try
+                      again later.
+                    </p>
+                  </div>
+                }
+                hasButtons={false}
+                hasRedirectButton={false}
+                closeHandler={closeErrorModal}
+              />
+            )}
+          </div>
+        )
+      }
     </UserContext.Consumer>
   );
 };
