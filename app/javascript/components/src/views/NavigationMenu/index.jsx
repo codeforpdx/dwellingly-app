3bf5eb5af6dcfcd908cb98cca425d92141d4aa49
Chris-Boe <cboe15@gmail.com>
2022-02-20 21:38:14 -0800

    Formatting

--- app/javascript/components/src/views/NavigationMenu/index.jsx
+++ app/javascript/components/src/views/NavigationMenu/index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faColumns,
-	faPlusCircle,
-	faUserCog,
-	faUserAlt,
-	faPhoneAlt,
-	faCog,
-	faXmark,
+  faColumns,
+  faPlusCircle,
+  faUserCog,
+  faUserAlt,
+  faPhoneAlt,
+  faCog,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { useLocation, Link } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -15,114 +15,114 @@ import UserContext from "../../contexts/UserContext";
 
 import './styles/index.scss'
 
-export const MenuLink = ({isMobile, menuClose, icon, isBold, name, href, passedClassName, category }) => {
-	const loc = useLocation();
-	let isActiveLink =
-		loc.pathname.includes(href)
-		|| (category && loc.pathname.split('/')[1].includes(category));
+export const MenuLink = ({ isMobile, menuClose, icon, isBold, name, href, passedClassName, category }) => {
+  const loc = useLocation();
+  let isActiveLink =
+    loc.pathname.includes(href)
+    || (category && loc.pathname.split('/')[1].includes(category));
   let cursorOverride = href ? "" : "default-cursor";
-	const linkColor = isActiveLink ? "has-text-black" : "has-text-white";
-	return (
-		<li className={passedClassName ? `pb-2 ${passedClassName}` : "pb-2"}>
-			<Link to={href} onClick={isMobile ? menuClose : null} className={`is-size-7 ${isBold && "has-text-weight-bold"} ${linkColor} ${cursorOverride}`}>
-				{icon && (
-					<span className="icon is-small">
-						<FontAwesomeIcon icon={icon} />
-					</span>
-				)}
-				<span className="pl-1">{name}</span>
-			</Link>
-		</li>
-	);
+  const linkColor = isActiveLink ? "has-text-black" : "has-text-white";
+  return (
+    <li className={passedClassName ? `pb-2 ${passedClassName}` : "pb-2"}>
+      <Link to={href} onClick={isMobile ? menuClose : null} className={`is-size-7 ${isBold && "has-text-weight-bold"} ${linkColor} ${cursorOverride}`}>
+        {icon && (
+          <span className="icon is-small">
+            <FontAwesomeIcon icon={icon} />
+          </span>
+        )}
+        <span className="pl-1">{name}</span>
+      </Link>
+    </li>
+  );
 };
 
 MenuLink.propTypes = {
-	isMobile: PropTypes.bool,
-	menuClose: PropTypes.func,
-	icon: PropTypes.object,
-	isBold: PropTypes.bool,
-	name: PropTypes.string.isRequired,
-	href: PropTypes.string.isRequired,
-	passedClassName: PropTypes.string
+  isMobile: PropTypes.bool,
+  menuClose: PropTypes.func,
+  icon: PropTypes.object,
+  isBold: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+  passedClassName: PropTypes.string
 };
 
 export const NavMenu = (props) => {
-	const loc = useLocation();
-	const sidebar = props.isOpen ? "sidebar is-open" : "sidebar";
-	const sidebarBlock = props.isOpen ? "sidebarBlock is-open" : "sidebarBlock";
-	if (
-		loc.pathname === "/login" ||
-		loc.pathname === "/signup" ||
-		loc.pathname === "/terms"
-	) {
-		return null;
-	}
+  const loc = useLocation();
+  const sidebar = props.isOpen ? "sidebar is-open" : "sidebar";
+  const sidebarBlock = props.isOpen ? "sidebarBlock is-open" : "sidebarBlock";
+  if (
+    loc.pathname === "/login" ||
+    loc.pathname === "/signup" ||
+    loc.pathname === "/terms"
+  ) {
+    return null;
+  }
 
 
-	return (
-		<UserContext.Consumer>
-			{({ user }) => (
-				<div>
-				<div className = {sidebarBlock}>
-					<button onClick={props.toggle}></button>
-				</div>
-				<div className={`has-background-primary ${sidebar}`}>
-						
-					<div className="sidebar-header">
-          					<button
-            					variant="link"
-            					onClick={props.toggle}            				
-          					>
-            					<FontAwesomeIcon icon={faXmark} size="sm" />
-          					</button>
-						
-					</div>
-					<div className="menu">
-						<ul className="menu-list">
-							<MenuLink
-								isMobile={props.isMobile}
-								menuClose={props.toggle}
-								name={`${user.firstName} ${user.lastName}`}
-								isBold
-								icon={faUserAlt}
-								href="/home"
-								passedClassName="pb-5 pt-1"
-							/>
-							<MenuLink
-								isMobile={props.isMobile}
-								menuClose={props.toggle}
-								props
-								name="Dashboard"
-								isBold
-								icon={faColumns}
-								href="/dashboard"
-								category="dashboard"
-							/>
-							<MenuLink name="Add New" isBold icon={faPlusCircle} category="add" />
-							<div className="pl-4 is-child-link">
-								<MenuLink name="Tenant" href="/add/tenant" isMobile={props.isMobile} menuClose={props.toggle}/>
-								<MenuLink name="Property" href="/add/property" isMobile={props.isMobile} menuClose={props.toggle}/>
-								<MenuLink name="Property Manager" href="/add/manager" isMobile={props.isMobile} menuClose={props.toggle}/>
-								<MenuLink name="Ticket" href="/add/ticket" isMobile={props.isMobile} menuClose={props.toggle}/>
-							</div>
+  return (
+    <UserContext.Consumer>
+      {({ user }) => (
+        <div>
+          <div className={sidebarBlock}>
+            <button onClick={props.toggle}></button>
+          </div>
+          <div className={`has-background-primary ${sidebar}`}>
 
-							<MenuLink name="Manage" isBold icon={faUserCog} category="manage" />
-							<div className="pl-4 is-child-link">
-								<MenuLink name="Tenants" href="/manage/tenants" isMobile={props.isMobile} menuClose={props.toggle}/>
-								<MenuLink name="Properties" href="/manage/properties" isMobile={props.isMobile} menuClose={props.toggle}/>
-								<MenuLink name="Property Managers" href="/manage/managers" isMobile={props.isMobile} menuClose={props.toggle}/>
-								<MenuLink name="Tickets" href="/manage/tickets" isMobile={props.isMobile} menuClose={props.toggle}/>
-							</div>
+            <div className="sidebar-header">
+              <button
+                variant="link"
+                onClick={props.toggle}
+              >
+                <FontAwesomeIcon icon={faXmark} size="sm" />
+              </button>
 
-							<MenuLink name="JOIN Staff" isBold icon={faUserAlt} href="/staff" isMobile={props.isMobile} menuClose={props.toggle}/>
-							<MenuLink name="Emergency Numbers" isBold icon={faPhoneAlt} href="/emergency" isMobile={props.isMobile} menuClose={props.toggle}/>
-							<MenuLink name="Settings" isBold icon={faCog} href="/settings" isMobile={props.isMobile} menuClose={props.toggle}/>
-						</ul>
-					</div>
-				</div>
-				</div>
+            </div>
+            <div className="menu">
+              <ul className="menu-list">
+                <MenuLink
+                  isMobile={props.isMobile}
+                  menuClose={props.toggle}
+                  name={`${user.firstName} ${user.lastName}`}
+                  isBold
+                  icon={faUserAlt}
+                  href="/home"
+                  passedClassName="pb-5 pt-1"
+                />
+                <MenuLink
+                  isMobile={props.isMobile}
+                  menuClose={props.toggle}
+                  props
+                  name="Dashboard"
+                  isBold
+                  icon={faColumns}
+                  href="/dashboard"
+                  category="dashboard"
+                />
+                <MenuLink name="Add New" isBold icon={faPlusCircle} category="add" />
+                <div className="pl-4 is-child-link">
+                  <MenuLink name="Tenant" href="/add/tenant" isMobile={props.isMobile} menuClose={props.toggle} />
+                  <MenuLink name="Property" href="/add/property" isMobile={props.isMobile} menuClose={props.toggle} />
+                  <MenuLink name="Property Manager" href="/add/manager" isMobile={props.isMobile} menuClose={props.toggle} />
+                  <MenuLink name="Ticket" href="/add/ticket" isMobile={props.isMobile} menuClose={props.toggle} />
+                </div>
 
-			)}
-		</UserContext.Consumer>
-	);
+                <MenuLink name="Manage" isBold icon={faUserCog} category="manage" />
+                <div className="pl-4 is-child-link">
+                  <MenuLink name="Tenants" href="/manage/tenants" isMobile={props.isMobile} menuClose={props.toggle} />
+                  <MenuLink name="Properties" href="/manage/properties" isMobile={props.isMobile} menuClose={props.toggle} />
+                  <MenuLink name="Property Managers" href="/manage/managers" isMobile={props.isMobile} menuClose={props.toggle} />
+                  <MenuLink name="Tickets" href="/manage/tickets" isMobile={props.isMobile} menuClose={props.toggle} />
+                </div>
+
+                <MenuLink name="JOIN Staff" isBold icon={faUserAlt} href="/staff" isMobile={props.isMobile} menuClose={props.toggle} />
+                <MenuLink name="Emergency Numbers" isBold icon={faPhoneAlt} href="/emergency" isMobile={props.isMobile} menuClose={props.toggle} />
+                <MenuLink name="Settings" isBold icon={faCog} href="/settings" isMobile={props.isMobile} menuClose={props.toggle} />
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      )}
+    </UserContext.Consumer>
+  );
 }
