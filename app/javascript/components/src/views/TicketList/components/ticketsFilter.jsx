43060b9b9f3654f527580925d94d27c2497ebdfd
Kevin Gottfried <kgott541@gmail.com>
2022-02-25 16:57:14 -0800

    Added filter by date

--- app/javascript/components/src/views/TicketList/components/ticketsFilter.jsx
+++ app/javascript/components/src/views/TicketList/components/ticketsFilter.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { formatDate } from '../../../utils/date';
+import { formatDate, isValidDate } from '../../../utils/date';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 function Filter(props) {
     const { input, placeholderMessage } = props;
     const [status, setStatus] = useState('');
-
+    const [isActive, setActive] = useState('');
     let timerId;
     const throttleFunction = (func, delay) => {
         if (timerId) {
@@ -30,11 +30,15 @@ function Filter(props) {
 
     const handleStatus = (e) => {
         setStatus(e.currentTarget.id);
+        setActive(e.currentTarget.id);
+
     };
 
     const clearDate = () => {
         document.getElementById("filterDate").value = "";
         props.setIsFilteredStateFalse();
+        setStatus('');
+        setActive('');
     };
 
     const searchInput = () => {
@@ -43,19 +47,43 @@ function Filter(props) {
         let date = formatDate(document.getElementById("filterDate").value);
         let searchTokens = [];
 
-        date ? searchTokens.push(date) : null;
+        isValidDate(date) ? searchTokens.push(date) : null;
         status ? searchTokens.push(status) : null;
         if (searchTokens.length > 0) {
-            for (var i = 0; i < allData.length; i++) {
-                let dataPoint = Object.assign({}, allData[i]);
-                delete dataPoint.id;
-                delete dataPoint.updated_at;
-                let dataValues = Object.values(dataPoint).toString().toLowerCase()
-                if (searchTokens.every(cur => dataValues.includes(cur))) {
-                    output.push(allData[i]);
+            if (searchTokens.length === 1){
+                if (searchTokens[0] === status){
+                    for (var i = 0; i < allData.length; i++) {
+                        let dataPoint = Object.assign({}, allData[i]);
+                        if (dataPoint.status.toLowerCase() === searchTokens[0]) {
+                            output.push(allData[i]);
+                        }
+                    }
                 }
-
+                else{
+                    for (var i = 0; i < allData.length; i++) {
+                        let dataPoint = Object.assign({}, allData[i]);
+                        delete dataPoint.id;
+                        delete dataPoint.updated_at;
+                        if (Date.parse(dataPoint.created_at) >= Date.parse(searchTokens[0])) {
+                            output.push(allData[i]);
+                            
+                        }
+                    }
+                }
             }
+            else{
+                for (var i = 0; i < allData.length; i++) {
+                    let dataPoint = Object.assign({}, allData[i]);
+                    delete dataPoint.id;
+                    delete dataPoint.updated_at;
+                    if (dataPoint.status.toLowerCase() === searchTokens[1] 
+                    && Date.parse(dataPoint.created_at) >= Date.parse(searchTokens[0])) {
+                        output.push(allData[i]);
+                    }
+                    
+                }
+            }
+            
             props.setOutputState(output, true);
         }
         else {
@@ -73,9 +101,12 @@ function Filter(props) {
                 <div className="filter-control">
                     <label>Status</label>
                     <div className="buttons has-addons" >
-                        <button id="open" onClick={e => handleStatus(e)} className="button is-rounded btn-group">Open </button>
-                        <button id="progress" onClick={e => handleStatus(e)} className="button is-rounded btn-group">In Progress</button>
-                        <button id="closed" onClick={e => handleStatus(e)} className="button is-rounded btn-group">Closed</button>
+                        <button id="open" onClick={e => handleStatus(e)} 
+                            className={`button ${isActive === "open" ? 'active ' : ''}is-rounded btn-group`}>Open </button>
+                        <button id="in progress" onClick={e => handleStatus(e)} 
+                            className={`button ${isActive === "in progress" ? 'active ' : ''}is-rounded btn-group`}>In Progress</button>
+                        <button id="closed" onClick={e => handleStatus(e)} 
+                            className={`button ${isActive === "closed" ? 'active ' : ''}is-rounded btn-group`}p>Closed</button>
                     </div>
                 </div>
                 <div className="filter-control">
