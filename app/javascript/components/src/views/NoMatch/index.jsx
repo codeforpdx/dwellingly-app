6da285f46035b54a021dc9fc0de80c58e6d1b5c0
Chris Boe <cboe15@gmail.com>
2022-08-17 20:01:42 -0700

    Other fixes (#849)
    * Fix add property manager page
    
    * Hide Join staff link on mobile dashboard for PM's
    
    * Update wording

--- app/javascript/components/src/views/NoMatch/index.jsx
+++ app/javascript/components/src/views/NoMatch/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NoMatch = () => {
   return (
     <div className='main-container'>
       <div className="noMatch__container">
         <h2>404</h2>
-        <p>Beep, boop. Sorry, but no page matching your request was found.</p>
+        <p>Sorry, but no page matching your request was found.</p>
+        <p>Click <Link to="/dashboard">here</Link> to go back to your Dashboard.</p>
       </div>
     </div>
   );
